752d776b04ca8835b943efd4aabc2e60f2e1c3cd
Tom Hughes <tom@compton.nu>
2013-06-17 22:49:32 +0100

    Add cgiirc cookbook

---
+++ cookbooks/cgiirc/metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "OpenStreetMap Administrators"
+maintainer_email  "admins@openstreetmap.org"
+license           "Apache 2.0"
+description       "Installs and configures cgiirc"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           "1.0.0"
+depends           "apache"
