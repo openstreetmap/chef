55e94b06f8e0d01c6c91b584b9ffe37d809e066a
Tom Hughes <tom@compton.nu>
2013-07-05 09:21:03 +0100

    Only run awstats for planet on Sunday

--- roles/stats.rb
+++ roles/stats.rb
@@ -6,11 +6,13 @@ default_attributes(
     :sites => [
       { 
         :name => "planet.openstreetmap.org",
-        :log_pattern => "%YYYY-168-%MM-168-%DD-168.gz"
+        :log_pattern => "%YYYY-168-%MM-168-%DD-168.gz",
+        :days => "7"
       },
       { 
         :name => "www.openstreetmap.org",
-        :log_pattern => "*-%YYYY-48-%MM-48-%DD-48.gz"
+        :log_pattern => "*-%YYYY-48-%MM-48-%DD-48.gz",
+        :days => "*"
       }
     ]
   }
