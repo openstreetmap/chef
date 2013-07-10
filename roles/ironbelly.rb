7862d728d8217f3b3b47ff404364363646da82fc
Tom Hughes <tom@compton.nu>
2013-07-10 17:18:23 +0100

    Add IP addresses for ironbelly

--- roles/ironbelly.rb
+++ roles/ironbelly.rb
@@ -4,23 +4,23 @@ description "Master role applied to ironbelly"
 default_attributes(
   :networking => {
     :interfaces => {
-      :external_ipv4 => {
-        :interface => "eth0",
-        :role => :external,
-        :family => :inet,
-        :address => ""
-      },
-      :external_ipv6 => {
-        :interface => "eth0",
-        :role => :external,
-        :family => :inet6,
-        :address => ""
-      },
       :internal_ipv4 => {
-        :interface => "eth1",
+        :interface => "eth0",
         :role => :internal,
         :family => :inet,
-        :address => ""
+        :address => "146.179.159.177"
+      },
+      :external_ipv4 => {
+        :interface => "eth1",
+        :role => :external,
+        :family => :inet,
+        :address => "193.63.75.107"
+      },
+      :external_ipv6 => {
+        :interface => "eth1",
+        :role => :external,
+        :family => :inet6,
+        :address => "2001:630:12:500:225:90ff:fec4:f6ef"
       }
     }
   }
