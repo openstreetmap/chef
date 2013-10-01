dd70748b24f3641e494383ea2894b95e5950f17e
Tom Hughes <tom@compton.nu>
2013-10-01 20:15:59 +0100

    Update noquiklos role for move to UCL

--- roles/noquiklos.rb
+++ roles/noquiklos.rb
@@ -8,12 +8,18 @@ default_attributes(
         :interface => "eth0",
         :role => :internal,
         :family => :inet,
-        :address => "10.89.121.98"
+        :address => "10.0.0.13"
+      },
+      :external_ipv4 => {
+        :interface => "eth1",
+        :role => :external,
+        :family => :inet,
+        :address => "128.40.168.99"
       }
     }
   }
 )
 
 run_list(
-  "role[firefishynet]"
+  "role[ucl-internal]"
 )
