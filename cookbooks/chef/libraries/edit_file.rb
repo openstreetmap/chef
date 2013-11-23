7f6415938eb888893b49cc968a5c01082743b7d7
Tom Hughes <tom@compton.nu>
2013-11-23 11:48:42 +0000

    Defer evaluation of an edited file's contents
    We want to make sure we don't build the contents until the resource
    is asctually being run, as it may rely on template files installed by
    a previous resource.

--- cookbooks/chef/libraries/edit_file.rb
+++ cookbooks/chef/libraries/edit_file.rb
@@ -1,9 +1,22 @@
 class Chef
+  class Util
+    class EditedFile
+      def initialize(file, block)
+        @file = file
+        @block = block
+      end
+
+      def to_s
+        ::File.new(@file).collect do |line|
+          line = @block.call(line)
+        end.join("")
+      end
+    end
+  end
+
   class Recipe
     def edit_file(file, &block)
-      ::File.new(file).collect do |line|
-        line = yield line
-      end.join("")
+      Chef::Util::EditedFile.new(file, block)
     end
   end
 end
