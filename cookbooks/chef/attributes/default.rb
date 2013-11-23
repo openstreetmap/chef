a49460026a3a31ca582f63914cf33f1efca20bd6
Tom Hughes <tom@compton.nu>
2013-11-23 11:14:32 +0000

    Upgrade chef client to 11.8.0

--- cookbooks/chef/attributes/default.rb
+++ cookbooks/chef/attributes/default.rb
@@ -2,7 +2,7 @@
 default[:apt][:sources] = node[:apt][:sources] | [ "opscode" ]
 
 # Set the default client version
-default[:chef][:client][:version] = "11.6.0-1"
+default[:chef][:client][:version] = "11.8.0-1"
 
 # A list of gems needed by chef recipes
 default[:chef][:gems] = []
