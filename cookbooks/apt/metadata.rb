3634febd0a66e60ca36e0e33779b3b35f67c1198
Tom Hughes <tom@compton.nu>
2013-11-23 17:51:28 +0000

    Use README.md for the long_description

--- cookbooks/apt/metadata.rb
+++ cookbooks/apt/metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Tom Hughes"
 maintainer_email "tom@compton.nu"
 license          "Apache 2.0"
 description      "Installs/Configures apt"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
 supports         "debian"
 supports         "ubuntu"
--- cookbooks/tilecache/metadata.rb
+++ cookbooks/tilecache/metadata.rb
@@ -3,7 +3,7 @@ maintainer        "OpenStreetMap Administrators"
 maintainer_email  "admins@openstreetmap.org"
 license           "Apache 2.0"
 description       "Installs and configures a tile cache"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.0"
 depends           "ssl"
 depends           "squid"
