d047e2bd47ec035f11ce1ced0d1e217a10edd838
Tom Hughes <tom@compton.nu>
2013-11-24 11:21:34 +0000

    Reimplement edit_file using Chef::DelayedEvaluator

--- cookbooks/chef/libraries/edit_file.rb
+++ cookbooks/chef/libraries/edit_file.rb
@@ -1,26 +1,11 @@
 class Chef
-  class Util
-    class EditedFile
-      def initialize(file, block)
-        @file = file
-        @block = block
-      end
-
-      def kind_of?(klass)
-        klass == String || super
-      end
-
-      def to_s
-        ::File.new(@file).collect do |line|
-          line = @block.call(line)
-        end.join("")
-      end
-    end
-  end
-
   class Recipe
     def edit_file(file, &block)
-      Chef::Util::EditedFile.new(file, block)
+      Chef::DelayedEvaluator.new do
+        ::File.new(file).collect do |line|
+          line = block.call(line)
+        end.join("")
+      end
     end
   end
 end
