3093125b06b088ec7daab3e21f75b85851a586a6
Grant Slater <git@firefishy.com>
2014-02-07 01:57:33 +0000

    Add basic forum cookbook + role and apply for clifford

---
+++ roles/forum.rb
@@ -0,0 +1,25 @@
+name "forum"
+description "Role applied to all forum servers"
+
+default_attributes(
+  :accounts => {
+    :users => {
+      :lambertus => { :status => :administrator }
+    }
+  },
+  :apache => {
+    :mpm => "prefork",
+    :timeout => 60,
+    :keepalive => false,
+    :prefork => {
+      :start_servers => 20,
+      :min_spare_servers => 20,
+      :max_spare_servers => 50,
+      :max_clients => 256,
+    }
+  }
+)
+
+run_list(
+  "recipe[forum]"
+)
