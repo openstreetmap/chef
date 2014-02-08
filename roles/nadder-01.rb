78e29ff52119417c16425562cead9eb4c77bc1ea
Grant Slater <git@firefishy.com>
2014-02-08 06:43:27 +0000

    nadder: Decrease scheduler queue depth to improve latency

--- roles/nadder-01.rb
+++ roles/nadder-01.rb
@@ -18,8 +18,8 @@ default_attributes(
     :hdd_tune => {
       :comment => "Tune the queue for improved performance",
       :parameters => {
-        "block/vda/queue/nr_requests" => "512",
-        "block/vdb/queue/nr_requests" => "512",
+        "block/vda/queue/nr_requests" => "64",
+        "block/vdb/queue/nr_requests" => "64",
         "block/vda/queue/scheduler" => "deadline",
         "block/vdb/queue/scheduler" => "deadline"
       }
