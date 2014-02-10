f1d53774f16d3ac9956520d407a1a8488df201fe
Grant Slater <git@firefishy.com>
2014-02-10 21:40:59 +0000

    forum: fix depends

--- cookbooks/forum/metadata.rb
+++ cookbooks/forum/metadata.rb
@@ -6,3 +6,4 @@ description       "Installs and configures a roundup server"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.0"
 depends           "apache"
+depends           "mysql"
