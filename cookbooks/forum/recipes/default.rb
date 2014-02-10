66bb863bca75089fffd47acdb0c4db4001a70be8
Grant Slater <git@firefishy.com>
2014-02-10 21:34:36 +0000

    Add basic forum/fluxbb cookbook

--- cookbooks/forum/recipes/default.rb
+++ cookbooks/forum/recipes/default.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe "apache:ssl"
+include_recipe "apache::ssl"
 include_recipe "git"
 include_recipe "mysql"
 
