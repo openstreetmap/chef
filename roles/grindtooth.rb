e015caf5d19c5b47dcd7912665259c78f01b0ff9
Tom Hughes <tom@compton.nu>
2014-02-15 10:33:01 +0000

    Move norbert's external IP to grindtooth

--- roles/grindtooth.rb
+++ roles/grindtooth.rb
@@ -9,6 +9,12 @@ default_attributes(
         :role => :internal,
         :family => :inet,
         :address => "10.0.0.19"
+      },
+      :external_ipv4 => {
+        :interface => "eth1",
+        :role => :external,
+        :family => :inet,
+        :address => "128.40.168.100"
       }
     }
   }
