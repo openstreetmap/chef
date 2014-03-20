0d6170ebcea91c7cbc057c5aeb133e6d882ab3b3
Grant Slater <git@firefishy.com>
2014-03-20 09:57:56 +0000

    jakelong: smaller squid coss store

--- roles/jakelong.rb
+++ roles/jakelong.rb
@@ -23,8 +23,8 @@ default_attributes(
     }
   },
   :squid => {
-    :cache_mem => "650 MB",
-    :cache_dir => "coss /store/squid/coss-01 15000 block-size=8192 max-size=262144 membufs=30"
+    :cache_mem => "700 MB",
+    :cache_dir => "coss /store/squid/coss-01 10000 block-size=8192 max-size=262144 membufs=30"
   },
   :sysctl => {
     :kvm => {
