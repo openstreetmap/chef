368b16382fa5378336e88b511baa35d522ff1bfa
Tom Hughes <tom@compton.nu>
2014-03-24 22:43:53 +0000

    Configure two mailboxes for civicrm

--- roles/crm.rb
+++ roles/crm.rb
@@ -6,11 +6,20 @@ default_attributes(
   :exim => {
     :local_domains => [ "crm.osmfoundation.org" ],
     :routes => {
-      :crm => {
-        :comment => "crm.osmfoundation.org",
+      :crm_return => {
+        :comment => "return@crm.osmfoundation.org",
         :domains => [ "crm.osmfoundation.org" ],
-        :maildir => "/var/mail/crm",
-        :user => "wordpress",
+        :local_parts => [ "return" ],
+        :maildir => "/var/mail/crm-return",
+        :user => "www-data",
+        :group => "mail"
+      },
+      :crm_mail => {
+        :comment => "mail@crm.osmfoundation.org",
+        :domains => [ "crm.osmfoundation.org" ],
+        :local_parts => [ "mail" ],
+        :maildir => "/var/mail/crm-mail",
+        :user => "www-data",
         :group => "mail"
       }
     }
