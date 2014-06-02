bf255e52404fad159c83c5ca94afa5f8bba15e4f
Tom Hughes <tom@compton.nu>
2014-06-03 00:28:25 +0100

    Upodate chef client to 11.12.4

--- cookbooks/chef/attributes/default.rb
+++ cookbooks/chef/attributes/default.rb
@@ -5,7 +5,7 @@ default[:apt][:sources] = node[:apt][:sources] | [ "opscode" ]
 default[:chef][:server][:version] = "11.0.12-1"
 
 # Set the default client version
-default[:chef][:client][:version] = "11.12.2-1"
+default[:chef][:client][:version] = "11.12.4-1"
 
 # A list of gems needed by chef recipes
 default[:chef][:gems] = []
