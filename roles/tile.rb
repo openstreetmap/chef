5b2a2b1f80153fa0423b0256740ae5a99f586dcb
Tom Hughes <tom@compton.nu>
2014-06-09 18:36:48 +0100

    Update carto stylesheet to v2.15.0

--- roles/tile.rb
+++ roles/tile.rb
@@ -83,7 +83,7 @@ default_attributes(
     :styles => {
       :default => {
         :repository => "git://github.com/gravitystorm/openstreetmap-carto.git",
-        :revision => "v2.14.1",
+        :revision => "v2.15.0",
         :max_zoom => 19
       }
     }
