c349bc6136c164c58c7bdc90e77283ba0d440c7e
Tom Hughes <tom@compton.nu>
2014-07-10 22:50:28 +0100

    Update chef server to 11.1.3-1

--- cookbooks/chef/attributes/default.rb
+++ cookbooks/chef/attributes/default.rb
@@ -2,7 +2,7 @@
 default[:apt][:sources] = node[:apt][:sources] | [ "opscode" ]
 
 # Set the default server version
-default[:chef][:server][:version] = "11.0.12-1"
+default[:chef][:server][:version] = "11.1.3-1"
 
 # Set the default client version
 default[:chef][:client][:version] = "11.12.8-2"
