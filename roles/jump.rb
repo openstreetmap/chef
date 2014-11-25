391dee0b5eea292f0b358c127623d83374d30f2c
Tom Hughes <tom@compton.nu>
2014-11-25 19:54:45 +0000

    Add role for saphira

---
+++ roles/jump.rb
@@ -0,0 +1,28 @@
+name "jump"
+description "Role applied to all servers at Jump Networks"
+
+default_attributes(
+  :networking => {
+    :nameservers => [
+      "185.73.44.3",
+      "2001:ba8:0:2c02::",
+      "2001:ba8:0:2c03::",
+      "2001:ba8:0:2c04::"
+    ],
+    :roles => {
+      :external => {
+        :zone => "jn"
+      }
+    }
+  }
+)
+
+override_attributes(
+  :ntp => {
+    :servers => [ "0.uk.pool.ntp.org", "1.uk.pool.ntp.org", "europe.pool.ntp.org" ]
+  }
+)
+
+run_list(
+  "role[gb]"
+)
---
+++ roles/saphira.rb
@@ -0,0 +1,44 @@
+name "saphira"
+description "Master role applied to saphira"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :external_ipv4 => {
+        :interface => "eth0",
+        :role => :external,
+        :family => :inet,
+        :address => "185.73.44.30",
+        :prefix => "22",
+        :gateway => "185.73.44.1"
+      },
+      :external_ipv6 => {
+        :interface => "eth0",
+        :role => :external,
+        :family => :inet6,
+        :address => "2001:ba8:0:2c1e::",
+        :prefix => "64",
+        :gateway => "fe80::fcff:ffff:feff:ffff"
+      }
+    }
+  },
+  :squid => {
+    :cache_mem => "7500 MB",
+    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+  },
+  :tilecache => {
+    :tile_parent => "london.render.openstreetmap.org",
+    :tile_siblings => [
+      "trogdor.openstreetmap.org",
+      "tabaluga.openstreetmap.org",
+      "ridgeback.openstreetmap.org",
+      "fume.openstreetmap.org",
+      "gorynych.openstreetmap.org"
+    ]
+  }
+)
+
+run_list(
+  "role[jump]",
+  "role[tilecache]"
+)
