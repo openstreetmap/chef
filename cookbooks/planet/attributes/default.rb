72c73d031f5f71b6fd56b0ad1dccbaf7b174c0d2
Matt Amos <zerebubuth@gmail.com>
2014-12-15 20:33:46 +0000

    Put source directory somewhere more sensible.

--- cookbooks/planet/attributes/default.rb
+++ cookbooks/planet/attributes/default.rb
@@ -3,6 +3,6 @@ default[:planet][:dump][:xml_history_directory] = "/store/planet/planet/full-his
 default[:planet][:dump][:pbf_directory] = "/store/planet/pbf"
 default[:planet][:dump][:pbf_history_directory] = "/store/planet/pbf/full-history"
 
-default[:planet][:tilelog][:source_directory] = "/home/www-data/tilelog"
+default[:planet][:tilelog][:source_directory] = "/opt/tilelog"
 default[:planet][:tilelog][:input_directory] = "/store/logs/tile.openstreetmap.org"
 default[:planet][:tilelog][:output_directory] = "/store/planet/tile_logs"
