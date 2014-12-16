fbc477801afb01cf547cddd5af9e0d931030ad04
Grant Slater <git@firefishy.com>
2014-12-16 15:16:29 +0000

    Tilecache: Update saphira coss store size

--- roles/saphira.rb
+++ roles/saphira.rb
@@ -24,7 +24,7 @@ default_attributes(
   },
   :squid => {
     :cache_mem => "7500 MB",
-    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+    :cache_dir => "coss /store/squid/coss-01 80000 block-size=8192 max-size=262144 membufs=80"
   },
   :tilecache => {
     :tile_parent => "london.render.openstreetmap.org",
