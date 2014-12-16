37a88ac76aebfb8fe01fae37d7a41bb74f857cea
Grant Slater <git@firefishy.com>
2014-12-16 16:58:26 +0000

    Make stormfly-02 a tile cache

--- roles/stormfly-02.rb
+++ roles/stormfly-02.rb
@@ -11,10 +11,25 @@ default_attributes(
         :address => "140.211.167.105"
       }
     }
+  },
+  :squid => {
+    :cache_mem => "32000 MB",
+    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+  },
+  :tilecache => {
+    :tile_parent => "corvallis.render.openstreetmap.org",
+    :tile_siblings => [
+      "nadder-01.openstreetmap.org",
+      "nadder-02.openstreetmap.org",
+      "jakelong.openstreetmap.org",
+      "nepomuk.openstreetmap.org",
+      "lurien.openstreetmap.org"
+    ]
   }
 )
 
 run_list(
   "role[osuosl]",
-  "role[hp-g6]"
+  "role[hp-g6]",
+  "role[tilecache]"
 )
