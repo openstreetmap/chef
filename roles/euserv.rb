1db8f6a1fdf56f74cc11d9f7bc22d2f762801013
Tom Hughes <tom@compton.nu>
2015-01-23 11:25:34 +0000

    Add role for konqi

---
+++ roles/euserv.rb
@@ -0,0 +1,25 @@
+name "euserv"
+description "Role applied to all servers at EUserv"
+
+default_attributes(
+  :networking => {
+    :nameservers => [
+      "85.31.184.60", "85.31.184.61", "85.31.185.60", "85.31.185.61"
+    ],
+    :roles => {
+      :external => {
+        :zone => "es"
+      }
+    }
+  }
+)
+
+override_attributes(
+  :ntp => {
+    :servers => [ "0.de.pool.ntp.org", "1.de.pool.ntp.org", "europe.pool.ntp.org" ]
+  }
+)
+
+run_list(
+  "role[de]"
+)
---
+++ roles/konqi.rb
@@ -0,0 +1,45 @@
+name "konqi"
+description "Master role applied to konqi"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :external_ipv4 => {
+        :interface => "eth0",
+        :role => :external,
+        :family => :inet,
+        :address => "81.7.11.83",
+        :prefix => "24",
+        :gateway => "81.7.11.1"
+      },
+      :external_ipv6 => {
+        :interface => "eth0",
+        :role => :external,
+        :family => :inet6,
+        :address => "2a02:0180:1:1::551f:bbf6",
+        :prefix => "64",
+        :gateway => "2a02:180:1:1::1"
+      }
+    }
+  },
+  :squid => {
+    :cache_mem => "12500 MB",
+    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+  },
+  :tilecache => {
+    :tile_parent => "falkenstein.render.openstreetmap.org",
+    :tile_siblings => [
+       "trogdor.openstreetmap.org",
+       "nepomuk.openstreetmap.org",
+       "ridgeback.openstreetmap.org",
+       "fume.openstreetmap.org",
+       "gorynych.openstreetmap.org",
+       "simurgh.openstreetmap.org"
+    ]
+  }
+)
+
+run_list(
+  "role[euserv]",
+  "role[tilecache]"
+)
