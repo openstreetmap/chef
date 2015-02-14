26a141f3581baada51b09427d0a9b4576c12b16f
Tom Hughes <tom@compton.nu>
2015-02-14 12:16:39 +0000

    Convert squid_fragment to an LWRP

---
+++ cookbooks/squid/providers/fragment.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: squid
+# Provider:: squid_fragment
+#
+# Copyright 2015, OpenStreetMap Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+def whyrun_supported?
+  true
+end
+
+use_inline_resources
+
+action :create do
+  template fragment_path do
+    source new_resource.template
+    owner "root"
+    group "root"
+    mode 0644
+    variables new_resource.variables
+  end
+end
+
+action :delete do
+  file fragment_path do
+    action :delete
+  end
+end
+
+def fragment_path
+  "/etc/squid/squid.conf.d/#{new_resource.name}.conf"
+end
