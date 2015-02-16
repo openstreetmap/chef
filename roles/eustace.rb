60ea9845c1c961e7ea4bccfa46133aa78bca8d91
Tom Hughes <tom@compton.nu>
2015-02-16 13:56:26 +0000

    Apply hp-g6 role to urmel and eustace

--- roles/eustace.rb
+++ roles/eustace.rb
@@ -22,5 +22,6 @@ default_attributes(
 
 run_list(
   "role[ucl-wolfson]",
+  "role[hp-g6]",
   "role[piwik]"
 )
--- roles/urmel.rb
+++ roles/urmel.rb
@@ -22,6 +22,6 @@ default_attributes(
 
 run_list(
   "role[ucl-wolfson]",
-  "role[hp-g5]",
+  "role[hp-g6]",
   "role[munin]"
 )
