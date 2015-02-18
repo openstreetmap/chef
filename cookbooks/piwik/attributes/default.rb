3a854a9a738132ff8519a289faf5cd9ca2ae6965
Tom Hughes <tom@compton.nu>
2015-02-18 08:56:30 +0000

    Update piwik to 2.11.0

--- cookbooks/piwik/attributes/default.rb
+++ cookbooks/piwik/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:piwik][:version] = "2.10.0"
+default[:piwik][:version] = "2.11.0"
 default[:piwik][:plugins] = %w(
   Actions API BulkTracking Contents CoreAdminHome CoreConsole CoreHome
   CorePluginsAdmin CoreUpdater CoreVisualizations CustomVariables
