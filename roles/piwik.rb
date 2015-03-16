525ab3da99ed14a2bf1ec2969a8a60a43fe96af9
Tom Hughes <tom@compton.nu>
2015-03-16 10:25:29 +0000

    Tune mysql for piwik

--- roles/piwik.rb
+++ roles/piwik.rb
@@ -4,6 +4,15 @@ description "Role applied to all Piwik servers"
 default_attributes(
   :apache => {
     :mpm => "prefork"
+  },
+  :mysql => {
+    :settings => {
+      :mysqld => {
+        :innodb_buffer_pool_instances => "8",
+        :innodb_buffer_pool_size => "16GB",
+        :innodb_flush_log_at_trx_commit => "2"
+      }
+    }
   }
 )
 
