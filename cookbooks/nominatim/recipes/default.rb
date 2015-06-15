6d1e54ea9e1a63b69b52eff117b0c54be427d6fd
Tom Hughes <tom@compton.nu>
2015-06-15 21:10:56 +0100

    Fix foodcritic warning

--- cookbooks/nominatim/recipes/default.rb
+++ cookbooks/nominatim/recipes/default.rb
@@ -278,10 +278,9 @@ directory File.dirname(node[:nominatim][:flatnode_file]) do
   recursive true
 end
 
-if node[:postgresql][:settings][:defaults][:archive_mode] == "on"
-  directory "/data/postgresql-archive" do
-    owner "postgres"
-    group "postgres"
-    mode 0700
-  end
+directory "/data/postgresql-archive" do
+  owner "postgres"
+  group "postgres"
+  mode 0700
+  only_if { node[:postgresql][:settings][:defaults][:archive_mode] == "on" }
 end
