0d8aa995f0a0866e8496dc1161fb1b72f2f6800a
Sarah Hoffmann <lonvia@denofr.de>
2015-07-14 10:16:24 +0200

    increase effective cache size for poldi

--- roles/poldi.rb
+++ roles/poldi.rb
@@ -63,7 +63,7 @@ default_attributes(
         :work_mem => "160MB",
         :maintenance_work_mem => "10GB",
         :random_page_cost => "1.5",
-        :effective_cache_size => "48GB",
+        :effective_cache_size => "70GB",
         :fsync => "on"
       }
     }
