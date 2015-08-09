dc6032dafa208c927c06e301c12a95255cc49b43
Tom Hughes <tom@compton.nu>
2015-08-09 15:31:29 +0100

    Update piwik to 2.14.3

--- cookbooks/piwik/attributes/default.rb
+++ cookbooks/piwik/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:piwik][:version] = "2.14.2"
+default[:piwik][:version] = "2.14.3"
 default[:piwik][:plugins] = %w(
   Actions API BulkTracking Contents CoreAdminHome CoreConsole CoreHome
   CorePluginsAdmin CoreUpdater CoreVisualizations CustomVariables
