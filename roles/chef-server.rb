e6e3452e72e6d11b796aacf1c17900d51cbb1783
Tom Hughes <tom@compton.nu>
2015-09-17 00:14:59 +0100

    Disable chef status alerts for nodes known to be down

--- roles/chef-server.rb
+++ roles/chef-server.rb
@@ -1,6 +1,20 @@
 name "chef-server"
 description "Role applied to all chef servers"
 
+default_attributes(
+  :munin => {
+    :plugins => {
+      :chef_status => {
+        :ascalon => { :warning => ":", :critical => ":" },
+        :idris => { :warning => ":", :critical => ":" },
+        :norbert => { :warning => ":", :critical => ":" },
+        :smaug => { :warning => ":", :critical => ":" },
+        :zark => { :warning => ":", :critical => ":" }
+      }
+    }
+  }
+)
+
 run_list(
   "recipe[chef::server]"
 )
