231b87f7a40ca53113eb08978dbfe71b4b6a0f5f
Tom Hughes <tom@compton.nu>
2015-10-01 17:37:59 +0100

    Add role for grisu

---
+++ roles/grisu.rb
@@ -0,0 +1,33 @@
+name "grisu"
+description "Master role applied to grisu"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :external_ipv4 => {
+        :interface => "em1",
+        :role => :external,
+        :family => :inet,
+        :address => "193.63.75.108",
+        :hwaddress => "d8:d3:85:5d:87:a0"
+      },
+      :external_ipv6 => {
+        :interface => "em1",
+        :role => :external,
+        :family => :inet6,
+        :address => "2001:630:12:500:dad3:85ff:fe5d:a780"
+      },
+      :internal_ipv4 => {
+        :interface => "em2",
+        :role => :internal,
+        :family => :inet,
+        :address => "146.179.159.168",
+        :hwaddress => "d8:d3:85:5d:87:a1"
+      }
+    }
+  }
+)
+
+run_list(
+  "role[ic]"
+)
