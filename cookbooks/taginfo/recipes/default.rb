10d6c4be7d58819dd15d4c064eb2ced47a2536d0
Jochen Topf <jochen@topf.org>
2015-11-15 14:46:08 +0100

    Switch taginfo from old osmium to libosmium.

--- cookbooks/taginfo/recipes/default.rb
+++ cookbooks/taginfo/recipes/default.rb
@@ -98,10 +98,18 @@ node[:taginfo][:sites].each do |site|
     mode 0755
   end
 
-  git "#{directory}/osmium" do
+#  git "#{directory}/osmium" do
+#    action :sync
+#    repository "git://github.com/joto/osmium.git"
+#    revision "osmorg-taginfo-live"
+#    user "taginfo"
+#    group "taginfo"
+#  end
+
+  git "#{directory}/libosmium" do
     action :sync
-    repository "git://github.com/joto/osmium.git"
-    revision "osmorg-taginfo-live"
+    repository "git://github.com/osmcode/libosmium.git"
+    revision "v2.5.2"
     user "taginfo"
     group "taginfo"
   end
@@ -131,8 +139,10 @@ node[:taginfo][:sites].each do |site|
     settings["sources"]["db"]["planetfile"] = "#{directory}/planet/planet.pbf"
     settings["sources"]["db"]["bindir"] = "#{directory}/taginfo/tagstats"
     settings["sources"]["db"]["tagstats"] = "#{directory}/taginfo/tagstats/tagstats"
-    settings["tagstats"]["cxxflags"] = "-I../../osmium/include"
-    settings["tagstats"]["geodistribution"] = "MmapAnon"
+#    settings["tagstats"]["cxxflags"] = "-I../../osmium/include"
+    settings["tagstats"]["cxxflags"] = "-I../../libosmium/include"
+#    settings["tagstats"]["geodistribution"] = "MmapAnon"
+    settings["tagstats"]["geodistribution"] = "DenseMmapArray"
     settings["user_interface"]["key_page"]["show_tab_similar"] = true
     settings["level0"]["overpass_url_prefix"] = "http://overpass-api.de/api/interpreter?"
 
@@ -153,7 +163,8 @@ node[:taginfo][:sites].each do |site|
     cwd "#{directory}/taginfo/tagstats"
     user "taginfo"
     group "taginfo"
-    subscribes :run, "git[#{directory}/osmium]"
+#    subscribes :run, "git[#{directory}/osmium]"
+    subscribes :run, "git[#{directory}/libosmium]"
     subscribes :run, "git[#{directory}/taginfo]"
     notifies :restart, "service[apache2]"
   end
