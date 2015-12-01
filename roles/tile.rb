cbc9b2fd64975c83280a0d80b2d8513a5465bbcb
Tom Hughes <tom@compton.nu>
2015-12-01 11:31:06 +0000

    Update carto stylesheet to v2.37.1
    Fixes performance issue with low zoom boundary queries.

--- roles/tile.rb
+++ roles/tile.rb
@@ -84,7 +84,7 @@ default_attributes(
     :styles => {
       :default => {
         :repository => "git://github.com/gravitystorm/openstreetmap-carto.git",
-        :revision => "v2.37.0",
+        :revision => "v2.37.1",
         :max_zoom => 19
       }
     }
