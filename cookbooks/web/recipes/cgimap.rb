14f19195bcaa9b3dd001db87d2df28b74667c437
Tom Hughes <tom@compton.nu>
2016-05-06 22:43:28 +0100

    Revert to original path to cgimap executable
    This reverts commit 1b6728246580546cc65a35ca414134a3886d9508.

--- cookbooks/web/recipes/cgimap.rb
+++ cookbooks/web/recipes/cgimap.rb
@@ -96,7 +96,7 @@ cgimap_init = edit_file "#{cgimap_directory}/scripts/cgimap.init" do |line|
   line.gsub!(/^CGIMAP_MEMCACHE=.*;/, "CGIMAP_MEMCACHE=rails1,rails2,rails3;")
   line.gsub!(/^CGIMAP_RATELIMIT=.*;/, "CGIMAP_RATELIMIT=204800;")
 
-  line.gsub!(%r{/home/rails/bin/map}, "#{cgimap_directory}/.libs/lt-map")
+  line.gsub!(%r{/home/rails/bin/map}, "#{cgimap_directory}/map")
 
   if database_readonly
     line.gsub!(/--daemon/, "--daemon --readonly")
