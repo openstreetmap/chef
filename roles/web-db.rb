ba2374aa0a7dc5c971ebda99f4c6bc19d37c18d2
Tom Hughes <tom@compton.nu>
2016-05-13 14:15:58 +0100

    Move readonly load to katla

--- roles/web-db.rb
+++ roles/web-db.rb
@@ -4,6 +4,6 @@ description "Role applied to all servers needing to find the main database"
 default_attributes(
   :web => {
     :database_host => "katla.bm.openstreetmap.org",
-    :readonly_database_host => "ramoth.ic.openstreetmap.org"
+    :readonly_database_host => "katla.bm.openstreetmap.org"
   }
 )
