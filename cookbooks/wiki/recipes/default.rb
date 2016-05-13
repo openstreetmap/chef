19362a202464253b297f1bbe5d8812d3c10ce38f
Grant Slater <grant.slater@wearefriday.com>
2016-05-13 17:13:39 +0100

    Add wiki site_readonly notice

--- cookbooks/wiki/recipes/default.rb
+++ cookbooks/wiki/recipes/default.rb
@@ -52,7 +52,7 @@ mediawiki_site "wiki.openstreetmap.org" do
   recaptcha_public_key "6LdFIQATAAAAAMwtHeI8KDgPqvRbXeNYSq1gujKz"
   recaptcha_private_key passwords["recaptcha"]
 
-  # site_readonly "MAINTENANCE UPDATE: WIKI READ-ONLY. ETA: Tuesday 8:00am UTC/GMT."
+  site_readonly "MAINTENANCE: WIKI READ-ONLY UNTIL Monday 16 May 2016 - 11:00am UTC/GMT."
 end
 
 cookbook_file "/srv/wiki.openstreetmap.org/osm_logo_wiki.png" do
