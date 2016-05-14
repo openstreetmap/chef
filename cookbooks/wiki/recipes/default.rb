5038552aa329db50d1710c9fb500217c640b287f
Grant Slater <git@firefishy.com>
2016-05-14 05:39:43 +0100

    Add wiki site notice support

--- cookbooks/wiki/recipes/default.rb
+++ cookbooks/wiki/recipes/default.rb
@@ -52,6 +52,7 @@ mediawiki_site "wiki.openstreetmap.org" do
   recaptcha_public_key "6LdFIQATAAAAAMwtHeI8KDgPqvRbXeNYSq1gujKz"
   recaptcha_private_key passwords["recaptcha"]
 
+  site_notice "MAINTENANCE: WIKI READ-ONLY UNTIL Monday 16 May 2016 - 11:00am UTC/GMT."
   site_readonly "MAINTENANCE: WIKI READ-ONLY UNTIL Monday 16 May 2016 - 11:00am UTC/GMT."
 end
 
