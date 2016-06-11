70e190d5bcb968129f5984b360d28f7bfefc5a3a
Grant Slater <git@firefishy.com>
2016-06-11 19:59:56 +0100

    imagery: ensure mapserver is restarted when required

--- cookbooks/imagery/resources/layer.rb
+++ cookbooks/imagery/resources/layer.rb
@@ -73,16 +73,18 @@ action :create do
     user "imagery"
     group "imagery"
     exec_start_pre "/bin/rm -f /run/mapserver-fastcgi/layer-#{layer}.socket"
-    exec_start "/usr/bin/spawn-fcgi -s /run/mapserver-fastcgi/layer-#{layer}.socket -M 0666 -P /run/mapserver-fastcgi/layer-#{layer}.pid -- /usr/bin/multiwatch -f 4 --signal=TERM -- /usr/lib/cgi-bin/mapserv"
+    exec_start "/usr/bin/spawn-fcgi -s /run/mapserver-fastcgi/layer-#{layer}.socket -M 0666 -P /run/mapserver-fastcgi/layer-#{layer}.pid -- /usr/bin/multiwatch -f 6 --signal=TERM -- /usr/lib/cgi-bin/mapserv"
     pid_file "/run/mapserver-fastcgi/layer-#{layer}.pid"
     type "forking"
     restart "always"
   end
 
   service "mapserv-fcgi-#{layer}" do
+    provider Chef::Provider::Service::Systemd
     action [:enable, :start]
+    supports :status => true, :restart => true, :reload => false
     subscribes :restart, "template[/srv/imagery/mapserver/layer-#{layer}.map]"
-    subscribes :restart, "systemd_service[mapserv-fcgi]"
+    subscribes :restart, "systemd_service[mapserv-fcgi-#{layer}]"
   end
 
   directory "/srv/imagery/nginx/#{site}" do
