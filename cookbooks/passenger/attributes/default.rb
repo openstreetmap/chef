42c5272f61028275c79d973ab0a85b05e043c8da
Tom Hughes <tom@compton.nu>
2016-07-04 09:10:02 +0100

    Drop brightbox-ruby-ng repo from passenger requirements

--- cookbooks/passenger/attributes/default.rb
+++ cookbooks/passenger/attributes/default.rb
@@ -2,4 +2,4 @@ default[:passenger][:ruby_version] = "2.3"
 default[:passenger][:max_pool_size] = 6
 default[:passenger][:pool_idle_time] = 300
 
-default[:apt][:sources] = node[:apt][:sources] | ["brightbox-ruby-ng", "passenger"]
+default[:apt][:sources] = node[:apt][:sources] | ["passenger"]
