a661370166bbac0bbe8c8b1f0ba947e3a269edee
Tom Hughes <tom@compton.nu>
2016-08-03 09:30:41 +0100

    Avoid hypens in munin variable names for composite graphs

--- cookbooks/munin/libraries/expand.rb
+++ cookbooks/munin/libraries/expand.rb
@@ -3,9 +3,13 @@ class Chef
     def self.expand(template, nodes)
       nodes.map do |node|
         if node.is_a?(Hash)
-          template.gsub(/%%([^%]+)%%/) { node[Regexp.last_match[1].to_sym] }
+          template
+            .gsub(/%%%([^%]+)%%%/) { node[Regexp.last_match[1].to_sym].tr("-", "_") }
+            .gsub(/%%([^%]+)%%/) { node[Regexp.last_match[1].to_sym] }
         else
-          template.gsub("%%%", node.tr("-", "_")).gsub("%%", node)
+          template
+            .gsub("%%%", node.tr("-", "_"))
+            .gsub("%%", node)
         end
       end.join(" ")
     end
