6a6563b55fb0318323d067d82d0031f578bff68a
Tom Hughes <tom@compton.nu>
2016-08-05 19:44:04 +0100

    Update URL

--- cookbooks/wordpress/recipes/default.rb
+++ cookbooks/wordpress/recipes/default.rb
@@ -31,7 +31,7 @@ apache_module "php5"
 apache_module "rewrite"
 
 fail2ban_filter "wordpress" do
-  source "http://plugins.svn.wordpress.org/wp-fail2ban/trunk/wordpress-hard.conf"
+  source "https://plugins.svn.wordpress.org/wp-fail2ban/trunk/filters.d/wordpress-hard.conf"
 end
 
 fail2ban_jail "wordpress" do
