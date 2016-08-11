944654857b1410513d996c16eb278db9da05cf22
Andy Allan <git@gravitystorm.co.uk>
2016-08-11 15:29:34 +0100

    Add a serverspec test to ensure the munin-node service is running.

---
+++ test/integration/munin/serverspec/munin_node_spec.rb
@@ -0,0 +1,10 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe "munin-node daemon" do
+  it "has a running service of munin-node" do
+    expect(service("munin-node")).to be_running
+  end
+end
