52e55367b5e2fa9a849d0155e011171da010c604
Tom Hughes <tom@compton.nu>
2016-08-14 22:29:50 +0100

    Use numbers for numeric attributes

--- cookbooks/tilecache/attributes/default.rb
+++ cookbooks/tilecache/attributes/default.rb
@@ -3,12 +3,12 @@ default[:tilecache][:tile_parent] = "parent.tile.openstreetmap.org"
 default[:tilecache][:tile_siblings] = []
 
 # Per IP bucket refill rate
-default[:tilecache][:ip_bucket_refill] = "4096"
+default[:tilecache][:ip_bucket_refill] = 4096
 # Per IP bucket size
-default[:tilecache][:ip_bucket_size] = "67108864"
+default[:tilecache][:ip_bucket_size] = 67108864
 # Per Class C refill rate
-default[:tilecache][:net_bucket_refill] = "8192"
+default[:tilecache][:net_bucket_refill] = 8192
 # Per Class C bucket size
-default[:tilecache][:net_bucket_size] = "134217728"
+default[:tilecache][:net_bucket_size] = 134217728
 
 default[:tilecache][:ssl][:certificate] = "tile.openstreetmap"
