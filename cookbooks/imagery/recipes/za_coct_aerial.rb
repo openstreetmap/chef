ad4da3b5b4a92d20a0904b005b86afae4a492f86
Grant Slater <git@firefishy.com>
2016-08-25 23:41:00 +0100

    imagery: remove broken coct 2013 imagery

--- cookbooks/imagery/recipes/za_coct_aerial.rb
+++ cookbooks/imagery/recipes/za_coct_aerial.rb
@@ -35,11 +35,11 @@ imagery_layer "za_coct_aerial_2015" do
   max_zoom 21
 end
 
-imagery_layer "za_coct_aerial_2013" do
-  projection "EPSG:3857"
-  site "coct.aerial.openstreetmap.org.za"
-  title "City of Cape Town Aerial - 2013 (OSM USE ONLY)"
-  source "/data/imagery/za/coct/2013-tif-epsg-3857.vrt"
-  copyright 'Copyright &copy; 2013 <a href="https://www.capetown.gov.za/">City of Cape Town</a>'
-  max_zoom 21
-end
+# imagery_layer "za_coct_aerial_2013" do
+#   projection "EPSG:3857"
+#   site "coct.aerial.openstreetmap.org.za"
+#   title "City of Cape Town Aerial - 2013 (OSM USE ONLY)"
+#   source "/data/imagery/za/coct/2013-tif-epsg-3857.vrt"
+#   copyright 'Copyright &copy; 2013 <a href="https://www.capetown.gov.za/">City of Cape Town</a>'
+#   max_zoom 21
+# end
