6bf6ab25dc9025aaa2b5899e608a9f1ff309b345
Grant Slater <git@firefishy.com>
2016-08-31 13:31:50 +0100

    imagery: update coct config

--- cookbooks/imagery/recipes/za_coct_aerial.rb
+++ cookbooks/imagery/recipes/za_coct_aerial.rb
@@ -29,8 +29,10 @@ imagery_layer "za_coct_aerial_2015" do
   projection "EPSG:3857"
   site "coct.aerial.openstreetmap.org.za"
   title "City of Cape Town Aerial - 2015 (OSM USE ONLY)"
-  source "/data/imagery/za/coct/2015-epsg-3857-fast.tif"
+  source "/data/imagery/za/coct/2015-epsg-3857.tif"
   copyright 'Copyright &copy; 2015 <a href="https://www.capetown.gov.za/">City of Cape Town</a>'
+  background_colour "0 0 0"
+  extension "jpeg"
   default_layer true
   max_zoom 21
 end
