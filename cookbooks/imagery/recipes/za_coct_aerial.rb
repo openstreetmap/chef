0a2263e8f744265c000a10a93a5e59dc5c78389a
Grant Slater <git@firefishy.com>
2016-09-07 08:21:27 +0100

    imagery: coct new drift test

--- cookbooks/imagery/recipes/za_coct_aerial.rb
+++ cookbooks/imagery/recipes/za_coct_aerial.rb
@@ -41,7 +41,7 @@ imagery_layer "za_coct_aerial_2015_drift" do
   projection "EPSG:3857"
   site "coct.aerial.openstreetmap.org.za"
   title "City of Cape Town Aerial - 2015 (DRIFT TEST) (OSM USE ONLY)"
-  source "/data/imagery/za/coct/2015-drift-epsg-3857.tif"
+  source "/data/imagery/za/coct/2015-drift1-epsg-3857.tif"
   copyright 'Copyright &copy; 2015 <a href="https://www.capetown.gov.za/">City of Cape Town</a>'
   background_colour "0 0 0"
   extension "jpeg"
