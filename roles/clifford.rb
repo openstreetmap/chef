5f6cd608cf149416e388446a6334043454bd7639
Tom Hughes <tom@compton.nu>
2016-10-17 13:36:38 +0100

    Update interface names for clifford

--- roles/clifford.rb
+++ roles/clifford.rb
@@ -5,13 +5,13 @@ default_attributes(
   :networking => {
     :interfaces => {
       :internal_ipv4 => {
-        :interface => "eth0",
+        :interface => "enp2s0f0",
         :role => :internal,
         :family => :inet,
         :address => "10.0.0.17"
       },
       :external_ipv4 => {
-        :interface => "eth1",
+        :interface => "enp2s0f1",
         :role => :external,
         :family => :inet,
         :address => "128.40.45.194"
