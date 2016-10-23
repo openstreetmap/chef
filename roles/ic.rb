39307e77ab75fd8594f9bf4847b7b546d0598efa
Tom Hughes <tom@compton.nu>
2016-10-23 10:18:36 +0100

    Switch IC machines to use karm for readonly queries

--- roles/ic.rb
+++ roles/ic.rb
@@ -32,7 +32,7 @@ default_attributes(
   :web => {
     :backends => %w(rails1 rails2 rails3),
     :fileserver => "ironbelly",
-    :readonly_database_host => "ramoth.ic.openstreetmap.org"
+    :readonly_database_host => "karm.ic.openstreetmap.org"
   }
 )
 
