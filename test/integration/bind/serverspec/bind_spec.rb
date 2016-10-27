9fa2ae0afe39b9b1b4753de674bffb31f69e5530
Tom Hughes <tom@compton.nu>
2016-10-27 10:20:06 +0100

    Add a few tests

--- test/integration/bind/serverspec/bind_spec.rb
+++ test/integration/bind/serverspec/bind_spec.rb
@@ -11,3 +11,7 @@ describe service("bind9") do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe port(53) do
+  it { should be_listening }
+end
--- test/integration/munin/serverspec/munin_node_spec.rb
+++ test/integration/munin/serverspec/munin_node_spec.rb
@@ -3,8 +3,15 @@ require "serverspec"
 # Required by serverspec
 set :backend, :exec
 
-describe "munin-node daemon" do
-  it "has a running service of munin-node" do
-    expect(service("munin-node")).to be_running
-  end
+describe package("munin-node") do
+  it { should be_installed }
+end
+
+describe service("munin-node") do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(4949) do
+  it { should be_listening }
 end
