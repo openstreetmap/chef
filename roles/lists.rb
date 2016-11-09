2d2235ed1e13caf69cd00aca9576b99f5f7ddf2d
Tom Hughes <tom@compton.nu>
2016-11-09 10:38:09 +0000

    Discard bounces to mailman-loop

--- roles/lists.rb
+++ roles/lists.rb
@@ -1,6 +1,14 @@
 name "lists"
 description "Role applied to all mailing list servers"
 
+default_attributes(
+  :exim => {
+    :aliases => {
+      "mailman-loop" => "/dev/null"
+    }
+  }
+)
+
 run_list(
   "recipe[mailman]"
 )
