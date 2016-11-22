84747961ab8bfee2f241049b4f1e3317da233190
Tom Hughes <tom@compton.nu>
2016-11-22 19:50:05 +0000

    Tune apache on web frontends

--- roles/web-frontend.rb
+++ roles/web-frontend.rb
@@ -5,12 +5,13 @@ default_attributes(
   :apache => {
     :mpm => "event",
     :event => {
-      :server_limit => 40,
+      :server_limit => 20,
       :max_request_workers => 1000,
-      :min_spare_threads => 50,
-      :max_spare_threads => 150,
       :threads_per_child => 50,
-      :max_connections_per_child => 10000
+      :min_spare_threads => 50,
+      :max_spare_threads => 450,
+      :max_connections_per_child => 10000,
+      :async_request_worker_factor => 4
     }
   },
   :logstash => {
