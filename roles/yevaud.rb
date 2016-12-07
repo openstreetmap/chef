5e9999b016797b0d2950481247e5e5208dd9a06e
Tom Hughes <tom@compton.nu>
2016-12-07 18:16:52 +0000

    Update interface names for errol and yevaud

--- roles/yevaud.rb
+++ roles/yevaud.rb
@@ -67,13 +67,13 @@ default_attributes(
   :networking => {
     :interfaces => {
       :internal_ipv4 => {
-        :interface => "eth1.2801",
+        :interface => "eth0.2801",
         :role => :internal,
         :family => :inet,
         :address => "10.0.0.15"
       },
       :external_ipv4 => {
-        :interface => "eth1.2800",
+        :interface => "eth0.2800",
         :role => :external,
         :family => :inet,
         :address => "193.60.236.22"
