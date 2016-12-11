160ec7aacb790ddad193cd5f6caf2a2102cd5e9a
Tom Hughes <tom@compton.nu>
2016-12-11 22:26:52 +0000

    Add roles for tiamat nodes

---
+++ roles/tiamat-01.rb
@@ -0,0 +1,25 @@
+name "tiamat-01"
+description "Master role applied to tiamat-01"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal_ipv4 => {
+        :interface => "enp1s0.2801",
+        :role => :internal,
+        :family => :inet,
+        :address => "10.0.0.41"
+      },
+      :external_ipv4 => {
+        :interface => "enp1s0.2800",
+        :role => :external,
+        :family => :inet,
+        :address => "193.60.236.41"
+      }
+    }
+  }
+)
+
+run_list(
+  "role[ucl]"
+)
