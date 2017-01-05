80d41d39e7490be63e81aebbca4987e4ff12f90d
Tom Hughes <tom@compton.nu>
2017-01-05 00:22:15 +0000

    Add HTML/JS/CSS for gps.tile.openstreetmap.org site

---
+++ cookbooks/gps-tile/files/default/html/map.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  // Create a map
+  var map = L.map("map");
+
+  // Add GPS tile layer
+  L.tileLayer("//gps-{s}.tile.openstreetmap.org/gps-lines/tile/{z}/{x}/{y}.png", {
+    attribution: "© <a target=\"_parent\" href=\"http://www.openstreetmap.org\">OpenStreetMap</a> and contributors, under an <a target=\"_parent\" href=\"http://www.openstreetmap.org/copyright\">open license</a>",
+    maxZoom: 18
+  }).addTo(map);
+
+  // SHow the whole world
+  map.fitWorld();
+});
