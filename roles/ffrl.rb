0ac33d2bf7e6129f0f7d6a90f27c76c022414be0
Tom Hughes <tom@compton.nu>
2017-01-07 17:27:45 +0000

    Add role for kalessin

---
+++ roles/ffrl.rb
@@ -0,0 +1,24 @@
+name "ffrl"
+description "Role applied to all servers at Freifunk Rheinland"
+
+default_attributes(
+  :hosted_by => "Freifunk Rheinland",
+  :location => "Berlin, Germany",
+  :networking => {
+    :roles => {
+      :external => {
+        :zone => "ffr"
+      }
+    }
+  }
+)
+
+override_attributes(
+  :ntp => {
+    :servers => ["0.de.pool.ntp.org", "1.de.pool.ntp.org", "europe.pool.ntp.org"]
+  }
+)
+
+run_list(
+  "role[de]"
+)
---
+++ roles/kalessin.rb
@@ -0,0 +1,46 @@
+name "kalessin"
+description "Master role applied to kalessin"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :external_ipv4 => {
+        :interface => "eth0",
+        :role => :external,
+        :family => :inet,
+        :address => "185.66.195.245",
+        :prefix => "28",
+        :gateway => "185.66.195.241"
+      },
+      :external_ipv6 => {
+        :interface => "eth0",
+        :role => :external,
+        :family => :inet6,
+        :address => "2a03:2260:2000:1::5",
+        :prefix => "64",
+        :gateway => "2a03:2260:2000:1::1"
+      }
+    }
+  },
+  :squid => {
+    :cache_mem => "12500 MB",
+    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+  },
+  :tilecache => {
+    :tile_parent => "germany.render.openstreetmap.org",
+    :tile_siblings => [
+      "konqi.openstreetmap.org",
+      "trogdor.openstreetmap.org",
+      "nepomuk.openstreetmap.org",
+      "ridgeback.openstreetmap.org",
+      "gorynych.openstreetmap.org",
+      "simurgh.openstreetmap.org"
+    ]
+  }
+)
+
+run_list(
+  "role[ffrl]",
+  "role[geodns]",
+  "role[tilecache]"
+)
