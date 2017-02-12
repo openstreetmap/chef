4a446ba563419ea9589df9ef5db84d9867bae8e0
Tom Hughes <tom@compton.nu>
2017-02-12 11:14:40 +0000

    Update civicrm and extensions to latest versions

--- cookbooks/civicrm/attributes/default.rb
+++ cookbooks/civicrm/attributes/default.rb
@@ -1,21 +1,21 @@
-default[:civicrm][:version] = "4.6.26"
+default[:civicrm][:version] = "4.7.16"
 
 default[:civicrm][:extensions][:cividiscount][:name] = "org.civicrm.module.cividiscount"
 default[:civicrm][:extensions][:cividiscount][:repository] = "git://github.com/dlobo/org.civicrm.module.cividiscount.git"
-default[:civicrm][:extensions][:cividiscount][:revision] = "3.2"
+default[:civicrm][:extensions][:cividiscount][:revision] = "3.3"
 
 default[:civicrm][:extensions][:osm][:name] = "de.systopia.osm"
 default[:civicrm][:extensions][:osm][:repository] = "git://github.com/systopia/de.systopia.osm.git"
-default[:civicrm][:extensions][:osm][:revision] = "1.1"
+default[:civicrm][:extensions][:osm][:revision] = "1.2"
 
 default[:civicrm][:extensions][:civirules][:name] = "org.civicoop.civirules"
 default[:civicrm][:extensions][:civirules][:repository] = "git://github.com/CiviCooP/org.civicoop.civirules.git"
-default[:civicrm][:extensions][:civirules][:revision] = "1.2"
+default[:civicrm][:extensions][:civirules][:revision] = "1.11"
 
 default[:civicrm][:extensions][:stripe][:name] = "com.drastikbydesign.stripe"
 default[:civicrm][:extensions][:stripe][:repository] = "git://github.com/drastik/com.drastikbydesign.stripe.git"
-default[:civicrm][:extensions][:stripe][:revision] = "1.9.1"
+default[:civicrm][:extensions][:stripe][:revision] = "4.7.1"
 
 default[:civicrm][:extensions][:mailchimp][:name] = "uk.co.vedaconsulting.mailchimp"
 default[:civicrm][:extensions][:mailchimp][:repository] = "git://github.com/veda-consulting/uk.co.vedaconsulting.mailchimp.git"
-default[:civicrm][:extensions][:mailchimp][:revision] = "v1.8.6"
+default[:civicrm][:extensions][:mailchimp][:revision] = "v2.0"
