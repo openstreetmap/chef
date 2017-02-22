7eeba28595f0089249c006550eab941e9f1521b3
Tom Hughes <tom@compton.nu>
2017-02-22 20:21:30 +0000

    Update subversion provider monkey patching

--- cookbooks/chef/libraries/subversion.rb
+++ cookbooks/chef/libraries/subversion.rb
@@ -7,11 +7,11 @@ class Chef
 
       def sync_command
         if current_repository_matches_target_repository?
-          c = scm :update, @new_resource.svn_arguments, verbose, authentication, "-r#{revision_int}", @new_resource.destination
-          Chef::Log.debug "#{@new_resource} updated working copy #{@new_resource.destination} to revision #{@new_resource.revision}"
+          c = scm :update, new_resource.svn_arguments, verbose, authentication, proxy, "-r#{revision_int}", new_resource.destination
+          Chef::Log.debug "#{new_resource} updated working copy #{new_resource.destination} to revision #{new_resource.revision}"
         else
-          c = scm :switch, @new_resource.svn_arguments, verbose, authentication, "-r#{revision_int}", @new_resource.repository, @new_resource.destination
-          Chef::Log.debug "#{@new_resource} updated working copy #{@new_resource.destination} to #{@new_resource.repository} revision #{@new_resource.revision}"
+          c = scm :switch, new_resource.svn_arguments, verbose, authentication, proxy, "-r#{revision_int}", new_resource.repository, new_resource.destination
+          Chef::Log.debug "#{new_resource} updated working copy #{new_resource.destination} to #{new_resource.repository} revision #{new_resource.revision}"
         end
         c
       end
@@ -21,24 +21,27 @@ class Chef
       end
 
       def current_repository_matches_target_repository?
-        !current_repository.nil? && (@new_resource.repository == current_repository)
+        !current_repository.nil? && (new_resource.repository == current_repository)
       end
 
       def repo_attrs
-        return {} unless ::File.exist?(::File.join(@new_resource.destination, ".svn"))
+        return {} unless ::File.exist?(::File.join(new_resource.destination, ".svn"))
 
         @repo_attrs ||= svn_info.lines.each_with_object({}) do |line, attrs|
-          raise "Could not parse `svn info` data: #{line}" unless line =~ SVN_INFO_PATTERN
-
+          next unless line =~ SVN_INFO_PATTERN
           property = Regexp.last_match[1]
           value = Regexp.last_match[2]
           attrs[property] = value
         end
+
+        raise "Could not parse `svn info` data: #{svn_info}" if @repo_attrs.empty?
+
+        @repo_attrs
       end
 
       def svn_info
         command = scm(:info)
-        shell_out!(command, run_options(:cwd => cwd)).stdout
+        shell_out!(command, run_options(:cwd => cwd, :returns => [0, 1])).stdout
       end
     end
   end
