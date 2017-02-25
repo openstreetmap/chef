e3956086c7d1f49f5ed58ce014741dfca2505990
Tom Hughes <tom@compton.nu>
2017-02-25 17:48:25 +0000

    Bring mysqld settings for mediawiki machines under chef control

--- roles/foundation.rb
+++ roles/foundation.rb
@@ -23,6 +23,18 @@ default_attributes(
     :memory_limit => 400,
     :chunk_growth_factor => 1.05,
     :min_item_size => 5
+  },
+  :mysql => {
+    :settings => {
+      :mysqld => {
+        :innodb_buffer_pool_size => "512M",
+        :key_buffer_size => "64M",
+        :max_connections => "200",
+        :query_cache_size => "48M",
+        :sort_buffer_size => "8M",
+        :tmp_table_size => "48M"
+      }
+    }
   }
 )
 
--- roles/wiki.rb
+++ roles/wiki.rb
@@ -7,6 +7,16 @@ default_attributes(
       :wiki => { :status => :role }
     }
   },
+  :apache => {
+    :mpm => "prefork",
+    :timeout => 30,
+    :event => {
+      :server_limit => 32,
+      :max_request_workers => 800,
+      :threads_per_child => 50,
+      :max_connections_per_child => 10000
+    }
+  },
   :elasticsearch => {
     :version => "2.x",
     :cluster => {
@@ -35,14 +45,16 @@ default_attributes(
     :chunk_growth_factor => 1.05,
     :min_item_size => 5
   },
-  :apache => {
-    :mpm => "prefork",
-    :timeout => 30,
-    :event => {
-      :server_limit => 32,
-      :max_request_workers => 800,
-      :threads_per_child => 50,
-      :max_connections_per_child => 10000
+  :mysql => {
+    :settings => {
+      :mysqld => {
+        :innodb_buffer_pool_size => "4G",
+        :key_buffer_size => "64M",
+        :max_connections => "200",
+        :query_cache_size => "256M",
+        :sort_buffer_size => "8M",
+        :tmp_table_size => "128M"
+      }
     }
   }
 )
