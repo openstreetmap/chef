1ae223cfae9257cff7c851bd0f0c4d403986c04f
Sarah Hoffmann <lonvia@denofr.de>
2017-09-16 16:07:45 +0200

    disable automatic git updates on dulcy

--- roles/dulcy.rb
+++ roles/dulcy.rb
@@ -42,6 +42,7 @@ default_attributes(
   :nominatim => {
     :state => "standalone",
     :enable_backup => false,
+    :enable_git_updates => false,
     :dbadmins => %w[lonvia tomh],
     :dbcluster => "9.6/main",
     :flatnode_file => "/ssd/nominatim/nodes.store",
