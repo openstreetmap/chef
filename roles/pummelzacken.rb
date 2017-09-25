23b449a94ef4ffc4746775f6a6d6b88c8a1c46c9
Sarah Hoffmann <lonvia@denofr.de>
2017-09-25 23:55:18 +0200

    put pummelzacken back into update mode

--- roles/pummelzacken.rb
+++ roles/pummelzacken.rb
@@ -27,7 +27,7 @@ default_attributes(
         :maintenance_work_mem => "10GB",
         :random_page_cost => "1.5",
         :effective_cache_size => "60GB",
-        :fsync => "off"
+        :fsync => "on"
       }
     }
   },
