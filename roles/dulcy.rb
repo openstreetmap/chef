60792731668f9c32fa491c2bc8f5490ac4125fa0
Sarah Hoffmann <lonvia@denofr.de>
2017-10-19 22:09:48 +0200

    increase fpm pool sizes on dulcy

--- roles/dulcy.rb
+++ roles/dulcy.rb
@@ -58,6 +58,14 @@ default_attributes(
       "isearch" => "/ssd/tablespaces/isearch",
       "daux" => "/ssd/tablespaces/daux",
       "iaux" => "/ssd/tablespaces/iaux"
+    },
+    :fpm_pools => {
+      :www => {
+        :max_children => "120"
+      },
+      :bulk => {
+        :max_children => "20"
+      }
     }
   },
   :sysfs => {
