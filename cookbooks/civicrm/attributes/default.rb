711ff014c1eadd4a74fdc20ba5a300b5ed96e531
Tom Hughes <tom@compton.nu>
2018-03-20 11:23:10 +0000

    Update CiviCRM to 4.7.31

--- cookbooks/civicrm/attributes/default.rb
+++ cookbooks/civicrm/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:civicrm][:version] = "4.7.30"
+default[:civicrm][:version] = "4.7.31"
 
 default[:civicrm][:extensions][:cividiscount][:name] = "org.civicrm.module.cividiscount"
 default[:civicrm][:extensions][:cividiscount][:repository] = "git://github.com/dlobo/org.civicrm.module.cividiscount.git"
