b9aeb71952c933c6179c21448ce14c02e5d5fe5d
Tom Hughes <tom@compton.nu>
2018-05-09 15:45:58 +0100

    Update piwik to 3.5.0

--- cookbooks/piwik/attributes/default.rb
+++ cookbooks/piwik/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:piwik][:version] = "3.4.0"
+default[:piwik][:version] = "3.5.0"
 default[:piwik][:plugins] = %w[
   Actions API BulkTracking Contents CoreAdminHome CoreConsole CoreHome
   CorePluginsAdmin CoreUpdater CoreVisualizations CustomVariables
