5fff4b6033b48783961460aaa0963cc3a2ef9322
Tom Hughes <tom@compton.nu>
2018-05-10 08:42:18 +0100

    Update piwiki plugin list

--- cookbooks/piwik/attributes/default.rb
+++ cookbooks/piwik/attributes/default.rb
@@ -1,11 +1,13 @@
 default[:piwik][:version] = "3.5.0"
 default[:piwik][:plugins] = %w[
-  Actions API BulkTracking Contents CoreAdminHome CoreConsole CoreHome
-  CorePluginsAdmin CoreUpdater CoreVisualizations CustomVariables
-  Dashboard DevicesDetection DevicePlugins DoNotTrack Events Feedback Goals
-  Heartbeat ImageGraph Installation LanguagesManager Live Login Morpheus
-  MultiSites Overlay PrivacyManager Provider Proxy Referrers Resolution
-  SegmentEditor SEO SitesManager Transitions UserCountry UserCountryMap
-  UserLanguage UsersManager Widgetize VisitFrequency VisitorInterest
-  VisitsSummary VisitTime
+  Actions Annotations API BulkTracking Contents CoreAdminHome CoreConsole
+  CoreHome CorePluginsAdmin CoreUpdater CoreVisualizations CustomPiwikJs
+  CustomVariables Dashboard DevicePlugins DevicesDetection Diagnostics Ecommerce
+  Events Feedback GeoIp2 Goals Heartbeat ImageGraph Insights Installation Intl
+  LanguagesManager Live Login Marketplace MobileMessaging Monolog Morpheus
+  MultiSites Overlay PrivacyManager ProfessionalServices Provider Proxy
+  Referrers Resolution RssWidget ScheduledReports SegmentEditor SEO SitesManager
+  Transitions UserCountry UserCountryMap UserId UserLanguage UsersManager
+  VisitFrequency VisitorInterest VisitsSummary VisitTime WebsiteMeasurable
+  Widgetize
 ]
