6ca293f3364cfa26435780dfa6081e13a92b1da0
Tom Hughes <tom@compton.nu>
2018-06-07 19:18:48 +0100

    Update culebre configuration for squid 3

--- roles/culebre.rb
+++ roles/culebre.rb
@@ -22,8 +22,9 @@ default_attributes(
     }
   },
   :squid => {
+    :version => "3",
     :cache_mem => "6100 MB",
-    :cache_dir => "coss /store/squid/coss-01 80000 block-size=8192 max-size=262144 membufs=80"
+    :cache_dir => "rock /store/squid/rock-01 80000 swap-timeout=500 max-swap-rate=150 slot-size=4096 max-size=262144"
   },
   :tilecache => {
     :tile_parent => "zaragoza.render.openstreetmap.org",
