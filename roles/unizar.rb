a10b5842ffa385a2016558680e82e0a7caf17c12
Tom Hughes <tom@compton.nu>
2018-06-26 23:32:28 +0100

    Set sensible time servers for Spanish cache

--- roles/unizar.rb
+++ roles/unizar.rb
@@ -19,6 +19,12 @@ default_attributes(
   }
 )
 
+override_attributes(
+  :ntp => {
+    :servers => ["0.es.pool.ntp.org", "1.es.pool.ntp.org", "europe.pool.ntp.org"]
+  }
+)
+
 run_list(
   "role[es]"
 )
