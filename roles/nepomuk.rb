a7f8529fd91f04909fd7c9fde1ea843bfbbbf09c
Tom Hughes <tom@compton.nu>
2018-06-28 18:14:29 +0100

    Remove tuning of vda scheduler of nepomuk

--- roles/nepomuk.rb
+++ roles/nepomuk.rb
@@ -53,8 +53,7 @@ default_attributes(
     :hdd_tune => {
       :comment => "Tune the queue for improved performance",
       :parameters => {
-        "block/vda/queue/nr_requests" => "128",
-        "block/vda/queue/scheduler" => "deadline"
+        "block/vda/queue/nr_requests" => "128"
       }
     }
   },
