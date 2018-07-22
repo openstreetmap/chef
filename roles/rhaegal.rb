c283991b074eda3184dda15738dcbb994ff145a2
Tom Hughes <tom@compton.nu>
2018-07-22 19:01:49 +0100

    Configure rhaegal as a tile server

--- roles/rhaegal.rb
+++ roles/rhaegal.rb
@@ -7,6 +7,19 @@ default_attributes(
       :mmiler => { :status => :administrator }
     }
   },
+  :devices => {
+    :ssd_samsung => {
+      :comment => "Tune scheduler for SSD",
+      :type => "block",
+      :bus => "ata",
+      :serial => "Samsung_SSD_860_PRO_*",
+      :attrs => {
+        "queue/scheduler" => "noop",
+        "queue/nr_requests" => "256",
+        "queue/read_ahead_kb" => "2048"
+      }
+    }
+  },
   :location => "Zagreb, Croatia",
   :networking => {
     :interfaces => {
@@ -23,9 +36,42 @@ default_attributes(
       "10.5.0.7",
       "8.8.8.8"
     ]
+  },
+  :postgresql => {
+    :versions => ["10"],
+    :settings => {
+      :defaults => {
+        :shared_buffers => "8GB",
+        :maintenance_work_mem => "7144MB",
+        :effective_cache_size => "16GB"
+      }
+    }
+  },
+  :sysctl => {
+    :postgres => {
+      :comment => "Increase shared memory for postgres",
+      :parameters => {
+        "kernel.shmmax" => 9 * 1024 * 1024 * 1024,
+        "kernel.shmall" => 9 * 1024 * 1024 * 1024 / 4096
+      }
+    }
+  },
+  :tile => {
+    :database => {
+      :cluster => "10/main"
+    },
+    :node_file => "/store/database/nodes",
+    :styles => {
+      :default => {
+        :tile_directories => [
+          { :name => "/store/tiles/default", :min_zoom => 0, :max_zoom => 19 }
+        ]
+      }
+    }
   }
 )
 
 run_list(
-  "role[carnet]"
+  "role[carnet]",
+  "role[tile]"
 )
