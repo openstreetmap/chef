aab322e1d96e07027666b49a7fbf1443a0367eec
Tom Hughes <tom@compton.nu>
2018-07-28 13:47:45 +0100

    Add role for noomoahk

---
+++ roles/noomoahk.rb
@@ -0,0 +1,42 @@
+name "noomoahk"
+description "Master role applied to noomoahk"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :external_ipv4 => {
+        :interface => "ens3",
+        :role => :external,
+        :family => :inet,
+        :address => "91.224.148.166",
+        :prefix => "32",
+        :gateway => "89.234.156.230"
+      },
+      :external_ipv6 => {
+        :interface => "ens3",
+        :role => :external,
+        :family => :inet6,
+        :address => "2a03:7220:8080:a600::1",
+        :prefix => "56",
+        :gateway => "fe80::1"
+      }
+    }
+  },
+  :squid => {
+    :cache_mem => "6100 MB",
+    :cache_dir => "coss /store/squid/coss-01 80000 block-size=8192 max-size=262144 membufs=80"
+  },
+  :tilecache => {
+    :tile_parent => "toulouse.render.openstreetmap.org",
+    :tile_siblings => [
+      "nepomuk.openstreetmap.org",
+      "ladon.openstreetmap.org",
+      "culebre.openstreetmap.org"
+    ]
+  }
+)
+
+run_list(
+  "role[tetaneutral]",
+  "role[tilecache]"
+)
---
+++ roles/tetaneutral.rb
@@ -0,0 +1,28 @@
+name "tetaneutral"
+description "Role applied to all servers at Tetaneutral.net"
+
+default_attributes(
+  :hosted_by => "Tetaneutral.net",
+  :location => "Toulouse, France",
+  :networking => {
+    :nameservers => [
+      "8.8.8.8",
+      "8.8.4.4"
+    ],
+    :roles => {
+      :external => {
+        :zone => "tnn"
+      }
+    }
+  }
+)
+
+override_attributes(
+  :ntp => {
+    :servers => ["0.fr.pool.ntp.org", "1.fr.pool.ntp.org", "europe.pool.ntp.org"]
+  }
+)
+
+run_list(
+  "role[fr]"
+)
