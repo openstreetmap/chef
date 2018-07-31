6b0bf13c37e731b328a3f2cffa8dca2131a75a77
Tom Hughes <tom@compton.nu>
2018-07-31 21:50:01 +0100

    Increase nf_conntrack_max for tile caches

--- roles/tilecache.rb
+++ roles/tilecache.rb
@@ -25,7 +25,7 @@ default_attributes(
     :network_conntrack_max => {
       :comment => "Increase max number of connections tracked",
       :parameters => {
-        "net.netfilter.nf_conntrack_max" => "131072"
+        "net.netfilter.nf_conntrack_max" => "262142"
       }
     },
     :kernel_tfo_listen_enable => {
