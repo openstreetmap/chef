ecdfa97399f507c887f0caa006998e375d441f6e
Tom Hughes <tom@compton.nu>
2018-08-02 09:11:08 +0100

    Correct name of sysctl variable

--- roles/draco.rb
+++ roles/draco.rb
@@ -22,7 +22,7 @@ default_attributes(
     :tune_cpu_scheduler => {
       :comment => "Tune CPU scheduler for server scheduling",
       :parameters => {
-        "kernel.sched_migration_cost" => 50000000,
+        "kernel.sched_migration_cost_ns" => 50000000,
         "kernel.sched_autogroup_enabled" => 0
       }
     }
