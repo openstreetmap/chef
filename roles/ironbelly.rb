91946d5d67dc302bb147f0640cb5614416945891
Tom Hughes <tom@compton.nu>
2018-08-03 19:50:03 +0100

    Improve PDU monitoring

--- roles/ironbelly.rb
+++ roles/ironbelly.rb
@@ -26,21 +26,6 @@ default_attributes(
       :data => "/store/elasticsearch"
     }
   },
-  :munin => {
-    :graphs => {
-      :apcpdu_ams => {
-        :title => "Current for Amsterdam",
-        :vlabel => "Amps",
-        :category => "Ups",
-        :values => {
-          :load => {
-            :sum => ["apcpdu_pdu1.load", "apcpdu_pdu2.load"],
-            :label => "Load"
-          }
-        }
-      }
-    }
-  },
   :networking => {
     :interfaces => {
       :internal_ipv4 => {
