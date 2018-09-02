2d691940e6d5aa7fd466801901a44154f291c0a6
Grant Slater <git@firefishy.com>
2018-09-02 20:49:25 +0100

    update draco interface name

--- roles/draco.rb
+++ roles/draco.rb
@@ -5,13 +5,13 @@ default_attributes(
   :networking => {
     :interfaces => {
       :internal_ipv4 => {
-        :interface => "eth0.2801",
+        :interface => "enp3s0f0.2801",
         :role => :internal,
         :family => :inet,
         :address => "10.0.0.11"
       },
       :external_ipv4 => {
-        :interface => "eth0.2800",
+        :interface => "enp3s0f0.2800",
         :role => :external,
         :family => :inet,
         :address => "193.60.236.12"
