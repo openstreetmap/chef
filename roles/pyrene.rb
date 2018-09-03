db7c1696b53b00c79b0f4bd623b00eccf1f04897
Tom Hughes <tom@compton.nu>
2018-09-03 21:11:34 +0100

    Configure pyrene as a render server

--- roles/pyrene.rb
+++ roles/pyrene.rb
@@ -2,6 +2,9 @@ name "pyrene"
 description "Master role applied to pyrene"
 
 default_attributes(
+  :apt => {
+    :sources => ["postgresql"]
+  },
   :networking => {
     :interfaces => {
       :external_ipv4 => {
@@ -13,9 +16,43 @@ default_attributes(
         :gateway => "140.211.15.1"
       }
     }
+  },
+  :postgresql => {
+    :versions => ["10"],
+    :settings => {
+      :defaults => {
+        :shared_buffers => "8GB",
+        :maintenance_work_mem => "7144MB",
+        :effective_cache_size => "16GB"
+      }
+    }
+  },
+  :sysctl => {
+    :postgres => {
+      :comment => "Increase shared memory for postgres",
+      :parameters => {
+        "kernel.shmmax" => 9 * 1024 * 1024 * 1024,
+        "kernel.shmall" => 9 * 1024 * 1024 * 1024 / 4096
+      }
+    }
+  },
+  :tile => {
+    :database => {
+      :cluster => "10/main",
+      :postgis => "2.4"
+    },
+    :node_file => "/store/database/nodes",
+    :styles => {
+      :default => {
+        :tile_directories => [
+          { :name => "/store/tiles/default", :min_zoom => 0, :max_zoom => 19 }
+        ]
+      }
+    }
   }
 )
 
 run_list(
-  "role[osuosl]"
+  "role[osuosl]",
+  "role[tile]"
 )
