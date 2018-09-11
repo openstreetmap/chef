7e72af6717778f869498e10adcd77404f06c0035
Tom Hughes <tom@compton.nu>
2018-09-11 17:58:23 +0100

    Replace apt_source with builtin apt_repository resource
    Closes #189

--- cookbooks/apt/recipes/default.rb
+++ cookbooks/apt/recipes/default.rb
@@ -31,9 +31,8 @@ file "/etc/motd.tail" do
   action :delete
 end
 
-execute "apt-update" do
+apt_update "/etc/apt/sources.list" do
   action :nothing
-  command "/usr/bin/apt-get update"
 end
 
 archive_host = if node[:country]
@@ -48,110 +47,106 @@ template "/etc/apt/sources.list" do
   group "root"
   mode 0o644
   variables :archive_host => archive_host, :codename => node[:lsb][:codename]
-  notifies :run, "execute[apt-update]", :immediately
+  notifies :update, "apt_update[/etc/apt/sources.list]", :immediately
 end
 
-if node[:lsb][:release].to_f >= 16.04
-  apt_source "brightbox-ruby-ng" do
-    action :delete
+repository_actions = Hash.new do |_, repository|
+  node[:apt][:sources].include?(repository) ? :add : :remove
+end
+
+apt_repository "brightbox-ruby-ng" do
+  action repository_actions["brightbox-ruby-ng"]
+  uri "ppa:brightbox/ruby-ng"
+end
+
+apt_repository "ubuntugis-stable" do
+  action repository_actions["ubuntugis-stable"]
+  uri "ppa:ubuntugis/ppa"
+end
+
+apt_repository "ubuntugis-unstable" do
+  action repository_actions["ubuntugis-unstable"]
+  uri "ppa:ubuntugis/ubuntugis-unstable"
+end
+
+apt_repository "openstreetmap" do
+  action repository_actions["openstreetmap"]
+  uri "ppa:osmadmins/ppa"
+end
+
+apt_repository "squid2" do
+  action repository_actions["squid2"]
+  uri "ppa:osmadmins/squid2"
+end
+
+apt_repository "squid3" do
+  action repository_actions["squid3"]
+  uri "ppa:osmadmins/squid3"
+end
+
+apt_repository "management-component-pack" do
+  action repository_actions["management-component-pack"]
+  uri "https://downloads.linux.hpe.com/SDR/repo/mcp"
+  if node[:lsb][:release].to_f >= 16.04
+    distribution "xenial/current"
+  else
+    distribution "#{node[:lsb][:codename]}/current"
   end
-else
-  apt_source "brightbox-ruby-ng" do
-    url "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu"
-    key "F5DA5F09C3173AA6"
-  end
-end
-
-apt_source "ubuntugis-stable" do
-  url "http://ppa.launchpad.net/ubuntugis/ppa/ubuntu"
-  key "089EBE08314DF160"
-end
-
-apt_source "ubuntugis-unstable" do
-  url "http://ppa.launchpad.net/ubuntugis/ubuntugis-unstable/ubuntu"
-  key "089EBE08314DF160"
-end
-
-apt_source "openstreetmap" do
-  url "http://ppa.launchpad.net/osmadmins/ppa/ubuntu"
-  key "D57F48750AC4F2CB"
-  update true
-end
-
-apt_source "squid2" do
-  url "http://ppa.launchpad.net/osmadmins/squid2/ubuntu"
-  key "D57F48750AC4F2CB"
-  update true
-end
-
-apt_source "squid3" do
-  url "http://ppa.launchpad.net/osmadmins/squid3/ubuntu"
-  key "D57F48750AC4F2CB"
-  update true
-end
-
-apt_source "management-component-pack" do
-  source_template "hp.list.erb"
-  url "https://downloads.linux.hpe.com/SDR/repo/mcp"
+  components ["non-free"]
   key "C208ADDE26C2B797"
-  key_url "https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub"
 end
 
-apt_source "hwraid" do
-  source_template "hwraid.list.erb"
-  url "https://hwraid.le-vert.net/ubuntu"
+apt_repository "hwraid" do
+  action repository_actions["hwraid"]
+  uri "https://hwraid.le-vert.net/ubuntu"
+  distribution "precise"
+  components ["main"]
   key "6005210E23B3D3B4"
 end
 
-apt_source "mapnik-v210" do
-  url "http://ppa.launchpad.net/mapnik/v2.1.0/ubuntu"
-  key "4F7B93595D50B6BA"
-end
-
-apt_source "nginx" do
-  source_template "nginx.list.erb"
-  url "https://nginx.org/packages/ubuntu"
+apt_repository "nginx" do
+  action repository_actions["nginx"]
+  uri "https://nginx.org/packages/ubuntu"
+  components ["nginx"]
   key "ABF5BD827BD9BF62"
 end
 
-apt_source "elasticsearch1.7" do
-  source_template "elasticsearch.list.erb"
-  url "https://packages.elasticsearch.org/elasticsearch/1.7/debian"
+apt_repository "elasticsearch5.x" do
+  action repository_actions["elasticsearch5.x"]
+  uri "https://artifacts.elastic.co/packages/5.x/apt"
+  distribution "stable"
+  components ["main"]
   key "D27D666CD88E42B4"
 end
 
-apt_source "elasticsearch2.x" do
-  source_template "elasticsearch.list.erb"
-  url "https://packages.elasticsearch.org/elasticsearch/2.x/debian"
+apt_repository "logstash" do
+  action repository_actions["logstash"]
+  uri "https://packages.elasticsearch.org/logstash/2.3/debian"
+  distribution "stable"
+  components ["main"]
   key "D27D666CD88E42B4"
 end
 
-apt_source "elasticsearch5.x" do
-  source_template "elasticsearch.list.erb"
-  url "https://artifacts.elastic.co/packages/5.x/apt"
-  key "D27D666CD88E42B4"
-end
-
-apt_source "logstash" do
-  source_template "elasticsearch.list.erb"
-  url "https://packages.elasticsearch.org/logstash/2.3/debian"
-  key "D27D666CD88E42B4"
-end
-
-apt_source "passenger" do
-  url "https://oss-binaries.phusionpassenger.com/apt/passenger"
+apt_repository "passenger" do
+  action repository_actions["passenger"]
+  uri "https://oss-binaries.phusionpassenger.com/apt/passenger"
+  components ["main"]
   key "561F9B9CAC40B2F7"
 end
 
-apt_source "postgresql" do
-  source_template "postgresql.list.erb"
-  url "https://apt.postgresql.org/pub/repos/apt"
+apt_repository "postgresql" do
+  action repository_actions["postgresql"]
+  uri "https://apt.postgresql.org/pub/repos/apt"
+  distribution "#{node[:lsb][:codename]}-pgdg"
+  components ["main"]
   key "7FCC7D46ACCC4CF8"
 end
 
-apt_source "mediawiki" do
-  source_template "mediawiki.list.erb"
-  url "https://releases.wikimedia.org/debian"
+apt_repository "mediawiki" do
+  action repository_actions["mediawiki"]
+  uri "https://releases.wikimedia.org/debian"
+  distribution "jessie-mediawiki"
+  components ["main"]
   key "90E9F83F22250DD7"
 end
 
