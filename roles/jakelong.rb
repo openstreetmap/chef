a50797d1062f64717603c24b4e8cdfbeb60277e5
Tom Hughes <tom@compton.nu>
2018-10-18 08:33:33 +0100

    Increase memory and disk cache size on jakelong

--- roles/jakelong.rb
+++ roles/jakelong.rb
@@ -23,8 +23,8 @@ default_attributes(
     }
   },
   :squid => {
-    :cache_mem => "200 MB",
-    :cache_dir => "coss /store/squid/coss-01 7500 block-size=8192 max-size=262144 membufs=30"
+    :cache_mem => "1024 MB",
+    :cache_dir => "coss /store/squid/coss-01 24000 block-size=8192 max-size=262144 membufs=30"
   },
   :sysctl => {
     :kvm => {
