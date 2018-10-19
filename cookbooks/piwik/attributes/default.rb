a22511f3e362cc586b4fbf9cdbaac27a8703e09c
Tom Hughes <tom@compton.nu>
2018-10-19 16:53:18 +0100

    Update piwik to 3.6.1

--- cookbooks/piwik/attributes/default.rb
+++ cookbooks/piwik/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:piwik][:version] = "3.6.0"
+default[:piwik][:version] = "3.6.1"
 default[:piwik][:plugins] = %w[
   Actions Annotations API BulkTracking Contents CoreAdminHome CoreConsole
   CoreHome CorePluginsAdmin CoreUpdater CoreVisualizations CustomPiwikJs
