e88020178b6617b163bafb500a6cf2d1ab79428d
Grant Slater <git@firefishy.com>
2019-01-10 07:23:49 +0000

    imagery: update namibia vrt

--- cookbooks/imagery/recipes/na_sgswa_topo.rb
+++ cookbooks/imagery/recipes/na_sgswa_topo.rb
@@ -29,7 +29,7 @@ imagery_layer "na_sgswa_topo_50k" do
   site "namibia-topo.openstreetmap.org.za"
   title "Namibia Topo 50k"
   projection "EPSG:4326"
-  source "/data/imagery/na/topo-50k/namibia-50k-topo.vrt"
+  source "/data/imagery/na/topo-50k/namibia-50k-topo-v2.vrt"
   copyright "State Copyright &copy 1958 - 1991; Surveyor-General, Windhoek, SWA; CDSM: Chief Directorate Surveys & Mapping, Mowbray, RSA"
   default_layer true
   background_colour "0 0 0"
