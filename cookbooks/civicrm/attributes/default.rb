1ed516b655b06d7df8a661e5f60f105138947220
Tom Hughes <tom@compton.nu>
2019-02-23 17:18:10 +0000

    Update CiviCRM to 5.10.4

--- cookbooks/civicrm/attributes/default.rb
+++ cookbooks/civicrm/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:civicrm][:version] = "5.9.0"
+default[:civicrm][:version] = "5.10.4"
 
 default[:civicrm][:extensions][:cividiscount][:name] = "org.civicrm.module.cividiscount"
 default[:civicrm][:extensions][:cividiscount][:repository] = "git://github.com/dlobo/org.civicrm.module.cividiscount.git"
