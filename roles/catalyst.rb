a61d0b3dd1a9ebccff4d6fb6112f937256d75764
Tom Hughes <tom@compton.nu>
2019-03-05 18:59:09 +0000

    Add role for waima

---
+++ roles/catalyst.rb
@@ -0,0 +1,25 @@
+name "catalyst"
+description "Role applied to all servers at Catalyst"
+
+default_attributes(
+  :hosted_by => "Catalyst",
+  :location => "New Zealand",
+  :networking => {
+    :nameservers => ["202.78.244.85", "202.78.244.86", "202.78.244.87"],
+    :roles => {
+      :external => {
+        :zone => "osm"
+      }
+    }
+  }
+)
+
+override_attributes(
+  :ntp => {
+    :servers => ["0.nz.pool.ntp.org", "1.nz.pool.ntp.org", "asia.pool.ntp.org"]
+  }
+)
+
+run_list(
+  "role[nz]"
+)
---
+++ roles/waima.rb
@@ -0,0 +1,36 @@
+name "waima"
+description "Master role applied to waima"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :external_ipv4 => {
+        :interface => "ens3",
+        :role => :external,
+        :family => :inet,
+        :address => "192.168.1.4",
+        :prefix => "24",
+        :gateway => "192.168.1.1",
+        :public_address => "103.197.61.160"
+      }
+    }
+  },
+  :squid => {
+    :cache_mem => "7500 MB",
+    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+  },
+  :tilecache => {
+    :tile_parent => "wellington.render.openstreetmap.org",
+    :tile_siblings => [
+      "tuatara.openstreetmap.org",
+      "longma.openstreetmap.org",
+      "storfly-02.openstreetmap.org",
+      "jakelong.openstreetmap.org"
+    ]
+  }
+)
+
+run_list(
+  "role[catalyst]",
+  "role[tilecache]"
+)
