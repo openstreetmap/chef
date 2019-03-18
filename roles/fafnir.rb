f9e4be2de40fb27751305a8315b29901f9ae8886
Tom Hughes <tom@compton.nu>
2019-03-18 21:10:58 +0000

    Add role for fafnir

--- roles/fafnir.rb
+++ roles/fafnir.rb
@@ -1,35 +1,35 @@
-name "nidhogg"
-description "Master role applied to nidhogg"
+name "fafnir"
+description "Master role applied to fafnir"
 
 default_attributes(
   :networking => {
     :interfaces => {
       :external_ipv4 => {
-        :interface => "ens18",
+        :interface => "enp3s0f0",
         :role => :external,
         :family => :inet,
-        :address => "130.236.254.221",
-        :prefix => "24",
-        :gateway => "130.236.254.1"
+        :address => "130.239.18.114",
+        :prefix => "27",
+        :gateway => "130.239.18.97"
       },
       :external_ipv6 => {
-        :interface => "ens18",
+        :interface => "enp3s0f0",
         :role => :external,
         :family => :inet6,
-        :address => "2001:6b0:17:f0a0::dd",
+        :address => "2001:6b0:e:2a18::114",
         :prefix => "64",
-        :gateway => "2001:6b0:17:f0a0::1"
+        :gateway => "fe80::5a97:bdff:fe79:dbc0"
       }
     }
   },
   :squid => {
-    :cache_mem => "7500 MB",
+    :cache_mem => "28000 MB",
     :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
   },
   :tilecache => {
-    :tile_parent => "linkoping.render.openstreetmap.org",
+    :tile_parent => "umea.render.openstreetmap.org",
     :tile_siblings => [
-      "fafnir.openstreetmap.org",
+      "nidhogg.openstreetmap.org",
       "ridgeback.openstreetmap.org",
       "rimfaxe.openstreetmap.org",
       "trogdor.openstreetmap.org"
@@ -38,6 +38,6 @@ default_attributes(
 )
 
 run_list(
-  "role[lysator]",
+  "role[umu]",
   "role[tilecache]"
 )
---
+++ roles/umu.rb
@@ -0,0 +1,25 @@
+name "umu"
+description "Role applied to all servers at Umeå University"
+
+default_attributes(
+  :hosted_by => "Academic Computer Club, Umeå University",
+  :location => "Umeå, Sweden",
+  :networking => {
+    :nameservers => ["130.239.18.251", "130.239.18.252", "130.239.1.90"],
+    :roles => {
+      :external => {
+        :zone => "osm"
+      }
+    }
+  }
+)
+
+override_attributes(
+  :ntp => {
+    :servers => ["0.se.pool.ntp.org", "1.se.pool.ntp.org", "europe.pool.ntp.org"]
+  }
+)
+
+run_list(
+  "role[se]"
+)
