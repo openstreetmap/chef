fb74c967e5abb0f7c1db92a2dcd10d8ecd3be933
Tom Hughes <tom@compton.nu>
2019-03-27 16:08:47 +0000

    Add roles for spike-{06,07,08}

---
+++ roles/spike-07.rb
@@ -0,0 +1,34 @@
+name "spike-07"
+description "Master role applied to spike-07"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal_ipv4 => {
+        :interface => "bond0",
+        :role => :internal,
+        :family => :inet,
+        :address => "10.0.48.12",
+        :bond => {
+          :slaves => %w[eno1 eno2]
+        }
+      },
+      :external_ipv4 => {
+        :interface => "bond0.2",
+        :role => :external,
+        :family => :inet,
+        :address => "130.117.76.12"
+      },
+      :external_ipv6 => {
+        :interface => "bond0.2",
+        :role => :external,
+        :family => :inet6,
+        :address => "2001:978:2:2C::172:C"
+      }
+    }
+  }
+)
+
+run_list(
+  "role[equinix]"
+)
