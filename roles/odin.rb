ed17684b5d2a41a49968c90be2cb6ee8eace8265
Tom Hughes <tom@compton.nu>
2019-04-03 13:26:45 +0100

    Add role for odin

---
+++ roles/odin.rb
@@ -0,0 +1,34 @@
+name "odin"
+description "Master role applied to odin"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal_ipv4 => {
+        :interface => "bond0",
+        :role => :internal,
+        :family => :inet,
+        :address => "10.0.48.15",
+        :bond => {
+          :slaves => %w[eno1 eno2]
+        }
+      },
+      :external_ipv4 => {
+        :interface => "bond0.2",
+        :role => :external,
+        :family => :inet,
+        :address => "130.117.76.15"
+      },
+      :external_ipv6 => {
+        :interface => "bond0.2",
+        :role => :external,
+        :family => :inet6,
+        :address => "2001:978:2:2C::172:F"
+      }
+    }
+  }
+)
+
+run_list(
+  "role[equinix]"
+)
