0dd6648dccbe588d9160e8f54ec05036c0d0bef8
Tom Hughes <tom@compton.nu>
2019-05-20 08:30:13 +0100

    Configure fume as a tile cache

--- roles/fume.rb
+++ roles/fume.rb
@@ -13,9 +13,23 @@ default_attributes(
         :gateway => "147.228.60.1"
       }
     }
+  },
+  :squid => {
+    :cache_mem => "4096 MB",
+    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+  },
+  :tilecache => {
+    :tile_parent => "pilsen.render.openstreetmap.org",
+    :tile_siblings => [
+      "sarkany.openstreetmap.org",
+      "chrysophylax.openstreetmap.org",
+      "drogon.openstreetmap.org",
+      "viserion.openstreetmap.org"
+    ]
   }
 )
 
 run_list(
-  "role[zcu]"
+  "role[zcu]",
+  "role[tilecache]"
 )
