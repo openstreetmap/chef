ee668218da1e9d167af302e601e787e511354710
Tom Hughes <tom@compton.nu>
2019-05-30 19:56:55 +0100

    Add role for toothless

--- roles/toothless.rb
+++ roles/toothless.rb
@@ -1,5 +1,5 @@
-name "saphira"
-description "Master role applied to saphira"
+name "toothless"
+description "Master role applied to toothless"
 
 default_attributes(
   :networking => {
@@ -8,7 +8,7 @@ default_attributes(
         :interface => "eth0",
         :role => :external,
         :family => :inet,
-        :address => "185.73.44.30",
+        :address => "185.73.44.167",
         :prefix => "22",
         :gateway => "185.73.44.1"
       },
@@ -16,20 +16,20 @@ default_attributes(
         :interface => "eth0",
         :role => :external,
         :family => :inet6,
-        :address => "2001:ba8:0:2c1e::",
+        :address => "2001:ba8:0:2ca7::",
         :prefix => "64",
         :gateway => "fe80::fcff:ffff:feff:ffff"
       }
     }
   },
   :squid => {
-    :cache_mem => "5120 MB",
+    :cache_mem => "3100 MB",
     :cache_dir => "coss /store/squid/coss-01 80000 block-size=8192 max-size=262144 membufs=80"
   },
   :tilecache => {
     :tile_parent => "london.render.openstreetmap.org",
     :tile_siblings => [
-      "toothless.openstreetmap.org",
+      "saphira.openstreetmap.org",
       "trogdor.openstreetmap.org",
       "katie.openstreetmap.org",
       "konqi.openstreetmap.org",
