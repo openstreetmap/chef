aa1fc7ef4a4508984711d96be2beed4d2a78fd9b
Guillaume RISCHARD <git@stereo.lu>
2019-06-08 21:37:45 +0200

    Update CiviCRM extensions to latest versions

--- cookbooks/civicrm/attributes/default.rb
+++ cookbooks/civicrm/attributes/default.rb
@@ -2,7 +2,7 @@ default[:civicrm][:version] = "5.14.0"
 
 default[:civicrm][:extensions][:cividiscount][:name] = "org.civicrm.module.cividiscount"
 default[:civicrm][:extensions][:cividiscount][:repository] = "git://github.com/dlobo/org.civicrm.module.cividiscount.git"
-default[:civicrm][:extensions][:cividiscount][:revision] = "3.3"
+default[:civicrm][:extensions][:cividiscount][:revision] = "3.7"
 
 default[:civicrm][:extensions][:osm][:name] = "de.systopia.osm"
 default[:civicrm][:extensions][:osm][:repository] = "git://github.com/systopia/de.systopia.osm.git"
@@ -18,7 +18,7 @@ default[:civicrm][:extensions][:civiruleshttppost][:revision] = "e2c7de5f0fee319
 
 default[:civicrm][:extensions][:civirules][:name] = "org.civicoop.civirules"
 default[:civicrm][:extensions][:civirules][:repository] = "https://lab.civicrm.org/extensions/civirules.git"
-default[:civicrm][:extensions][:civirules][:revision] = "2.3"
+default[:civicrm][:extensions][:civirules][:revision] = "2.7"
 
 default[:civicrm][:extensions][:stripe][:name] = "com.drastikbydesign.stripe"
 default[:civicrm][:extensions][:stripe][:repository] = "git://github.com/drastik/com.drastikbydesign.stripe.git"
