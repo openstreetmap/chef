31852a22c182c720bfae45f7daeaa32550bcb9ab
Tom Hughes <tom@compton.nu>
2019-06-10 00:16:14 +0100

    Tune cache sizes for fume

--- roles/fume.rb
+++ roles/fume.rb
@@ -18,10 +18,10 @@ default_attributes(
     :version => 4,
     :cache_mem => "4096 MB",
     :cache_dir => [
-      "rock /store/squid/rock-4096 50000 slot-size=4096 max-size=3996",
-      "rock /store/squid/rock-8192 15000 slot-size=8192 max-size=8092",
-      "rock /store/squid/rock-16384 25000 slot-size=16384 max-size=16284",
-      "rock /store/squid/rock-32768 35000 slot-size=32768 max-size=262144"
+      "rock /store/squid/rock-4096 20000 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 25000 slot-size=8192 max-size=8092",
+      "rock /store/squid/rock-16384 35000 slot-size=16384 max-size=16284",
+      "rock /store/squid/rock-32768 45000 slot-size=32768 max-size=262144"
     ]
   },
   :tilecache => {
