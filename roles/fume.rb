f03bcb4207a6b274b85e4e39b282ec44bff48e92
Tom Hughes <tom@compton.nu>
2019-08-28 12:58:42 +0100

    Add swap timeout to fume's squid cache stores

--- roles/fume.rb
+++ roles/fume.rb
@@ -21,10 +21,10 @@ default_attributes(
     :version => 4,
     :cache_mem => "4096 MB",
     :cache_dir => [
-      "rock /store/squid/rock-4096 20000 slot-size=4096 max-size=3996",
-      "rock /store/squid/rock-8192 25000 slot-size=8192 min-size=3997 max-size=8092",
-      "rock /store/squid/rock-16384 35000 slot-size=16384 min-size=8093 max-size=16284",
-      "rock /store/squid/rock-32768 45000 slot-size=32768 min-size=16285 max-size=262144"
+      "rock /store/squid/rock-4096 20000 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 25000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 35000 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 45000 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
     ]
   },
   :tilecache => {
