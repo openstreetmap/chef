b9eeecc2a3ad0fbba208b5b1cea862123fb746b6
Tom Hughes <tom@compton.nu>
2019-09-09 19:14:05 +0100

    Drop long_description from metadata files

--- cookbooks/ftp/metadata.rb
+++ cookbooks/ftp/metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Grant Slater"
 maintainer_email "chef@firefishy.com"
 license          "Apache-2.0"
 description      "Installs/Configures ftp daemon"
-long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+
 version          "0.1"
 supports         "ubuntu"
 depends          "networking"
--- cookbooks/nfs/metadata.rb
+++ cookbooks/nfs/metadata.rb
@@ -3,6 +3,6 @@ maintainer        "OpenStreetMap Administrators"
 maintainer_email  "admins@openstreetmap.org"
 license           "Apache-2.0"
 description       "Installs and configures nfs"
-long_description  IO.read(File.join(File.dirname(__FILE__), "README.md"))
+
 version           "1.0.0"
 supports          "ubuntu"
