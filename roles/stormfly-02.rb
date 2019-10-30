8a3859468e02e585f7ac43b1e2f7eef7349a50e3
Tom Hughes <tom@compton.nu>
2019-10-30 22:30:40 +0000

    Switch stormfly-02 to squid 4

--- roles/stormfly-02.rb
+++ roles/stormfly-02.rb
@@ -2,6 +2,9 @@ name "stormfly-02"
 description "Master role applied to stormfly-02"
 
 default_attributes(
+  :hardware => {
+    :shm_size => "36g"
+  },
   :networking => {
     :interfaces => {
       :external_ipv4 => {
@@ -19,8 +22,14 @@ default_attributes(
     }
   },
   :squid => {
-    :cache_mem => "21000 MB",
-    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+    :version => 4,
+    :cache_mem => "32768 MB",
+    :cache_dir => [
+      "rock /store/squid/rock-4096 20000 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 25000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 35000 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 45000 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+    ]
   },
   :tilecache => {
     :tile_parent => "usa.render.openstreetmap.org",
