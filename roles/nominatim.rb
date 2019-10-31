52aa16e8539c59c07269c16a8cecb62583069ac5
Sarah Hoffmann <lonvia@denofr.de>
2019-10-31 09:54:13 +0100

    Revert "nominatim: remove firewall http rate limiting"
    This reverts commit f2e8efe99b471cd5aef85977e6b22d024151c9a4.

--- roles/nominatim.rb
+++ roles/nominatim.rb
@@ -28,6 +28,11 @@ default_attributes(
   :apt => {
     :sources => ["postgresql"]
   },
+  :networking => {
+    :firewall => {
+      :http_rate_limit => "s:7/sec:15"
+    }
+  },
   :postgresql => {
     :settings => {
       :defaults => {
