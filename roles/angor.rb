1b50376d9b025051576a3a495a760c9db992d7ba
Tom Hughes <tom@compton.nu>
2019-11-05 22:33:29 +0000

    Switch angor, balerion, tuatara and waima to squid 4

--- roles/angor.rb
+++ roles/angor.rb
@@ -2,6 +2,9 @@ name "angor"
 description "Master role applied to angor"
 
 default_attributes(
+  :hardware => {
+    :shm_size => "18g"
+  },
   :networking => {
     :interfaces => {
       :external_ipv4 => {
@@ -23,8 +26,14 @@ default_attributes(
     }
   },
   :squid => {
-    :cache_mem => "6100 MB",
-    :cache_dir => "coss /store/squid/coss-01 80000 block-size=8192 max-size=262144 membufs=80"
+    :version => 4,
+    :cache_mem => "16384 MB",
+    :cache_dir => [
+      "rock /store/squid/rock-4096 12800 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 16000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 22400 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 22800 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+    ]
   },
   :tilecache => {
     :tile_parent => "capetown.render.openstreetmap.org",
--- roles/balerion.rb
+++ roles/balerion.rb
@@ -2,6 +2,9 @@ name "balerion"
 description "Master role applied to balerion"
 
 default_attributes(
+  :hardware => {
+    :shm_size => "36g"
+  },
   :networking => {
     :interfaces => {
       :external_ipv4 => {
@@ -18,8 +21,14 @@ default_attributes(
     }
   },
   :squid => {
-    :cache_mem => "32000 MB",
-    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+    :version => 4,
+    :cache_mem => "32768 MB",
+    :cache_dir => [
+      "rock /store/squid/rock-4096 20000 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 25000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 35000 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 45000 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+    ]
   },
   :tilecache => {
     :tile_parent => "melbourne.render.openstreetmap.org",
