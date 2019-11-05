de36e6558743eebc1185d32fa0b58dbafe9bbc53
Tom Hughes <tom@compton.nu>
2019-11-05 20:44:29 +0000

    Switch culebre, fafnir, ladon and trogdor to squid 4

--- roles/ladon.rb
+++ roles/ladon.rb
@@ -2,6 +2,9 @@ name "ladon"
 description "Master role applied to ladon"
 
 default_attributes(
+  :hardware => {
+    :shm_size => "36g"
+  },
   :networking => {
     :interfaces => {
       :external_ipv4 => {
@@ -29,8 +32,14 @@ default_attributes(
     }
   },
   :squid => {
-    :cache_mem => "6100 MB",
-    :cache_dir => "coss /store/squid/coss-01 80000 block-size=8192 max-size=262144 membufs=80"
+    :version => 4,
+    :cache_mem => "32768 MB",
+    :cache_dir => [
+      "rock /store/squid/rock-4096 12800 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 16000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 22400 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 22800 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+    ]
   },
   :tilecache => {
     :tile_parent => "athens.render.openstreetmap.org",
