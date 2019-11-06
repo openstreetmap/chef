d5c4929d8366de7aeb0dd18cbc051e53dae1420d
Tom Hughes <tom@compton.nu>
2019-11-06 18:05:38 +0000

    Switch cmok, drogon, gorynych and sarkany to squid 4

--- roles/cmok.rb
+++ roles/cmok.rb
@@ -2,6 +2,9 @@ name "cmok"
 description "Master role applied to cmok"
 
 default_attributes(
+  :hardware => {
+    :shm_size => "6g"
+  },
   :networking => {
     :interfaces => {
       :external_ipv4 => {
@@ -23,8 +26,14 @@ default_attributes(
     }
   },
   :squid => {
-    :cache_mem => "2560 MB",
-    :cache_dir => "coss /store/squid/coss-01 80000 block-size=8192 max-size=262144 membufs=80"
+    :version => 4,
+    :cache_mem => "4096 MB",
+    :cache_dir => [
+      "rock /store/squid/rock-4096 12800 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 16000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 22400 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 22800 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+    ]
   },
   :tilecache => {
     :tile_parent => "minsk.render.openstreetmap.org",
--- roles/drogon.rb
+++ roles/drogon.rb
@@ -7,6 +7,9 @@ default_attributes(
       :zelja => { :status => :administrator }
     }
   },
+  :hardware => {
+    :shm_size => "18g"
+  },
   :location => "Osijek, Croatia",
   :networking => {
     :interfaces => {
@@ -33,8 +36,14 @@ default_attributes(
     ]
   },
   :squid => {
-    :cache_mem => "20000 MB",
-    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+    :version => 4,
+    :cache_mem => "16384 MB",
+    :cache_dir => [
+      "rock /store/squid/rock-4096 12800 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 16000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 22400 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 22800 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+    ]
   },
   :tilecache => {
     :tile_parent => "osijek.render.openstreetmap.org",
