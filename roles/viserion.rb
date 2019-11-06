600c4e6130ff3b98df2897badfb5a6fbdcca1df2
Tom Hughes <tom@compton.nu>
2019-11-06 18:25:16 +0000

    Switch chrysophylax, simurgh, vipertooth and viserion to squid 4

--- roles/viserion.rb
+++ roles/viserion.rb
@@ -7,6 +7,9 @@ default_attributes(
       :anovak => { :status => :administrator }
     }
   },
+  :hardware => {
+    :shm_size => "36g"
+  },
   :location => "Pula, Croatia",
   :munin => {
     :allow => ["193.198.233.210"]
@@ -38,8 +41,14 @@ default_attributes(
     ]
   },
   :squid => {
-    :cache_mem => "28000 MB",
-    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+    :version => 4,
+    :cache_mem => "32768 MB",
+    :cache_dir => [
+      "rock /store/squid/rock-4096 20000 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 25000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 35000 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 45000 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+    ]
   },
   :tilecache => {
     :tile_parent => "pula.render.openstreetmap.org",
