60fb340c348c186bb9977877e8c2993623baa36a
Tom Hughes <tom@compton.nu>
2019-11-07 18:24:55 +0000

    Switch kalessin, katie, keizer and konqi to squid 4

--- roles/kalessin.rb
+++ roles/kalessin.rb
@@ -2,6 +2,9 @@ name "kalessin"
 description "Master role applied to kalessin"
 
 default_attributes(
+  :hardware => {
+    :shm_size => "18g"
+  },
   :networking => {
     :interfaces => {
       :external_ipv4 => {
@@ -23,8 +26,14 @@ default_attributes(
     }
   },
   :squid => {
-    :cache_mem => "12500 MB",
-    :cache_dir => "coss /store/squid/coss-01 128000 block-size=8192 max-size=262144 membufs=80"
+    :version => 4,
+    :cache_mem => "16384 MB",
+    :cache_dir => [
+      "rock /store/squid/rock-4096 20000 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 25000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 35000 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 45000 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+    ]
   },
   :nginx => {
     :cache => {
--- roles/keizer.rb
+++ roles/keizer.rb
@@ -2,6 +2,9 @@ name "keizer"
 description "Master role applied to keizer"
 
 default_attributes(
+  :hardware => {
+    :shm_size => "14g"
+  },
   :location => "Nuremberg, Germany",
   :networking => {
     :interfaces => {
@@ -24,8 +27,14 @@ default_attributes(
     }
   },
   :squid => {
-    :cache_mem => "7500 MB",
-    :cache_dir => "coss /store/squid/coss-01 80000 block-size=8192 max-size=262144 membufs=80"
+    :version => 4,
+    :cache_mem => "12288 MB",
+    :cache_dir => [
+      "rock /store/squid/rock-4096 12800 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 16000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 22400 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 22800 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+    ]
   },
   :tilecache => {
     :tile_parent => "germany.render.openstreetmap.org",
