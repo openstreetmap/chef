0727d9054668ffca19e63567e7c33dc36b10ebea
Tom Hughes <tom@compton.nu>
2019-11-14 21:13:16 +0000

    Increase CPU temperature warning thresholds on rhaegal

--- roles/rhaegal.rb
+++ roles/rhaegal.rb
@@ -24,6 +24,24 @@ default_attributes(
     }
   },
   :location => "Zagreb, Croatia",
+  :munin => {
+    :plugins => {
+      :sensors_temp => {
+        :temp1 => { :warning => "85.0" },
+        :temp2 => { :warning => "85.0" },
+        :temp3 => { :warning => "85.0" },
+        :temp4 => { :warning => "85.0" },
+        :temp5 => { :warning => "85.0" },
+        :temp6 => { :warning => "85.0" },
+        :temp8 => { :warning => "85.0" },
+        :temp9 => { :warning => "85.0" },
+        :temp10 => { :warning => "85.0" },
+        :temp11 => { :warning => "85.0" },
+        :temp12 => { :warning => "85.0" },
+        :temp13 => { :warning => "85.0" }
+      }
+    }
+  },
   :networking => {
     :interfaces => {
       :external_ipv4 => {
