3447bb8c50e33a34e171dc9f071f3a212cc3da47
Grant Slater <git@firefishy.com>
2019-11-26 00:47:16 +0000

    longma: use new store.new-san mount

--- roles/longma.rb
+++ roles/longma.rb
@@ -34,7 +34,7 @@ default_attributes(
     :version => 4,
     :cache_mem => "16384 MB",
     :cache_dir => [
-      "rock /store/squid/rock-4096 20000 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store.new-san/squid/rock-4096 20000 swap-timeout=200 slot-size=4096 max-size=3996",
       "rock /store/squid/rock-8192 25000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
       "rock /store/squid/rock-16384 35000 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
       "rock /store/squid/rock-32768 45000 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
@@ -43,7 +43,7 @@ default_attributes(
   :nginx => {
     :cache => {
       :proxy => {
-        :directory => "/store/nginx-cache/proxy-cache"
+        :directory => "/store.new-san/nginx-cache/proxy-cache"
       }
     }
   },
