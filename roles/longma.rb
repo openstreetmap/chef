b19417ab9e42039a3a72a60ea0058d2c8abe07f4
Grant Slater <git@firefishy.com>
2019-11-26 09:39:26 +0000

    longma: move squid completely to new disk

--- roles/longma.rb
+++ roles/longma.rb
@@ -35,9 +35,9 @@ default_attributes(
     :cache_mem => "16384 MB",
     :cache_dir => [
       "rock /store.new-san/squid/rock-4096 20000 swap-timeout=200 slot-size=4096 max-size=3996",
-      "rock /store/squid/rock-8192 25000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
-      "rock /store/squid/rock-16384 35000 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
-      "rock /store/squid/rock-32768 45000 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+      "rock /store.new-san/squid/rock-8192 25000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store.new-san/squid/rock-16384 35000 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store.new-san/squid/rock-32768 45000 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
     ]
   },
   :nginx => {
