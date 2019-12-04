a4936a38c4593c7e780b32ab866c4e4c44068fbd
Tom Hughes <tom@compton.nu>
2019-12-04 10:13:52 +0000

    Enable SNMP access to naga

--- roles/fullsave.rb
+++ roles/fullsave.rb
@@ -5,6 +5,30 @@ default_attributes(
   :hosted_by => "FullSave",
   :location => "Toulouse, France",
   :networking => {
+    :firewall => {
+      :inet => [
+        {
+          :action => "ACCEPT",
+          :source => "net:185.116.130.12",
+          :dest => "fw",
+          :proto => "udp",
+          :dest_ports => "snmp",
+          :source_ports => "1024:",
+          :rate_limit => "-",
+          :connection_limit => "-"
+        },
+        {
+          :action => "ACCEPT",
+          :source => "net:100.80.8.0/24",
+          :dest => "fw",
+          :proto => "udp",
+          :dest_ports => "snmp",
+          :source_ports => "1024:",
+          :rate_limit => "-",
+          :connection_limit => "-"
+        }
+      ]
+    },
     :nameservers => ["141.0.202.202", "141.0.202.203"],
     :roles => {
       :external => {
