b2b40f7187850e6840f7f9a225801b26f3e33ec9
Guillaume RISCHARD <git@stereo.lu>
2019-12-26 21:25:54 +0100

    Version bump

--- cookbooks/civicrm/attributes/default.rb
+++ cookbooks/civicrm/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:civicrm][:version] = "5.19.0"
+default[:civicrm][:version] = "5.20.2"
 
 default[:civicrm][:extensions][:cividiscount][:name] = "org.civicrm.module.cividiscount"
 default[:civicrm][:extensions][:cividiscount][:repository] = "git://github.com/dlobo/org.civicrm.module.cividiscount.git"
