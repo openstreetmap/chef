7cae8dae965aaef737edd5c8438f32c70ec32bda
Tom Hughes <tom@compton.nu>
2020-01-21 18:46:37 +0000

    Restrict test image to DHCP provided resolvers

--- .kitchen.provision.rb
+++ .kitchen.provision.rb
@@ -1,5 +1,7 @@
 Vagrant.configure(2) do |config|
   config.vm.provision "shell", :inline => <<-SHELL
+    sed -i -e '/nameservers:/d' -e '/addresses:/d' /etc/netplan/01-netcfg.yaml
+    netplan apply
     apt-get update -y
   SHELL
 end
