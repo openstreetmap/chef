355b6afde6c212e886aa0d3788a2070b47ac680f
Tom Hughes <tom@compton.nu>
2020-02-11 11:01:14 +0000

    Increase cache memory limit on culebre

--- roles/culebre.rb
+++ roles/culebre.rb
@@ -3,7 +3,7 @@ description "Master role applied to culebre"
 
 default_attributes(
   :hardware => {
-    :shm_size => "14g"
+    :shm_size => "18g"
   },
   :networking => {
     :interfaces => {
@@ -26,7 +26,7 @@ default_attributes(
   },
   :squid => {
     :version => 4,
-    :cache_mem => "10240 MB",
+    :cache_mem => "16384 MB",
     :cache_dir => [
       "rock /store/squid/rock-4096 12800 swap-timeout=200 slot-size=4096 max-size=3996",
       "rock /store/squid/rock-8192 16000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
