186e4a0d86d300fee401014d1781adb87ed8d3e5
Tom Hughes <tom@compton.nu>
2020-03-16 14:32:22 +0000

    Make stormfly-01 a tile cache

--- roles/stormfly-01.rb
+++ roles/stormfly-01.rb
@@ -2,6 +2,9 @@ name "stormfly-01"
 description "Master role applied to stormfly-01"
 
 default_attributes(
+  :hardware => {
+    :shm_size => "38g"
+  },
   :networking => {
     :interfaces => {
       :external_ipv4 => {
@@ -17,10 +20,32 @@ default_attributes(
         :address => "2605:bc80:3010:700::8cde:a768"
       }
     }
+  },
+  :squid => {
+    :version => 4,
+    :cache_mem => "32768 MB",
+    :cache_dir => [
+      "rock /store/squid/rock-4096 20000 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 25000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 35000 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 45000 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+    ]
+  },
+  :nginx => {
+    :cache => {
+      :proxy => {
+        :max_size => "65536M"
+      }
+    }
+  },
+  :tilecache => {
+    :tile_parent => "corvallis.render.openstreetmap.org"
   }
 )
 
 run_list(
   "role[osuosl]",
-  "role[hp-dl360-g6]"
+  "role[hp-dl360-g6]",
+  "role[geodns]",
+  "role[tilecache]"
 )
