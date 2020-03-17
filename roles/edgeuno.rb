61b9c7c86689238f8460c7fb6a2e5870b93d1441
Tom Hughes <tom@compton.nu>
2020-03-17 22:33:42 +0000

    Enable snmp monitoring for Edgeuno machines

--- roles/edgeuno.rb
+++ roles/edgeuno.rb
@@ -5,6 +5,30 @@ default_attributes(
   :hosted_by => "EdgeUno",
   :location => "Bogotá, Colombia",
   :networking => {
+    :firewall => {
+      :inet => [
+        {
+          :action => "ACCEPT",
+          :source => "net:200.25.3.8/31",
+          :dest => "fw",
+          :proto => "udp",
+          :dest_ports => "snmp",
+          :source_ports => "1024:",
+          :rate_limit => "-",
+          :connection_limit => "-"
+        },
+        {
+          :action => "ACCEPT",
+          :source => "net:200.25.3.8/31",
+          :dest => "fw",
+          :proto => "tcp",
+          :dest_ports => "snmp",
+          :source_ports => "1024:",
+          :rate_limit => "-",
+          :connection_limit => "-"
+        }
+      ]
+    },
     :nameservers => [
       "8.8.8.8",
       "1.1.1.1"
