839b33b39c87675b65d5b810a455575154cea105
Tom Hughes <tom@compton.nu>
2020-03-19 17:27:59 +0000

    Configure albi as a tile server

--- roles/albi.rb
+++ roles/albi.rb
@@ -21,9 +21,43 @@ default_attributes(
         :gateway => "fe80::a293:51ff:fea2:ded5"
       }
     }
+  },
+  :postgresql => {
+    :versions => ["12"],
+    :settings => {
+      :defaults => {
+        :shared_buffers => "8GB",
+        :maintenance_work_mem => "7144MB",
+        :effective_cache_size => "16GB"
+      }
+    }
+  },
+  :sysctl => {
+    :postgres => {
+      :comment => "Increase shared memory for postgres",
+      :parameters => {
+        "kernel.shmmax" => 9 * 1024 * 1024 * 1024,
+        "kernel.shmall" => 9 * 1024 * 1024 * 1024 / 4096
+      }
+    }
+  },
+  :tile => {
+    :database => {
+      :cluster => "12/main",
+      :postgis => "3"
+    },
+    :styles => {
+      :default => {
+        :revision => "v5.0.0",
+        :tile_directories => [
+          { :name => "/store/tiles/default", :min_zoom => 0, :max_zoom => 19 }
+        ]
+      }
+    }
   }
 )
 
 run_list(
-  "role[scaleway]"
+  "role[scaleway]",
+  "role[tile]"
 )
