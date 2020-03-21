9b13010a7baff071a202ffa523e112e390b463dc
Tom Hughes <tom@compton.nu>
2020-03-21 14:48:29 +0000

    Make postgres 12 and postgis 3 the default for tile servers

--- cookbooks/tile/attributes/default.rb
+++ cookbooks/tile/attributes/default.rb
@@ -1,5 +1,5 @@
-default[:tile][:database][:cluster] = "10/main"
-default[:tile][:database][:postgis] = "2.4"
+default[:tile][:database][:cluster] = "12/main"
+default[:tile][:database][:postgis] = "3"
 default[:tile][:database][:node_file] = "/store/database/nodes"
 default[:tile][:database][:multi_geometry] = true
 default[:tile][:database][:hstore] = true
@@ -9,6 +9,6 @@ default[:tile][:database][:tag_transform_script] = nil
 default[:tile][:data] = {}
 default[:tile][:styles] = {}
 
-default[:postgresql][:versions] |= ["10"]
+default[:postgresql][:versions] |= ["12"]
 
 default[:accounts][:users][:tile][:status] = :role
