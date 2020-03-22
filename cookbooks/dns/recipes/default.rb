d16a271815bbfa1c2d5f38f106db0c7af7291a52
Tom Hughes <tom@compton.nu>
2020-03-22 19:27:40 +0000

    Update dnscontrol to 3.0.0

--- cookbooks/dns/recipes/default.rb
+++ cookbooks/dns/recipes/default.rb
@@ -39,7 +39,7 @@ package %w[
 
 remote_file "/usr/local/bin/dnscontrol" do
   action :create
-  source "https://github.com/StackExchange/dnscontrol/releases/download/v2.11/dnscontrol-Linux"
+  source "https://github.com/StackExchange/dnscontrol/releases/download/v3.0.0/dnscontrol-Linux"
   owner "root"
   group "root"
   mode 0o755
