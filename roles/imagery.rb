bae514454a33da6ee5eade431f7db9fa691330ff
Grant Slater <git@firefishy.com>
2020-03-24 21:49:32 +0000

    imagery: enable lu imagery

--- roles/imagery.rb
+++ roles/imagery.rb
@@ -55,5 +55,6 @@ run_list(
   "recipe[imagery::gb_surrey_aerial]",
   "recipe[imagery::za_ngi_topo]",
   "recipe[imagery::za_coct_aerial]",
-  "recipe[imagery::na_sgswa_topo]"
+  "recipe[imagery::na_sgswa_topo]",
+  "recipe[imagery::lu_ngl_dtm]"
 )
