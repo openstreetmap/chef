b1deb24e996c8209467984a74a3b675085aed328
Grant Slater <git@firefishy.com>
2020-03-26 21:35:36 +0000

    imagery: fix delete service action to match create

--- cookbooks/imagery/resources/site.rb
+++ cookbooks/imagery/resources/site.rb
@@ -143,13 +143,15 @@ action :create do
 end
 
 action :delete do
-  service "mapserv-fcgi-#{new_resource.site}" do
-    provider Chef::Provider::Service::Systemd
-    action [:stop, :disable]
-  end
+  %w[0 1 2 3 4 5 6 7].each do |index|
+    service "mapserv-fcgi-#{new_resource.site}-#{index}" do
+      provider Chef::Provider::Service::Systemd
+      action [:stop, :disable]
+    end
 
-  systemd_service "mapserv-fcgi-#{new_resource.site}" do
-    action :delete
+    systemd_service "mapserv-fcgi-#{new_resource.site}-#{index}" do
+      action :delete
+    end
   end
 
   nginx_site new_resource.site do
