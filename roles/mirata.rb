9f6af8c7159a9689d76aa46d7156c44df45bfcd3
heroku-miraheze <61781343+heroku-miraheze@users.noreply.github.com>
2020-05-03 14:27:04 +0200

    Create mirata.rb

---
+++ roles/mirata.rb
@@ -0,0 +1,38 @@
+name "mirata"
+description "Master role applied to mirata"
+
+default_attributes(
+  :hardware => {
+    :shm_size => "18g"
+  },
+  :networking => {
+    :interfaces => {
+      :external_ipv4 => {
+        :interface => "ens3",
+        :role => :external,
+        :family => :inet,
+        :address => "213.177.192.8",
+        :prefix => "28",
+        :gateway => "213.177.192.12"
+      }
+    }
+  },
+  :squid => {
+    :version => 4,
+    :cache_mem => "16384 MB",
+    :cache_dir => [
+      "rock /store/squid/rock-4096 12800 swap-timeout=200 slot-size=4096 max-size=3996",
+      "rock /store/squid/rock-8192 16000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
+      "rock /store/squid/rock-16384 22400 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
+      "rock /store/squid/rock-32768 22800 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
+    ]
+  },
+  :tilecache => {
+    :tile_parent => "madrid.render.openstreetmap.org"
+  }
+)
+
+run_list(
+  "role[masmovil]",
+  "role[tilecache]"
+)
