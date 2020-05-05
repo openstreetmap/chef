a74b0487fe7b9ca6d7220fa2d31bb188e10aa838
Tom Hughes <tom@compton.nu>
2020-05-05 18:40:36 +0100

    Monkey patch git provider to add --force when fetching tags

---
+++ cookbooks/chef/libraries/git.rb
@@ -0,0 +1,13 @@
+module OpenStreetMap
+  module Provider
+    module Git
+      def git(*args, **run_opts)
+        args.push("--force") if args.first == "fetch" && args.last == "--tags"
+
+        super(args, run_opts)
+      end
+    end
+  end
+end
+
+Chef::Provider::Git.prepend(OpenStreetMap::Provider::Git)
