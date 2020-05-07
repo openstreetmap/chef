bd9b2b0eb8b47c6a3ca6617a4dcdf821fde761f0
Tom Hughes <tom@compton.nu>
2020-05-08 00:06:32 +0100

    Update chef client to 15.10.12

--- cookbooks/chef/attributes/default.rb
+++ cookbooks/chef/attributes/default.rb
@@ -5,4 +5,4 @@ default[:apt][:sources] = node[:apt][:sources] | ["opscode"]
 default[:chef][:server][:version] = "12.17.33"
 
 # Set the default client version
-default[:chef][:client][:version] = "14.14.29"
+default[:chef][:client][:version] = "15.10.12"
