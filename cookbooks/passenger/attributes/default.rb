59e4ebe405e91d048e14a794f9f7b5bd24a6c8c8
Tom Hughes <tom@compton.nu>
2020-05-17 18:25:54 +0000

    Use ruby 2.7 on Ubuntu 20.04

--- cookbooks/passenger/attributes/default.rb
+++ cookbooks/passenger/attributes/default.rb
@@ -1,4 +1,9 @@
-default[:passenger][:ruby_version] = "2.5"
+if node[:lsb][:release].to_f < 20.04
+  default[:passenger][:ruby_version] = "2.5"
+else
+  default[:passenger][:ruby_version] = "2.7"
+end
+
 default[:passenger][:max_pool_size] = 6
 default[:passenger][:pool_idle_time] = 300
 default[:passenger][:instance_registry_dir] = "/run/passenger"
