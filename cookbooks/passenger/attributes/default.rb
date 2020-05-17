4794ca67bef30bd8247e18a9d12e7ece170c8c21
Tom Hughes <tom@compton.nu>
2020-05-17 19:35:23 +0100

    Fix cookstyle warnings

--- cookbooks/passenger/attributes/default.rb
+++ cookbooks/passenger/attributes/default.rb
@@ -1,8 +1,8 @@
-if node[:lsb][:release].to_f < 20.04
-  default[:passenger][:ruby_version] = "2.5"
-else
-  default[:passenger][:ruby_version] = "2.7"
-end
+default[:passenger][:ruby_version] = if node[:lsb][:release].to_f < 20.04
+                                       "2.5"
+                                     else
+                                       "2.7"
+                                     end
 
 default[:passenger][:max_pool_size] = 6
 default[:passenger][:pool_idle_time] = 300
