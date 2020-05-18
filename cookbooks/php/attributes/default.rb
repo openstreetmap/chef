f55c80c586ce03dcff307bd37d5422cabf2161a3
Tom Hughes <tom@compton.nu>
2020-05-18 15:22:36 +0100

    Use PHP 7.4 on Ubuntu 20.04

--- cookbooks/php/attributes/default.rb
+++ cookbooks/php/attributes/default.rb
@@ -1,1 +1,5 @@
-default[:php][:version] = "7.2"
+default[:php][:version] = if node[:lsb][:release].to_f < 20.04
+                            "7.2"
+                          else
+                            "7.4"
+                          end
