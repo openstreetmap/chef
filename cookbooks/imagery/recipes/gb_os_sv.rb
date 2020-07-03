9b54c53f363126cd032ad6c33cf09274f343d742
Grant Slater <git@firefishy.com>
2020-07-03 01:00:51 +0100

    imagery: update os om local key

--- cookbooks/imagery/recipes/gb_os_sv.rb
+++ cookbooks/imagery/recipes/gb_os_sv.rb
@@ -1063,7 +1063,8 @@ imagery_layer "gb_os_om_local_2020_04" do
   projection "EPSG:27700"
   source "/data/imagery/gb/openmap-local/2020-04/os-openmap-local-2020-04-combined-sea.vrt"
   copyright "Contains OS data &copy; Crown copyright and database right 2020"
+  revision 1
   background_colour "213 244 248" # OS OpenMap Local Water Blue
   extension "os_om_local_png"
-  url_aliases ["/om-local-2020-04"]
+  url_aliases ["/om-local-2020-04", "/om-local"]
 end
