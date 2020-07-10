b42aa45f75653c1e8cdba875ee64bc14709b473a
Grant Slater <git@firefishy.com>
2020-07-10 14:21:30 +0100

    imagery: update OS OpenMap Local to use averaged overview

--- cookbooks/imagery/recipes/gb_os_sv.rb
+++ cookbooks/imagery/recipes/gb_os_sv.rb
@@ -1061,9 +1061,9 @@ imagery_layer "gb_os_om_local_2020_04" do
   site "os.openstreetmap.org"
   title "OS OpenMap Local - April 2020"
   projection "EPSG:27700"
-  source "/data/imagery/gb/openmap-local/2020-04/os-openmap-local-2020-04-combined-sea.vrt"
+  source "/data/imagery/gb/openmap-local/2020-04/os-openmap-local-2020-04-combined-sea-average-zstd22.vrt"
   copyright "Contains OS data &copy; Crown copyright and database right 2020"
-  revision 1
+  revision 2
   background_colour "213 244 248" # OS OpenMap Local Water Blue
   extension "os_om_local_png"
   url_aliases ["/om-local-2020-04", "/om-local"]
