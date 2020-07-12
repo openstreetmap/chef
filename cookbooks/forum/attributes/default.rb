b0b2743589c084b4fbc6260b05126690348441e5
Tom Hughes <tom@compton.nu>
2020-07-12 19:45:54 +0100

    Move forum PHP configuration options to FPM configuration

--- cookbooks/forum/attributes/default.rb
+++ cookbooks/forum/attributes/default.rb
@@ -1,2 +1,8 @@
 # Enable the "forum" role
 default[:accounts][:users][:forum][:status] = :role
+
+# Configure PHP options
+default[:php][:fpm][:options][:open_basedir] = "/srv/forum.openstreetmap.org/html/:/usr/share/php/:/tmp/"
+default[:php][:fpm][:options][:disable_functions] = "exec,shell_exec,system,passthru,popen,proc_open"
+default[:php][:fpm][:options][:upload_max_filesize] = "70M"
+default[:php][:fpm][:options][:post_max_size] = "100M"
