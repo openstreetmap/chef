a86724a8db309dcdafff46b762171af67b1bb05e
Tom Hughes <tom@compton.nu>
2020-07-29 00:34:11 +0100

    Fix interface name for kessie

--- roles/kessie.rb
+++ roles/kessie.rb
@@ -23,14 +23,14 @@ default_attributes(
   :networking => {
     :interfaces => {
       :external_ipv4 => {
-        :interface => "em1",
+        :interface => "enp2s0f0",
         :role => :external,
         :family => :inet,
         :address => "178.250.74.36",
         :hwaddress => "d8:d3:85:5d:87:5e"
       },
       :external_ipv6 => {
-        :interface => "em1",
+        :interface => "enp2s0f0",
         :role => :external,
         :family => :inet6,
         :address => "2a02:1658:4:0:dad3:85ff:fe5d:875e",
