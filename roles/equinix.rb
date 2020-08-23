af59a82e05fa288b68e1b998beed8ee97ff8dcd2
Tom Hughes <tom@compton.nu>
2020-08-23 10:29:15 +0100

    Switch readonly load at Equinix to snap-01

--- roles/equinix.rb
+++ roles/equinix.rb
@@ -32,7 +32,7 @@ default_attributes(
   :web => {
     :backends => %w[rails1 rails2 rails3],
     :fileserver => "ironbelly",
-    :readonly_database_host => "karm.ams.openstreetmap.org",
+    :readonly_database_host => "snap-01.ams.openstreetmap.org",
     :primary_cluster => true
   }
 )
