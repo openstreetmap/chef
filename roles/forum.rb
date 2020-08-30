7de853b281eccfadf0459936da6b00f7d3cb598b
Grant Slater <git@firefishy.com>
2020-08-30 22:59:55 +0100

    forum: switch to apache mpm event

--- roles/forum.rb
+++ roles/forum.rb
@@ -3,10 +3,14 @@ description "Role applied to all forum servers"
 
 default_attributes(
   :apache => {
+    :mpm => "event",
     :timeout => 60,
-    :keepalive => false,
-    :worker => {
-      :max_request_workers => 250
+    :event => {
+      :server_limit => 18,
+      :max_request_workers => 450,
+      :min_spare_threads => 50,
+      :max_spare_threads => 150,
+      :listen_cores_buckets_ratio => 4
     }
   }
 )
