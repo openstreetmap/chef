1f7623e527fcda92f5e326c99a65515d3a81312f
Tom Hughes <tom@compton.nu>
2020-09-15 17:23:21 +0100

    Remove a few legacy settings

--- roles/db.rb
+++ roles/db.rb
@@ -50,7 +50,6 @@ default_attributes(
         :cpu_tuple_cost => "0.1",
         :log_min_duration_statement => "1000",
         :late_authentication_rules => [
-          { :address => "10.0.16.0/20" },
           { :address => "10.0.32.0/20" },
           { :address => "10.0.48.0/20" }
         ]
