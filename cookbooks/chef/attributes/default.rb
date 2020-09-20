dda82d79b5db092e3b7d436efe42a2ab4e15077f
Tom Hughes <tom@compton.nu>
2020-09-20 12:53:21 +0100

    Update chef client to 16.5.64

--- cookbooks/chef/attributes/default.rb
+++ cookbooks/chef/attributes/default.rb
@@ -5,4 +5,4 @@ default[:apt][:sources] = node[:apt][:sources] | ["opscode"]
 default[:chef][:server][:version] = "12.17.33"
 
 # Set the default client version
-default[:chef][:client][:version] = "16.4.38"
+default[:chef][:client][:version] = "16.5.64"
