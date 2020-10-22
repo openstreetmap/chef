add025cb00ec3601515b7571cf8b0e4a56c30a4a
Tom Hughes <tom@compton.nu>
2020-10-22 21:44:50 +0100

    Update interface names for grindtooth

--- roles/grindtooth.rb
+++ roles/grindtooth.rb
@@ -5,13 +5,13 @@ default_attributes(
   :networking => {
     :interfaces => {
       :internal_ipv4 => {
-        :interface => "em1.2801",
+        :interface => "enp3s0f0.2801",
         :role => :internal,
         :family => :inet,
         :address => "10.0.0.19"
       },
       :external_ipv4 => {
-        :interface => "em1.2800",
+        :interface => "enp3s0f0.2800",
         :role => :external,
         :family => :inet,
         :address => "193.60.236.15"
