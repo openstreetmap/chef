920f26a63594e42d9e9689ec866493c78712c513
Tom Hughes <tom@compton.nu>
2020-12-05 17:59:35 +0000

    Update piwik configuration

--- cookbooks/piwik/attributes/default.rb
+++ cookbooks/piwik/attributes/default.rb
@@ -1,13 +1,13 @@
 default[:piwik][:version] = "4.0.4"
 default[:piwik][:plugins] = %w[
   Actions Annotations API BulkTracking Contents CoreAdminHome CoreConsole
-  CoreHome CorePluginsAdmin CoreUpdater CoreVisualizations CustomPiwikJs
-  CustomVariables Dashboard DevicePlugins DevicesDetection Diagnostics Ecommerce
-  Events Feedback GeoIp2 Goals Heartbeat ImageGraph Insights Installation Intl
-  LanguagesManager Live Login Marketplace MobileMessaging Monolog Morpheus
-  MultiSites Overlay PrivacyManager ProfessionalServices Provider Proxy
-  Referrers Resolution RssWidget ScheduledReports SegmentEditor SEO SitesManager
-  Transitions UserCountry UserCountryMap UserId UserLanguage UsersManager
-  VisitFrequency VisitorInterest VisitsSummary VisitTime WebsiteMeasurable
-  Widgetize
+  CoreHome CorePluginsAdmin CoreUpdater CoreVisualizations CustomJsTracker
+  CustomVariables Dashboard DevicePlugins DevicesDetection Diagnostics
+  Ecommerce Events Feedback GeoIp2 Goals Heartbeat ImageGraph Insights
+  Installation Intl LanguagesManager Live Login Marketplace MobileMessaging
+  Monolog Morpheus MultiSites Overlay PagePerformance PrivacyManager
+  ProfessionalServices Provider Proxy Referrers Resolution RssWidget
+  ScheduledReports SegmentEditor SEO SitesManager Transitions UserCountry
+  UserCountryMap UserId UserLanguage UsersManager VisitFrequency
+  VisitorInterest VisitsSummary VisitTime WebsiteMeasurable Widgetize
 ]
