ef3e963c0c8532dcc2d2c132138e7e220db17b9f
Tom Hughes <tom@compton.nu>
2021-01-15 17:46:55 +0000

    Add promscale for long term storage of prometheus data

--- cookbooks/prometheus/recipes/server.rb
+++ cookbooks/prometheus/recipes/server.rb
@@ -20,6 +20,7 @@
 include_recipe "apache"
 include_recipe "apt"
 include_recipe "networking"
+include_recipe "timescaledb"
 
 passwords = data_bag_item("prometheus", "passwords")
 tokens = data_bag_item("prometheus", "tokens")
@@ -32,6 +33,112 @@ prometheus_exporter "fastly" do
 end
 
 package "prometheus"
+
+promscale_version = "0.1.4"
+
+database_cluster = node[:timescaledb][:cluster]
+
+postgresql_user "prometheus" do
+  cluster database_cluster
+  createrole true
+end
+
+postgresql_database "promscale" do
+  cluster database_cluster
+  owner "prometheus"
+end
+
+postgresql_extension "timescaledb" do
+  cluster database_cluster
+  database "promscale"
+end
+
+directory "/opt/promscale" do
+  owner "root"
+  group "root"
+  mode "755"
+end
+
+package %w[
+  make
+  gcc
+  clang-9
+  llvm-9
+  cargo
+]
+
+git "/opt/promscale/extension" do
+  action :sync
+  repository "https://github.com/timescale/promscale_extension.git"
+  revision "0.1.1"
+  user "root"
+  group "root"
+end
+
+execute "/opt/promscale/extension/Makefile" do
+  action :nothing
+  command "make install"
+  cwd "/opt/promscale/extension"
+  user "root"
+  group "root"
+  subscribes :run, "git[/opt/promscale/extension]", :immediate
+  notifies :restart, "service[postgresql]", :immediate
+end
+
+directory "/opt/promscale/bin" do
+  owner "root"
+  group "root"
+  mode "755"
+end
+
+remote_file "/opt/promscale/bin/promscale" do
+  action :create
+  source "https://github.com/timescale/promscale/releases/download/#{promscale_version}/promscale_#{promscale_version}_Linux_x86_64"
+  owner "root"
+  group "root"
+  mode "755"
+end
+
+systemd_service "promscale" do
+  description "Promscale Connector"
+  type "simple"
+  user "prometheus"
+  exec_start "/opt/promscale/bin/promscale --db-host /run/postgresql --db-port 5432 --db-user prometheus --db-name promscale --db-connections-max 400"
+#  exec_start lazy { "/opt/promscale/bin/promscale --db-host /run/postgresql --db-port #{node[:postgresql][:clusters][database_cluster][:port]} --db-user prometheus --db-name promscale --db-max-connections 400" }
+  private_tmp true
+  protect_system "strict"
+  protect_home true
+  no_new_privileges true
+end
+
+service "promscale" do
+  action [:enable, :start]
+  subscribes :restart, "remote_file[/opt/promscale/bin/promscale]"
+  subscribes :restart, "systemd_service[promscale]"
+end
+
+postgresql_extension "promscale" do
+  cluster database_cluster
+  database "promscale"
+  notifies :restart, "service[promscale]"
+end
+
+systemd_service "promscale-maintenance" do
+  description "Promscale Maintenace"
+  type "simple"
+  user "prometheus"
+  exec_start "/usr/bin/psql --command='CALL prom_api.execute_maintenance()' promscale"
+  private_tmp true
+  protect_system "strict"
+  protect_home true
+  no_new_privileges true
+end
+
+systemd_timer "promscale-maintenance" do
+  description "Promscale Maintenace"
+  on_active_sec 1800
+  on_unit_inactive_sec 1800
+end
 
 jobs = {}
 
