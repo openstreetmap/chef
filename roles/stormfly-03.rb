143160f8b3b63f5a43f02c41ec07e782b71f23b4
Tom Hughes <tom@compton.nu>
2021-01-27 19:15:53 +0000

    Repurpose stormfly-03 as a prometheus server

--- roles/stormfly-03.rb
+++ roles/stormfly-03.rb
@@ -2,9 +2,6 @@ name "stormfly-03"
 description "Master role applied to stormfly-03"
 
 default_attributes(
-  :hardware => {
-    :shm_size => "38g"
-  },
   :networking => {
     :interfaces => {
       :external_ipv4 => {
@@ -24,23 +21,6 @@ default_attributes(
       }
     }
   },
-  :squid => {
-    :version => 4,
-    :cache_mem => "32768 MB",
-    :cache_dir => [
-      "rock /store/squid/rock-4096 20000 swap-timeout=200 slot-size=4096 max-size=3996",
-      "rock /store/squid/rock-8192 25000 swap-timeout=200 slot-size=8192 min-size=3997 max-size=8092",
-      "rock /store/squid/rock-16384 35000 swap-timeout=200 slot-size=16384 min-size=8093 max-size=16284",
-      "rock /store/squid/rock-32768 45000 swap-timeout=200 slot-size=32768 min-size=16285 max-size=262144"
-    ]
-  },
-  :nginx => {
-    :cache => {
-      :proxy => {
-        :max_size => "65536M"
-      }
-    }
-  },
   :tilecache => {
     :tile_parent => "corvallis.render.openstreetmap.org"
   }
@@ -49,5 +29,5 @@ default_attributes(
 run_list(
   "role[osuosl]",
   "role[hp-g9]",
-  "role[tilecache]"
+  "role[prometheus]"
 )
