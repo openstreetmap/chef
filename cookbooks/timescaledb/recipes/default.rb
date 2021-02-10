e39ce14712633ee84052cfec5de1b3aa6a02e877
Tom Hughes <tom@compton.nu>
2021-02-10 22:35:17 +0000

    Move promscale tuning to role and tune WAL sizes

--- cookbooks/timescaledb/recipes/default.rb
+++ cookbooks/timescaledb/recipes/default.rb
@@ -26,31 +26,11 @@ package %W[
   timescaledb-2-postgresql-#{database_version}
 ]
 
-memory_gb = (node[:memory][:total].to_f / 1024 / 1024).ceil
-
 node.default_unless[:postgresql][:versions] = []
 node.default[:postgresql][:versions] |= [database_version]
 node.default[:postgresql][:monitor_tables] = false
-node.default[:postgresql][:settings][database_version][:max_connections] = 500
-node.default[:postgresql][:settings][database_version][:shared_buffers] = "#{memory_gb / 4}GB"
-node.default[:postgresql][:settings][database_version][:work_mem] = "#{memory_gb * 128 / 50 / node[:cpu][:total]}MB"
-node.default[:postgresql][:settings][database_version][:maintenance_work_mem] = "2GB"
-node.default[:postgresql][:settings][database_version][:effective_io_concurrency] = "200"
-node.default[:postgresql][:settings][database_version][:max_worker_processes] = node[:cpu][:total] + node[:timescaledb][:max_background_workers] + 3
-node.default[:postgresql][:settings][database_version][:max_parallel_workers_per_gather] = node[:cpu][:total] / 2
-node.default[:postgresql][:settings][database_version][:max_parallel_workers] = node[:cpu][:total]
-node.default[:postgresql][:settings][database_version][:wal_buffers] = "16MB"
-node.default[:postgresql][:settings][database_version][:max_wal_size] = "1GB"
-node.default[:postgresql][:settings][database_version][:min_wal_size] = "512MB"
-node.default[:postgresql][:settings][database_version][:checkpoint_completion_target] = "0.9"
-node.default[:postgresql][:settings][database_version][:random_page_cost] = "1.1"
-node.default[:postgresql][:settings][database_version][:effective_cache_size] = "#{memory_gb * 3 / 4}GB"
-node.default[:postgresql][:settings][database_version][:default_statistics_target] = "500"
-node.default[:postgresql][:settings][database_version][:autovacuum_max_workers] = "10"
-node.default[:postgresql][:settings][database_version][:autovacuum_naptime] = "10"
 node.default_unless[:postgresql][:settings][database_version][:shared_preload_libraries] = []
 node.default[:postgresql][:settings][database_version][:shared_preload_libraries] |= ["timescaledb"]
-node.default[:postgresql][:settings][database_version][:max_locks_per_transaction] = "512"
 node.default_unless[:postgresql][:settings][database_version][:customized_options] = {}
 node.default[:postgresql][:settings][database_version][:customized_options]["timescaledb.max_background_workers"] = node[:timescaledb][:max_background_workers]
 
