33495d55e31808c9fdde426a1b7bb936681a4c57
Sarah Hoffmann <lonvia@denofr.de>
2021-02-12 20:04:50 +0100

    nominatim: switch to nominatim-ui fork on git.osm.org

--- cookbooks/nominatim/attributes/default.rb
+++ cookbooks/nominatim/attributes/default.rb
@@ -9,7 +9,7 @@ default[:nominatim][:repository] = "https://git.openstreetmap.org/public/nominat
 default[:nominatim][:revision] = "master"
 default[:nominatim][:enable_backup] = false
 default[:nominatim][:enable_git_updates] = true
-default[:nominatim][:ui_repository] = "https://github.com/osm-search/nominatim-ui"
+default[:nominatim][:ui_repository] = "https://git.openstreetmap.org/public/nominatim-ui.git"
 default[:nominatim][:ui_revision] = "master"
 
 default[:nominatim][:fpm_pools] = {
