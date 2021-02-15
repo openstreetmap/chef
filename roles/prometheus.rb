f7dd6716f0f63c8f74b2a14efe19672a0853c6c2
Tom Hughes <tom@compton.nu>
2021-02-15 18:29:22 +0000

    Log autovacuums for promscale

--- roles/prometheus.rb
+++ roles/prometheus.rb
@@ -20,6 +20,7 @@ default_attributes(
         :random_page_cost => "1.1",
         :effective_cache_size => "144GB",
         :default_statistics_target => "500",
+        :log_autovacuum_min_duration => "0",
         :autovacuum_max_workers => "10",
         :autovacuum_naptime => "10",
         :max_locks_per_transaction => "512"
