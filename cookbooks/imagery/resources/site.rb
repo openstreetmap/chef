94d510fe3242731a3a91e5f68eba46d596865657
Tom Hughes <tom@compton.nu>
2021-02-18 16:41:05 +0000

    Tighten memory limits for imagery servers

--- cookbooks/imagery/resources/site.rb
+++ cookbooks/imagery/resources/site.rb
@@ -107,8 +107,8 @@ action :create do
                   "GDAL_CACHEMAX" => "512"
       limit_nofile 16384
       limit_cpu 180
-      memory_high "1G"
-      memory_max "2G"
+      memory_high "512M"
+      memory_max "1G"
       user "imagery"
       group "imagery"
       exec_start_pre "/bin/rm -f /run/mapserver-fastcgi/layer-#{new_resource.site}-#{index}.socket"
