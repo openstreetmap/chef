06a9c9178079c6c4038e4cd05533819a7c02ca1a
Grant Slater <git@firefishy.com>
2021-05-10 15:18:43 +0100

    dns: Update dnscontrol to 3.8.1

--- cookbooks/dns/recipes/default.rb
+++ cookbooks/dns/recipes/default.rb
@@ -41,7 +41,7 @@ package %w[
 
 remote_file "/usr/local/bin/dnscontrol" do
   action :create
-  source "https://github.com/StackExchange/dnscontrol/releases/download/v3.8.0/dnscontrol-Linux"
+  source "https://github.com/StackExchange/dnscontrol/releases/download/v3.8.1/dnscontrol-Linux"
   owner "root"
   group "root"
   mode "755"
