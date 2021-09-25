923a6c9ed709bc4ac17c412b2ca265b8e3f49c9f
Tom Hughes <tom@compton.nu>
2021-09-26 00:14:41 +0100

    Configure bind and dhcp for Dublin

--- roles/fafnir.rb
+++ roles/fafnir.rb
@@ -2,6 +2,13 @@ name "fafnir"
 description "Master role applied to fafnir"
 
 default_attributes(
+  :bind => {
+    :clients => "equinix-dub"
+  },
+  :dhcpd => {
+    :first_address => "10.0.79.1",
+    :last_address => "10.0.79.254"
+  },
   :networking => {
     :interfaces => {
       :internal_ipv4 => {
