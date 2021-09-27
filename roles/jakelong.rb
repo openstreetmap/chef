d95e601da5219ee70f08d20237713c75b8804645
Tom Hughes <tom@compton.nu>
2021-09-27 13:33:47 +0100

    Add roles for gorwen and jakelong

---
+++ roles/jakelong.rb
@@ -0,0 +1,37 @@
+name "jakelong"
+description "Master role applied to jakelong"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal_ipv4 => {
+        :interface => "bond0",
+        :role => :internal,
+        :family => :inet,
+        :address => "10.0.64.12",
+        :bond => {
+          :mode => "802.3ad",
+          :lacprate => "fast",
+          :xmithashpolicy => "layer3+4",
+          :slaves => %w[eno1 eno2 eno3 eno4 ens1f0 ens1f1]
+        }
+      },
+      :external_ipv4 => {
+        :interface => "bond0.101",
+        :role => :external,
+        :family => :inet,
+        :address => "184.104.226.108"
+      },
+      :external_ipv6 => {
+        :interface => "bond0.101",
+        :role => :external,
+        :family => :inet6,
+        :address => "2001:470:1:b3b::c"
+      }
+    }
+  }
+)
+
+run_list(
+  "role[equinix-dub]"
+)
