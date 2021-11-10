664be70c80555f4efb20bdb6a838fa9215d8a727
Tom Hughes <tom@compton.nu>
2021-11-10 19:19:49 +0000

    Add roles for horntail and norbert

---
+++ roles/horntail.rb
@@ -0,0 +1,37 @@
+name "horntail"
+description "Master role applied to horntail"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal_ipv4 => {
+        :interface => "bond0",
+        :role => :internal,
+        :family => :inet,
+        :address => "10.0.64.10",
+        :bond => {
+          :mode => "802.3ad",
+          :lacprate => "fast",
+          :xmithashpolicy => "layer3+4",
+          :slaves => %w[eno1 eno2 eno3 eno4]
+        }
+      },
+      :external_ipv4 => {
+        :interface => "bond0.101",
+        :role => :external,
+        :family => :inet,
+        :address => "184.104.226.106"
+      },
+      :external_ipv6 => {
+        :interface => "bond0.101",
+        :role => :external,
+        :family => :inet6,
+        :address => "2001:470:1:b3b::a"
+      }
+    }
+  }
+)
+
+run_list(
+  "role[equinix-dub]"
+)
---
+++ roles/norbert.rb
@@ -0,0 +1,35 @@
+name "norbert"
+description "Master role applied to norbert"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal_ipv4 => {
+        :interface => "bond0",
+        :role => :internal,
+        :family => :inet,
+        :address => "10.0.48.17",
+        :bond => {
+          :slaves => %w[eno1 eno2 eno3 eno4]
+        }
+      },
+      :external_ipv4 => {
+        :interface => "bond0.2",
+        :role => :external,
+        :family => :inet,
+        :address => "130.117.76.17"
+      },
+      :external_ipv6 => {
+        :interface => "bond0.2",
+        :role => :external,
+        :family => :inet6,
+        :address => "2001:978:2:2C::172:11"
+      }
+
+    }
+  }
+)
+
+run_list(
+  "role[equinix-ams]"
+)
