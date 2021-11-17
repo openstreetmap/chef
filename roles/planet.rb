2babb3f7b81746ad81746eab3c8288bc30c9cb71
Tom Hughes <tom@compton.nu>
2021-11-17 08:37:30 +0000

    Order attributes

--- roles/planet.rb
+++ roles/planet.rb
@@ -2,6 +2,23 @@ name "planet"
 description "Role applied to all planet servers"
 
 default_attributes(
+  :apache => {
+    :mpm => "event",
+    :keepalive => true,
+    :event => {
+      :server_limit => 20,
+      :max_request_workers => 1000,
+      :threads_per_child => 50,
+      :min_spare_threads => 75,
+      :max_spare_threads => 525,
+      :listen_cores_buckets_ratio => 4
+    }
+  },
+  :networking => {
+    :firewall => {
+      :http_connection_limit => 10
+    }
+  },
   :rsyncd => {
     :modules => {
       :planet => {
@@ -20,23 +37,6 @@ default_attributes(
         :timeout => 3600,
         :refuse_options => ["checksum"]
       }
-    }
-  },
-  :networking => {
-    :firewall => {
-      :http_connection_limit => 10
-    }
-  },
-  :apache => {
-    :mpm => "event",
-    :keepalive => true,
-    :event => {
-      :server_limit => 20,
-      :max_request_workers => 1000,
-      :threads_per_child => 50,
-      :min_spare_threads => 75,
-      :max_spare_threads => 525,
-      :listen_cores_buckets_ratio => 4
     }
   }
 )
