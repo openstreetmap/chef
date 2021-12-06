5a4919337b3d34a106cbc4b1ce6bf5c84a1dae44
Tom Hughes <tom@compton.nu>
2021-12-06 18:03:49 +0000

    Add role for longma

---
+++ roles/longma.rb
@@ -0,0 +1,37 @@
+name "longma"
+description "Master role applied to longma"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal_ipv4 => {
+        :interface => "bond0",
+        :role => :internal,
+        :family => :inet,
+        :address => "10.0.64.13",
+        :bond => {
+          :mode => "802.3ad",
+          :lacprate => "fast",
+          :xmithashpolicy => "layer3+4",
+          :slaves => %w[]
+        }
+      },
+      :external_ipv4 => {
+        :interface => "bond0.101",
+        :role => :external,
+        :family => :inet,
+        :address => "184.104.226.109"
+      },
+      :external_ipv6 => {
+        :interface => "bond0.101",
+        :role => :external,
+        :family => :inet6,
+        :address => "2001:470:1:b3b::d"
+      }
+    }
+  }
+)
+
+run_list(
+  "role[equinix-dub]"
+)
