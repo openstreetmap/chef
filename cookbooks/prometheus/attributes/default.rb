c76d172b32340781d10aba15fce7a309b8f77614
Tom Hughes <tom@compton.nu>
2022-01-18 13:43:01 +0000

    Disable promscale

--- cookbooks/prometheus/attributes/default.rb
+++ cookbooks/prometheus/attributes/default.rb
@@ -3,7 +3,7 @@ default[:prometheus][:exporters] = {}
 default[:prometheus][:snmp] = {}
 default[:prometheus][:metrics] = {}
 default[:prometheus][:files] = []
-default[:prometheus][:promscale] = true
+default[:prometheus][:promscale] = false
 
 if node[:recipes].include?("prometheus::server")
   default[:apt][:sources] |= %w[grafana timescaledb]
