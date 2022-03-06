b9d17fd242de21fb38a36dc97e9aa0dab4f7c973
Tom Hughes <tom@compton.nu>
2022-03-06 12:27:09 +0000

    Drop NFS cookbook

--- cookbooks/nfs/README.md
+++
@@ -1,7 +0,0 @@
-# NFS Cookbook
-
-This cookbook configures NFS (the Network File System) which is used on
-various servers. There are two recipes:
-
-* default: configures NFS clients based on node attributes.
-* server: configures the central NFS server.
--- cookbooks/nfs/metadata.rb
+++
@@ -1,8 +0,0 @@
-name              "nfs"
-maintainer        "OpenStreetMap Administrators"
-maintainer_email  "admins@openstreetmap.org"
-license           "Apache-2.0"
-description       "Installs and configures nfs"
-
-version           "1.0.0"
-supports          "ubuntu"
