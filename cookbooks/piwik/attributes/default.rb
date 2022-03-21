8fe6f6e3ef5e5274f615a4daac4f0762b2e65ff9
Tom Hughes <tom@compton.nu>
2022-03-21 00:24:29 +0000

    Update piwik plugins list

--- cookbooks/piwik/attributes/default.rb
+++ cookbooks/piwik/attributes/default.rb
@@ -1,13 +1,14 @@
 default[:piwik][:version] = "4.8.0"
 default[:piwik][:plugins] = %w[
   Actions Annotations API BulkTracking Contents CoreAdminHome CoreConsole
-  CoreHome CorePluginsAdmin CoreUpdater CoreVisualizations CustomJsTracker
-  CustomVariables Dashboard DevicePlugins DevicesDetection Diagnostics
+  CoreHome CorePluginsAdmin CoreUpdater CoreVisualizations CoreVue
+  CustomJsTracker Dashboard DBStats DevicePlugins DevicesDetection Diagnostics
   Ecommerce Events Feedback GeoIp2 Goals Heartbeat ImageGraph Insights
-  Installation Intl LanguagesManager Live Login Marketplace MobileMessaging
-  Monolog Morpheus MultiSites Overlay PagePerformance PrivacyManager
-  ProfessionalServices Provider Proxy Referrers Resolution RssWidget
-  ScheduledReports SegmentEditor SEO SitesManager Transitions UserCountry
-  UserCountryMap UserId UserLanguage UsersManager VisitFrequency
-  VisitorInterest VisitsSummary VisitTime WebsiteMeasurable Widgetize
+  Installation Intl IntranetMeasurable LanguagesManager Live Login Marketplace
+  MobileAppMeasurable MobileMessaging Monolog Morpheus MultiSites Overlay
+  PagePerformance PrivacyManager ProfessionalServices Proxy Referrers Resolution
+  RssWidget ScheduledReports SegmentEditor SEO SitesManager Tour Transitions
+  TwoFactorAuth UserCountry UserCountryMap UserId UserLanguage UsersManager
+  VisitFrequency VisitorInterest VisitsSummary VisitTime WebsiteMeasurable
+  Widgetize
 ]
