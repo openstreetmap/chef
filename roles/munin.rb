238023c2173dee7b9c835ffc940587be4f0b51b5
Tom Hughes <tom@compton.nu>
2022-05-19 20:31:18 +0100

    Disable munin email alerting

--- roles/munin.rb
+++ roles/munin.rb
@@ -1,16 +1,6 @@
 name "munin"
 description "Role applied to all munin servers"
 
-default_attributes(
-  :munin => {
-    :plugins => {
-      :munin_update => {
-        :contacts => "null"
-      }
-    }
-  }
-)
-
 run_list(
   "recipe[munin::server]"
 )
