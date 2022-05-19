d5121aacd65b72ac4b7c9b9b14e653c9d644e293
Tom Hughes <tom@compton.nu>
2022-05-19 20:49:41 +0100

    Update chef client to 17.10.3

--- cookbooks/chef/attributes/default.rb
+++ cookbooks/chef/attributes/default.rb
@@ -5,4 +5,4 @@ default[:apt][:sources] = node[:apt][:sources] | ["opscode"]
 default[:chef][:server][:version] = "12.17.33"
 
 # Set the default client version
-default[:chef][:client][:version] = "17.7.29"
+default[:chef][:client][:version] = "17.10.3"
