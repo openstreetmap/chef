29b058d82947a8ce172388d30b3cef650cf8de1e
Tom Hughes <tom@compton.nu>
2022-06-29 08:55:41 +0100

    Use ruby 3.0 on Ubuntu 22.04

--- cookbooks/ruby/attributes/default.rb
+++ cookbooks/ruby/attributes/default.rb
@@ -1,6 +1,6 @@
-default[:ruby][:version] = if node[:lsb][:release].to_f < 20.04
-                             "2.5"
-                           else
+default[:ruby][:version] = if node[:lsb][:release].to_f < 22.04
                              "2.7"
+                           else
+                             "3.0"
                            end
 default[:ruby][:bundle] = "/usr/bin/bundle#{node[:ruby][:version]}"
