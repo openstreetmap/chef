e261b6f728d5bfceb9a98d5e35b29915afd49035
Tom Hughes <tom@compton.nu>
2022-07-14 11:38:22 +0000

    Get docker working in the test-kitchen environment

---
+++ test/integration/docker/serverspec/docker_spec.rb
@@ -0,0 +1,13 @@
+require "serverspec"
+
+# Required by serverspec
+set :backend, :exec
+
+describe package("docker-ce") do
+  it { should be_installed }
+end
+
+describe service("docker") do
+  it { should be_enabled }
+  it { should be_running }
+end
