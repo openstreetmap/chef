9cfac467d61744131c15f35ff779a99c731d211d
Tom Hughes <tom@compton.nu>
2022-07-25 22:20:41 +0100

    Remove tile role from albi, necrosan and scorch

--- roles/albi.rb
+++ roles/albi.rb
@@ -21,42 +21,9 @@ default_attributes(
         :gateway => "fe80::a293:51ff:fea2:ded5"
       }
     }
-  },
-  :postgresql => {
-    :settings => {
-      :defaults => {
-        :shared_buffers => "8GB",
-        :maintenance_work_mem => "7144MB",
-        :effective_cache_size => "16GB"
-      }
-    }
-  },
-  :sysctl => {
-    :postgres => {
-      :comment => "Increase shared memory for postgres",
-      :parameters => {
-        "kernel.shmmax" => 9 * 1024 * 1024 * 1024,
-        "kernel.shmall" => 9 * 1024 * 1024 * 1024 / 4096
-      }
-    }
-  },
-  :tile => {
-    :database => {
-      :cluster => "14/main",
-      :postgis => "3"
-    },
-    :mapnik => "3.1",
-    :styles => {
-      :default => {
-        :tile_directories => [
-          { :name => "/store/tiles/default", :min_zoom => 0, :max_zoom => 19 }
-        ]
-      }
-    }
   }
 )
 
 run_list(
-  "role[scaleway]",
-  "role[tile]"
+  "role[scaleway]"
 )
--- roles/necrosan.rb
+++ roles/necrosan.rb
@@ -27,42 +27,9 @@ default_attributes(
         :gateway => "2a05:46c0:100:1004::"
       }
     }
-  },
-  :postgresql => {
-    :settings => {
-      :defaults => {
-        :shared_buffers => "8GB",
-        :maintenance_work_mem => "7144MB",
-        :effective_cache_size => "16GB"
-      }
-    }
-  },
-  :sysctl => {
-    :postgres => {
-      :comment => "Increase shared memory for postgres",
-      :parameters => {
-        "kernel.shmmax" => 9 * 1024 * 1024 * 1024,
-        "kernel.shmall" => 9 * 1024 * 1024 * 1024 / 4096
-      }
-    }
-  },
-  :tile => {
-    :database => {
-      :cluster => "14/main",
-      :postgis => "3"
-    },
-    :mapnik => "3.1",
-    :styles => {
-      :default => {
-        :tile_directories => [
-          { :name => "/store/tiles/default", :min_zoom => 0, :max_zoom => 19 }
-        ]
-      }
-    }
   }
 )
 
 run_list(
-  "role[appliwave]",
-  "role[tile]"
+  "role[appliwave]"
 )
