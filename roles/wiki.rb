72fb961e4150856e029b53375c86e7e647026453
Grant Slater <git@firefishy.com>
2022-08-05 04:26:55 +0300

    wiki: increase memcache memory to 4GB

--- roles/wiki.rb
+++ roles/wiki.rb
@@ -37,7 +37,7 @@ default_attributes(
     ]
   },
   :memcached => {
-    :memory_limit => 2048,
+    :memory_limit => 4096,
     :connection_limit => 8192,
     :chunk_growth_factor => 1.25,
     :min_item_size => 48
