3dd5baca2a498497cb9a96a36259bd94c92e20ea
Grant Slater <github@firefishy.com>
2022-10-01 23:36:47 +0100

    Add oxidized backup daemon

---
+++ cookbooks/oxidized/README.md
@@ -0,0 +1,4 @@
+# oxidized cookbook
+
+This cookbook installs and configures [oxidized](https://github.com/ytti/oxidized) to
+backup the configurations of OpenStreetMap equipment.
---
+++ cookbooks/oxidized/metadata.rb
@@ -0,0 +1,10 @@
+name              "oxidized"
+maintainer        "OpenStreetMap Administrators"
+maintainer_email  "admins@openstreetmap.org"
+license           "Apache-2.0"
+description       "Configures oxidized to backup equipment configuration"
+
+version           "1.0.0"
+supports          "ubuntu"
+depends           "git"
+depends           "ruby"
