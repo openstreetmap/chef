9bb78787b17907354cc60ad73bd738560233c799
Grant Slater <github@firefishy.com>
2022-10-12 10:48:11 +0100

    Only set default_qdisc in base role to prevent yoyo

--- roles/base.rb
+++ roles/base.rb
@@ -74,10 +74,9 @@ default_attributes(
       }
     },
     :default_qdisc => {
-      :comment => "Use fq as the default queuing discipline and cubic for congestion control",
+      :comment => "Use fq as the default queuing discipline",
       :parameters => {
-        "net.core.default_qdisc" => "fq",
-        "net.ipv4.tcp_congestion_control" => "cubic"
+        "net.core.default_qdisc" => "fq"
       }
     },
     :tune_cpu_scheduler => {
