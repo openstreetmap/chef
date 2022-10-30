b6a86046121d80e5c5076ef8482e6b13c35ecae2
Grant Slater <github@firefishy.com>
2022-10-30 16:40:27 +0000

    imagery: add OS OpenMap Local 2022 Oct

--- cookbooks/imagery/recipes/gb_os_sv.rb
+++ cookbooks/imagery/recipes/gb_os_sv.rb
@@ -1165,6 +1165,16 @@ imagery_layer "gb_os_om_local_2022_04" do
   copyright "Contains OS data &copy; Crown copyright and database right 2022"
   background_colour "213 244 248" # OS OpenMap Local Water Blue
   extension "os_om_local_png"
-  url_aliases ["/om-local-2022-04", "/om-local"]
+end
+
+imagery_layer "gb_os_om_local_2022_10" do
+  site "os.openstreetmap.org"
+  title "OS OpenMap Local - October 2022"
+  projection "EPSG:27700"
+  source "/data/imagery/gb/openmap-local/2022-10/os-openmap-local-2022-10.vrt"
+  copyright "Contains OS data &copy; Crown copyright and database right 2022"
+  background_colour "213 244 248" # OS OpenMap Local Water Blue
+  extension "os_om_local_png"
+  url_aliases ["/om-local-2022-10", "/om-local"]
   default_layer true
 end
