2b8fcbaea032fe9a2e5c98a8c63311756809c493
Tom Hughes <tom@compton.nu>
2022-11-09 13:33:43 +0000

    Add role for palulukon

---
+++ roles/palulukon.rb
@@ -0,0 +1,27 @@
+name "palulukon"
+description "Master role applied to palulukon"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :external_ipv4 => {
+        :interface => "ens5",
+        :role => :external,
+        :family => :inet,
+        :address => "172.31.37.101",
+        :prefix => "20",
+        :gateway => "172.31.32.1"
+      }
+    }
+  }
+)
+
+override_attributes(
+  :networking => {
+    :nameservers => ["172.31.0.2"]
+  }
+)
+
+run_list(
+  "role[aws-us-east-2]"
+)
