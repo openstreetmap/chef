fb2cba4f788259c105a1f45609a6287e6c793a62
Tom Hughes <tom@compton.nu>
2022-11-16 19:48:01 +0000

    Fix rails-jobs restart trigger

--- cookbooks/web/recipes/frontend.rb
+++ cookbooks/web/recipes/frontend.rb
@@ -68,19 +68,19 @@ service "rails-jobs@mailers" do
   action [:enable, :start]
   supports :restart => true
   subscribes :restart, "rails_port[www.openstreetmap.org]"
-  subscribes :restart, "systemd_service[rails-jobs]"
+  subscribes :restart, "systemd_service[rails-jobs@]"
 end
 
 service "rails-jobs@storage" do
   action [:enable, :start]
   supports :restart => true
   subscribes :restart, "rails_port[www.openstreetmap.org]"
-  subscribes :restart, "systemd_service[rails-jobs]"
+  subscribes :restart, "systemd_service[rails-jobs@]"
 end
 
 service "rails-jobs@traces" do
   action [:enable, :start]
   supports :restart => true
   subscribes :restart, "rails_port[www.openstreetmap.org]"
-  subscribes :restart, "systemd_service[rails-jobs]"
+  subscribes :restart, "systemd_service[rails-jobs@]"
 end
