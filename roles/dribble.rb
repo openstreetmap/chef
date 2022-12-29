8b58a2b891263945306d5a1cafeb032f74b4bbab
Tom Hughes <tom@compton.nu>
2022-12-29 15:21:25 +0000

    Add roles for dribble and vhagar

---
+++ roles/dribble.rb
@@ -0,0 +1,37 @@
+name "dribble"
+description "Master role applied to dribble"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal_ipv4 => {
+        :interface => "bond0",
+        :role => :internal,
+        :family => :inet,
+        :address => "10.0.48.4",
+        :bond => {
+          :mode => "802.3ad",
+          :lacprate => "fast",
+          :xmithashpolicy => "layer3+4",
+          :slaves => %w[eno1 eno2 eno3 eno4 eno5 eno6]
+        }
+      },
+      :external_ipv4 => {
+        :interface => "bond0.2",
+        :role => :external,
+        :family => :inet,
+        :address => "130.117.76.4"
+      },
+      :external_ipv6 => {
+        :interface => "bond0.2",
+        :role => :external,
+        :family => :inet6,
+        :address => "2001:978:2:2C::172:4"
+      }
+    }
+  }
+)
+
+run_list(
+  "role[equinix-ams]"
+)
