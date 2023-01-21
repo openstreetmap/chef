083dea407581aea5e3bada02a5cd32f83632eb88
Tom Hughes <tom@compton.nu>
2023-01-21 11:26:25 +0000

    Revert "minor civicrm upgrade"
    This reverts commit 8562f344274d9968779f13e24353a850a5b71b39.

--- cookbooks/civicrm/attributes/default.rb
+++ cookbooks/civicrm/attributes/default.rb
@@ -1,8 +1,8 @@
-default[:civicrm][:version] = "5.51.4"
+default[:civicrm][:version] = "5.51.1"
 
 default[:civicrm][:extensions][:cividiscount][:name] = "org.civicrm.module.cividiscount"
 default[:civicrm][:extensions][:cividiscount][:repository] = "https://github.com/dlobo/org.civicrm.module.cividiscount.git"
-default[:civicrm][:extensions][:cividiscount][:revision] = "3.8.7"
+default[:civicrm][:extensions][:cividiscount][:revision] = "3.8.4"
 
 default[:civicrm][:extensions][:osm][:name] = "de.systopia.osm"
 default[:civicrm][:extensions][:osm][:repository] = "https://github.com/systopia/de.systopia.osm.git"
@@ -10,7 +10,7 @@ default[:civicrm][:extensions][:osm][:revision] = "1.3"
 
 default[:civicrm][:extensions][:emailapi][:name] = "org.civicoop.emailapi"
 default[:civicrm][:extensions][:emailapi][:repository] = "https://lab.civicrm.org/extensions/emailapi.git"
-default[:civicrm][:extensions][:emailapi][:revision] = "2.9"
+default[:civicrm][:extensions][:emailapi][:revision] = "2.8"
 
 default[:civicrm][:extensions][:civiruleshttppost][:name] = "org.civicoop.civiruleshttppost"
 default[:civicrm][:extensions][:civiruleshttppost][:repository] = "https://github.com/CiviCooP/org.civicoop.civiruleshttppost.git"
