50600cca037b24dff8f811f546ff8a424bbb8423
Tom Hughes <tom@compton.nu>
2023-01-24 21:11:59 +0000

    Use postgres 15 for OTRS

--- cookbooks/otrs/attributes/default.rb
+++ cookbooks/otrs/attributes/default.rb
@@ -1,13 +1,13 @@
 default[:otrs][:version] = "6.0.30"
 default[:otrs][:user] = "otrs"
 default[:otrs][:group] = nil
-default[:otrs][:database_cluster] = "13/main"
+default[:otrs][:database_cluster] = "15/main"
 default[:otrs][:database_name] = "otrs"
 default[:otrs][:database_user] = "otrs"
 default[:otrs][:database_password] = "otrs"
 default[:otrs][:site] = "otrs"
 
-default[:postgresql][:versions] |= ["13"]
+default[:postgresql][:versions] |= ["15"]
 
 default[:accounts][:users][:otrs][:status] = :role
 default[:accounts][:groups][:"www-data"][:members] = [:otrs]
