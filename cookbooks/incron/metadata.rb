ec3feb22735b9d721f5e1f1e95c4857838a7e710
Tom Hughes <tom@compton.nu>
2023-03-24 16:50:55 +0000

    Drop incron support

--- cookbooks/incron/metadata.rb
+++
@@ -1,8 +0,0 @@
-name              "incron"
-maintainer        "OpenStreetMap Administrators"
-maintainer_email  "admins@openstreetmap.org"
-license           "Apache-2.0"
-description       "Configures incron"
-
-version           "1.0.0"
-supports          "ubuntu"
