a8d4b2e02f9c19795a0a9ddde6f404cbf1446d61
Grant Slater <github@firefishy.com>
2023-05-25 13:57:07 +0300

    Imagery: add OS Local 2023 April per request

--- cookbooks/imagery/recipes/gb_os_sv.rb
+++ cookbooks/imagery/recipes/gb_os_sv.rb
@@ -1175,6 +1175,16 @@ imagery_layer "gb_os_om_local_2022_10" do
   copyright "Contains OS data &copy; Crown copyright and database right 2022"
   background_colour "213 244 248" # OS OpenMap Local Water Blue
   extension "os_om_local_png"
-  url_aliases ["/om-local-2022-10", "/om-local"]
+end
+
+imagery_layer "gb_os_om_local_2023_04" do
+  site "os.openstreetmap.org"
+  title "OS OpenMap Local - April 2023"
+  projection "EPSG:27700"
+  source "/data/imagery/gb/openmap-local/2023-04/os-openmap-local-2023-04.vrt"
+  copyright "Contains OS data &copy; Crown copyright and database right 2023"
+  background_colour "213 244 248" # OS OpenMap Local Water Blue
+  extension "os_om_local_png"
+  url_aliases ["/om-local-2023-04", "/om-local"]
   default_layer true
 end
