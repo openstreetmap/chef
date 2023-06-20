953c94b6908812def145c0732c3d52cef0e9651c
Tom Hughes <tom@compton.nu>
2023-06-20 17:46:56 +0000

    Use ruby 3.1 on Debian

--- cookbooks/ruby/attributes/default.rb
+++ cookbooks/ruby/attributes/default.rb
@@ -1,4 +1,6 @@
-default[:ruby][:version] = if node[:lsb][:release].to_f < 22.04
+default[:ruby][:version] = if platform?("debian")
+                             "3.1"
+                           elsif node[:lsb][:release].to_f < 22.04
                              "2.7"
                            else
                              "3.0"
