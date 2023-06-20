50a65ffb2e499e3f097bec904ffcaa91e6467c87
Tom Hughes <tom@compton.nu>
2023-06-20 19:19:01 +0100

    Fix typo

--- cookbooks/php/attributes/default.rb
+++ cookbooks/php/attributes/default.rb
@@ -1,6 +1,6 @@
 default[:php][:version] = if platform?("debian")
                             "8.2"
-                          elsif qnode[:lsb][:release].to_f < 22.04
+                          elsif node[:lsb][:release].to_f < 22.04
                             "7.4"
                           else
                             "8.1"
