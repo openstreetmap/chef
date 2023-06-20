beba7c025301ae190346f9fb986caf840265c643
Tom Hughes <tom@compton.nu>
2023-06-20 17:42:11 +0000

    Use PHP 8.2 on Debian

--- cookbooks/php/attributes/default.rb
+++ cookbooks/php/attributes/default.rb
@@ -1,4 +1,6 @@
-default[:php][:version] = if node[:lsb][:release].to_f < 22.04
+default[:php][:version] = if platform?("debian")
+                            "8.2"
+                          elsif qnode[:lsb][:release].to_f < 22.04
                             "7.4"
                           else
                             "8.1"
