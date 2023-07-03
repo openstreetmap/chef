6572248a9b1db350db9e1646b06677e7bcf7bcc6
Grant Slater <github@firefishy.com>
2023-07-03 17:01:59 +0100

    Add awscli cookbook

---
+++ cookbooks/awscli/metadata.rb
@@ -0,0 +1,9 @@
+name              "awscli"
+maintainer        "OpenStreetMap Administrators"
+maintainer_email  "admins@openstreetmap.org"
+license           "Apache-2.0"
+description       "Installs and configures awscli"
+
+version           "1.0.0"
+supports          "debian"
+supports          "ubuntu"
