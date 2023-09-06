e995d7744bce90aae044f23434a39b0e7f211dc1
Tom Hughes <tom@compton.nu>
2023-09-06 21:18:28 +0100

    Add role for piasa

---
+++ roles/piasa.rb
@@ -0,0 +1,59 @@
+name "piasa"
+description "Master role applied to piasa"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :external => {
+        :interface => "bond0",
+        :role => :external,
+        :inet => {
+          :address => "140.211.167.101"
+        },
+        :inet6 => {
+          :address => "2605:bc80:3010:700::8cd3:a765"
+        },
+        :bond => {
+          :slaves => %w[eno1 eno2 eno3 eno4 eno5 eno6]
+        }
+      }
+    }
+  },
+  :postgresql => {
+    :settings => {
+      :defaults => {
+        :shared_buffers => "8GB",
+        :maintenance_work_mem => "7144MB",
+        :effective_cache_size => "16GB"
+      }
+    }
+  },
+  :sysctl => {
+    :postgres => {
+      :comment => "Increase shared memory for postgres",
+      :parameters => {
+        "kernel.shmmax" => 9 * 1024 * 1024 * 1024,
+        "kernel.shmall" => 9 * 1024 * 1024 * 1024 / 4096
+      }
+    }
+  },
+  :tile => {
+    :database => {
+      :cluster => "14/main",
+      :postgis => "3"
+    },
+    :mapnik => "3.1",
+    :styles => {
+      :default => {
+        :tile_directories => [
+          { :name => "/store/tiles/default", :min_zoom => 0, :max_zoom => 19 }
+        ]
+      }
+    },
+  }
+)
+
+run_list(
+  "role[osuosl]",
+  "role[tile]"
+)
