90bd81afd1e51d04bd37991af3ea6034ad07c25c
Paul Norman <penorman@mac.com>
2023-09-12 02:34:27 -0700

    Set reasonable PostgreSQL GUCs for tile servers
    These are based off of the osm2pgsql.org recommendations, scaled
    for the larger servers we run, as well as experience running
    PostgreSQL in a large variety of environments.

--- roles/ysera.rb
+++ roles/ysera.rb
@@ -23,8 +23,6 @@ default_attributes(
   :postgresql => {
     :settings => {
       :defaults => {
-        :shared_buffers => "8GB",
-        :maintenance_work_mem => "7144MB",
         :effective_cache_size => "16GB"
       }
     }
