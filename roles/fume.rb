4849412468d7908178ec1b325be875bea740006a
Tom Hughes <tom@compton.nu>
2023-09-24 00:24:33 +0100

    Add roles for fume and grisu

---
+++ roles/fume.rb
@@ -0,0 +1,36 @@
+name "fume"
+description "Master role applied to fume"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal => {
+        :interface => "bond0",
+        :role => :internal,
+        :inet => {
+          :address => "10.0.64.16"
+        },
+        :bond => {
+          :mode => "802.3ad",
+          :lacprate => "fast",
+          :xmithashpolicy => "layer3+4",
+          :slaves => %w[eno1 eno2 eno3 eno4 eno5 eno6]
+        }
+      },
+      :external => {
+        :interface => "bond0.101",
+        :role => :external,
+        :inet => {
+          :address => "184.104.226.112"
+        },
+        :inet6 => {
+          :address => "2001:470:1:b3b::10"
+        }
+      }
+    }
+  }
+)
+
+run_list(
+  "role[equinix-dub]"
+)
---
+++ roles/grisu.rb
@@ -0,0 +1,36 @@
+name "grisu"
+description "Master role applied to grisu"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal => {
+        :interface => "bond0",
+        :role => :internal,
+        :inet => {
+          :address => "10.0.64.17"
+        },
+        :bond => {
+          :mode => "802.3ad",
+          :lacprate => "fast",
+          :xmithashpolicy => "layer3+4",
+          :slaves => %w[eno1 eno2 eno3 eno4 eno5 eno6]
+        }
+      },
+      :external => {
+        :interface => "bond0.101",
+        :role => :external,
+        :inet => {
+          :address => "184.104.226.113"
+        },
+        :inet6 => {
+          :address => "2001:470:1:b3b::11"
+        }
+      }
+    }
+  }
+)
+
+run_list(
+  "role[equinix-dub]"
+)
