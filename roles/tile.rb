4d8c19a35da5652a3c14d88fd8afbba0629e5214
Tom Hughes <tom@compton.nu>
2023-12-01 19:07:35 +0000

    Update to osm-carto v5.8.0
    Closes #639

--- roles/tile.rb
+++ roles/tile.rb
@@ -104,7 +104,7 @@ default_attributes(
     :styles => {
       :default => {
         :repository => "https://github.com/gravitystorm/openstreetmap-carto.git",
-        :revision => "v5.7.0",
+        :revision => "v5.8.0",
         :fonts_script => "/srv/tile.openstreetmap.org/styles/default/scripts/get-fonts.sh",
         :max_zoom => 19
       }
