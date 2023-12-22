e0cdebb7bf9b7e922de77405d88a90b865142f6a
Tom Hughes <tom@compton.nu>
2023-12-22 10:17:59 +0000

    Set timezone for servers in us-east-2

--- roles/aws-us-east-2.rb
+++ roles/aws-us-east-2.rb
@@ -2,7 +2,8 @@ name "aws-us-east-2"
 description "Role applied to all servers at AWS us-east-2"
 
 default_attributes(
-  :location => "Ohio"
+  :location => "Ohio",
+  :timezone => "US/Eastern"
 )
 
 run_list(
