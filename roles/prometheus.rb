4a2bda00ca60530e312587a1610d258f86b497c5
Tom Hughes <tom@compton.nu>
2024-03-11 17:39:18 +0000

    Disable mod_evasive for prometheus

--- roles/prometheus.rb
+++ roles/prometheus.rb
@@ -1,6 +1,12 @@
 name "prometheus"
 description "Role applied to all prometheus servers"
 
+default_attributes(
+  :apache => {
+    :evasive => false
+  }
+)
+
 run_list(
   "recipe[awscli]",
   "recipe[prometheus::server]",
