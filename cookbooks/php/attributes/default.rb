4dec6c4319879038a2d3c7691d4b23e4c09eb37d
Grant Slater <github@firefishy.com>
2024-04-22 13:49:20 +0100

    Add ubuntu 24.04 versions

--- cookbooks/php/attributes/default.rb
+++ cookbooks/php/attributes/default.rb
@@ -1,8 +1,14 @@
 default[:php][:version] = if platform?("debian")
-                            "8.2"
-                          elsif node[:lsb][:release].to_f < 22.04
-                            "7.4"
-                          else
-                            "8.1"
+                            "8.2"  # PHP version for Debian
+                          elsif platform?("ubuntu")
+                            case node[:lsb][:release].to_f
+                            when 24.04
+                              "8.3"  # PHP version for Ubuntu 24.04
+                            when 22.04
+                              "8.1"  # PHP version for Ubuntu 22.04
+                            when 20.04
+                              "7.4"  # PHP version for Ubuntu 20.04
+                            end
                           end
+
 default[:php][:fpm][:options] = {}
