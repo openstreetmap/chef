62c13124ef096a297f735f3942ce5b1790ef292b
Grant Slater <github@firefishy.com>
2024-05-09 19:51:51 +0100

    ohai: use standard plugin path

--- cookbooks/ohai/resources/plugin.rb
+++ cookbooks/ohai/resources/plugin.rb
@@ -29,11 +29,17 @@ action :create do
     action :nothing
   end
 
-  directory "/etc/chef/ohai" do
+  directory "/etc/chef/ohai/plugins" do
     owner "root"
     group "root"
     mode "755"
     recursive true
+  end
+
+  # FIXME: Cleanup of plugins in the old location
+  declare_resource :file, plugin_old_path do
+    action :delete
+    notifies :reload, "ohai[#{new_resource.plugin}]"
   end
 
   declare_resource :template, plugin_path do
@@ -52,7 +58,11 @@ action :delete do
 end
 
 action_class do
-  def plugin_path
+  def plugin_old_path
     "/etc/chef/ohai/#{new_resource.plugin}.rb"
+  end
+
+  def plugin_path
+    "/etc/chef/ohai/plugins/#{new_resource.plugin}.rb"
   end
 end
