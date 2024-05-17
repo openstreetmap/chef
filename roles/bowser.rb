b86b20ea2cfe55a288ecf4307fb13037511b1968
Tom Hughes <tom@compton.nu>
2024-05-17 17:14:59 +0100

    Move replication state to /store on balerion and bowser

--- roles/bowser.rb
+++ roles/bowser.rb
@@ -40,6 +40,9 @@ default_attributes(
       :postgis => "3"
     },
     :mapnik => "3.1",
+    :replication => {
+      :directory => "/store/replication"
+    },
     :styles => {
       :default => {
         :tile_directories => [
