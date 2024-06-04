7e950d54876c3b14e4495d85fb20fd397bdd7068
Grant Slater <github@firefishy.com>
2024-06-04 13:20:16 +0100

    rhaegal: add IPv6 address

--- roles/rhaegal.rb
+++ roles/rhaegal.rb
@@ -17,6 +17,11 @@ default_attributes(
           :address => "193.198.233.218",
           :prefix => "29",
           :gateway => "193.198.233.217"
+        },
+        :inet6 => {
+          :address => "2001:b68:40ff:3::2",
+          :prefix => "64",
+          :gateway => "2001:b68:40ff:3::1"
         }
       }
     }
