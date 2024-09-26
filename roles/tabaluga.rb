b3538164e1ccbc85adc78d47cdcd9ba4e456aacf
Tom Hughes <tom@compton.nu>
2024-09-26 14:30:58 +0100

    Restore role for tabaluga

---
+++ roles/tabaluga.rb
@@ -0,0 +1,37 @@
+name "tabaluga"
+description "Master role applied to tabaluga"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal => {
+        :interface => "bond0",
+        :role => :internal,
+        :inet => {
+          :address => "10.0.48.14"
+        },
+        :bond => {
+          :mode => "802.3ad",
+          :lacprate => "fast",
+          :xmithashpolicy => "layer3+4",
+          :slaves => %w[eno1 eno2]
+        }
+      },
+      :external => {
+        :interface => "bond0.2",
+        :role => :external,
+        :inet => {
+          :address => "184.104.179.142"
+        },
+        :inet6 => {
+          :address => "2001:470:1:fa1::e"
+        }
+      }
+    }
+  }
+)
+
+run_list(
+  "role[equinix-ams]",
+  "role[hp-g9]"
+)
