137c1558a1a89a99b4770ffa1f4b71fc46eb0020
Grant Slater <github@firefishy.com>
2024-10-28 22:47:24 +0000

    Update dulcy NIC names

--- roles/dulcy.rb
+++ roles/dulcy.rb
@@ -14,7 +14,7 @@ default_attributes(
           :mode => "802.3ad",
           :lacprate => "fast",
           :xmithashpolicy => "layer3+4",
-          :slaves => %w[ens18f0 ens18f1]
+          :slaves => %w[enp1s0f0 enp1s0f1]
         }
       },
       :external => {
