0df4f2f9bbec37153bdb6c92a38ed324409fa6a7
Paul Norman <penorman@mac.com>
2024-11-19 12:44:51 -0800

    Update to tilekiln 0.6.3

--- cookbooks/vectortile/recipes/default.rb
+++ cookbooks/vectortile/recipes/default.rb
@@ -99,7 +99,7 @@ end
 python_package "tilekiln" do
   python_virtualenv tilekiln_directory
   python_version "3"
-  version "0.6.2"
+  version "0.6.3"
 end
 
 template "/srv/vector.openstreetmap.org/html/index.html" do
