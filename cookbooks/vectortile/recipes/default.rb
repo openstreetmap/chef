cfea384b974c362aee67c16a27b36be6d5de5ec4
Paul Norman <penorman@mac.com>
2024-11-24 21:36:47 -0800

    vectortile: track osm2pgsql db age

--- cookbooks/vectortile/recipes/default.rb
+++ cookbooks/vectortile/recipes/default.rb
@@ -280,3 +280,17 @@ else
     action [:stop, :disable]
   end
 end
+
+package %w[
+  ruby-pg
+  ruby-webrick
+]
+
+prometheus_exporter "osm2pgsql" do
+  port 10027
+  user "tileupdate"
+  restrict_address_families "AF_UNIX"
+  options [
+    "--database-name=spirit"
+  ]
+end
