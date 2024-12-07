d3dc43c96d47c2a49c9a9d2118ae1d3c3d6af02f
Grant Slater <github@firefishy.com>
2024-12-07 16:54:40 +0000

    imagery: set mapserver memory limit

--- cookbooks/imagery/resources/site.rb
+++ cookbooks/imagery/resources/site.rb
@@ -107,6 +107,7 @@ action :create do
                 "MS_ERRORFILE" => "stderr",
                 "GDAL_CACHEMAX" => "512"
     limit_nofile 16384
+    memory_max "8G"
     user "imagery"
     group "imagery"
     exec_start "/usr/bin/multiwatch -f 8 --signal=TERM -- /usr/lib/cgi-bin/mapserv"
