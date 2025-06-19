8b51fe9a6d03f48d23fe4097b06e94f04c83a449
Paul Norman <penorman@mac.com>
2025-06-18 23:35:00 -0700

    vectortile: update spirit

--- cookbooks/vectortile/attributes/default.rb
+++ cookbooks/vectortile/attributes/default.rb
@@ -15,7 +15,7 @@ default[:vectortile][:rerender][:ocean][:threads] = [0.5 * node.cpu_cores, 2].ma
 default[:vectortile][:rerender][:ocean][:minzoom] = node[:vectortile][:rerender][:lowzoom][:maxzoom] + 1
 
 default[:vectortile][:tilekiln][:version] = "0.8.1"
-default[:vectortile][:spirit][:version] = "033a4117ec3604e28824b3f3608f5d163ef1b450"
+default[:vectortile][:spirit][:version] = "6cdedcda65fbb8fec83342571c74a33208943b32"
 default[:vectortile][:themepark][:version] = "beb454cc56e88533fb398ab293489c4e91f4d42b"
 
 default[:postgresql][:versions] |= [node[:vectortile][:database][:cluster].split("/").first]
