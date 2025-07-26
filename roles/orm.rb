13ded8ffeb47ccf2cdb4655c69657f0f25adf4d5
Tom Hughes <tom@compton.nu>
2025-07-26 17:07:27 +0100

    Add role for orm

---
+++ roles/orm.rb
@@ -0,0 +1,29 @@
+name "orm"
+description "Master role applied to orm"
+
+default_attributes(
+  :networking => {
+    :interfaces => {
+      :internal => {
+        :inet => {
+          :address => "10.5.7.34",
+          :prefix => "29",
+          :gateway => "10.5.7.33",
+          :public_address => "23.139.196.5"
+        },
+        :inet6 => {
+          :address => "2602:f629:0:bc::2",
+          :prefix => "64",
+          :gateway => "2602:f629:0:bc::1"
+        },
+        :bond => {
+          :slaves => %w[eno1 eno2]
+        }
+      }
+    }
+  }
+)
+
+run_list(
+  "role[pixeldeck]"
+)
---
+++ roles/pixeldeck.rb
@@ -0,0 +1,20 @@
+name "pixeldeck"
+description "Role applied to all servers at PixelDeck"
+
+default_attributes(
+  :hosted_by => "PixelDeck",
+  :location => "Kansas City, Missouri"
+)
+
+override_attributes(
+  :networking => {
+    :nameservers => ["10.5.7.33", "2602:f629:0:bc::1"]
+  },
+  :ntp => {
+    :servers => ["0.us.pool.ntp.org", "1.us.pool.ntp.org", "north-america.pool.ntp.org"]
+  }
+)
+
+run_list(
+  "role[us]"
+)
