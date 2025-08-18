ac07cd3e4ed4b0a5319a6cc77c3d78719bbda329
Grant Slater <github@firefishy.com>
2025-08-18 18:18:30 +0100

    php: improve version selection logic

--- cookbooks/php/attributes/default.rb
+++ cookbooks/php/attributes/default.rb
@@ -1,12 +1,18 @@
 default[:php][:version] = if platform?("debian")
-                            if node[:platform_version].to_i >= 13
-                              "8.4"
-                            else
-                              "8.2"
+                            case node[:platform_version].to_i
+                            when 13
+                              "8.4"  # PHP version for Debian 13 Trixie
+                            when 12
+                              "8.2"  # PHP version for Debian 12 Bookworm
                             end
-                          elsif node[:lsb][:release].to_f < 22.04
-                            "7.4"
-                          else
-                            "8.1"
+                          elsif platform?("ubuntu")
+                            case node[:lsb][:release].to_f
+                            when 24.04
+                              "8.3"  # PHP version for Ubuntu 24.04
+                            when 22.04
+                              "8.1"  # PHP version for Ubuntu 22.04
+                            when 20.04
+                              "7.4"  # PHP version for Ubuntu 20.04
+                            end
                           end
 default[:php][:fpm][:options] = {}
