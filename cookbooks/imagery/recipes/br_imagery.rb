9f5abcf0c38e298e6f00957fe315f0b60de55db9
Grant Slater <github@firefishy.com>
2025-09-02 01:09:04 +0100

    imagery: Add BR ibge-aerial-2021

---
+++ cookbooks/imagery/recipes/br_imagery.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook:: imagery
+# Recipe:: br_imagery
+#
+# Copyright:: 2025, OpenStreetMap Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "imagery"
+
+imagery_site "br-imagery.openstreetmap.org" do
+  title "OpenStreetMap - Brazil - Aerial Imagery"
+  aliases ["br-imagery.osm.org"]
+  bbox [[41.235, 22.357], [44.215, 28.608]]
+end
+
+imagery_layer "ibge-aerial-2021" do
+  site "br-imagery.openstreetmap.org"
+  title "Brazil IBGE Aerial Imagery 2021"
+  source "/store/imagery/br/ibge-aerial-2021/ibge-aerial-2021.webp.google.r_bilinear.bs_256.aligned.cog.tif"
+  copyright "(c) Instituto Brasileiro de Geografia e Estatística"
+  projection "EPSG:3857"
+  max_zoom 21
+  default_layer true
+  revision 1
+end
