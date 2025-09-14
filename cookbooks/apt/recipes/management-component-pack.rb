3f6b8f9030cbcf8318ddb84a046d6477088a38e4
Tom Hughes <tom@compton.nu>
2025-09-14 09:51:29 +0100

    Disable HPE repositories

--- cookbooks/apt/recipes/management-component-pack.rb
+++ cookbooks/apt/recipes/management-component-pack.rb
@@ -23,43 +23,43 @@ apt_repository "management-component-pack" do
   action :remove
 end
 
-if platform?("debian")
-  apt_repository "mcp" do
-    uri "https://downloads.linux.hpe.com/SDR/repo/mcp"
-    distribution "#{node[:lsb][:codename]}/current"
-    components ["non-free"]
-    key ["https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub", "https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key2.pub"]
-  end
-
-  if node[:dmi][:system][:product_name].end_with?("Gen9")
-    apt_repository "mcp-gen9" do
-      uri "https://downloads.linux.hpe.com/SDR/repo/mcp"
-      distribution "stretch/current-gen9"
-      components ["non-free"]
-      key ["https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub", "https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key2.pub"]
-    end
-  end
-elsif platform?("ubuntu")
-  if node[:dmi][:system][:product_name].end_with?("Gen10")
-    apt_repository "mcp-jammy" do
-      uri "https://downloads.linux.hpe.com/SDR/repo/mcp"
-      distribution "jammy/current"
-      components ["non-free"]
-      key ["https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub", "https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key2.pub"]
-    end
-
-    apt_repository "mcp-focal-gen10" do
-      uri "https://downloads.linux.hpe.com/SDR/repo/mcp"
-      distribution "focal/current-gen10"
-      components ["non-free"]
-      key ["https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub", "https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key2.pub"]
-    end
-  else
-    apt_repository "mcp-bionic-gen9" do
-      uri "https://downloads.linux.hpe.com/SDR/repo/mcp"
-      distribution "bionic/current-gen9"
-      components ["non-free"]
-      key ["https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub", "https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key2.pub"]
-    end
-  end
-end
+# if platform?("debian")
+#   apt_repository "mcp" do
+#     uri "https://downloads.linux.hpe.com/SDR/repo/mcp"
+#     distribution "#{node[:lsb][:codename]}/current"
+#     components ["non-free"]
+#     key ["https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub", "https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key2.pub"]
+#   end
+#
+#   if node[:dmi][:system][:product_name].end_with?("Gen9")
+#     apt_repository "mcp-gen9" do
+#       uri "https://downloads.linux.hpe.com/SDR/repo/mcp"
+#       distribution "stretch/current-gen9"
+#       components ["non-free"]
+#       key ["https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub", "https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key2.pub"]
+#     end
+#   end
+# elsif platform?("ubuntu")
+#   if node[:dmi][:system][:product_name].end_with?("Gen10")
+#     apt_repository "mcp-jammy" do
+#       uri "https://downloads.linux.hpe.com/SDR/repo/mcp"
+#       distribution "jammy/current"
+#       components ["non-free"]
+#       key ["https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub", "https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key2.pub"]
+#     end
+#
+#     apt_repository "mcp-focal-gen10" do
+#       uri "https://downloads.linux.hpe.com/SDR/repo/mcp"
+#       distribution "focal/current-gen10"
+#       components ["non-free"]
+#       key ["https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub", "https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key2.pub"]
+#     end
+#   else
+#     apt_repository "mcp-bionic-gen9" do
+#       uri "https://downloads.linux.hpe.com/SDR/repo/mcp"
+#       distribution "bionic/current-gen9"
+#       components ["non-free"]
+#       key ["https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub", "https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key2.pub"]
+#     end
+#   end
+# end
