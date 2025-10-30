c5d54c31a363882b3459d092b0fcf9fc55d9103f
Grant Slater <github@firefishy.com>
2025-10-30 14:04:22 +0000

    imagery: update africa topo test to v6

--- cookbooks/imagery/recipes/na_sgswa_topo.rb
+++ cookbooks/imagery/recipes/na_sgswa_topo.rb
@@ -61,8 +61,8 @@ imagery_layer "african_topo_50k" do
   site "namibia-topo.openstreetmap.org.za"
   title "African Topo 50k"
   projection "EPSG:4326"
-  source "/store/imagery/african-topo/african-topo-v5.vrt"
+  source "/store/imagery/african-topo/african-topo-v6.vrt"
   copyright "Copyright &copy"
   max_zoom 16
-  revision 5
+  revision 6
 end
