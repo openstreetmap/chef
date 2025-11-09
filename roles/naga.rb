4899467dd317bef90484860abcde227c348ce339
Tom Hughes <tom@compton.nu>
2025-11-09 19:04:57 +0000

    Add foundation wikis to naga

--- roles/naga.rb
+++ roles/naga.rb
@@ -2,6 +2,28 @@ name "naga"
 description "Master role applied to naga"
 
 default_attributes(
+  :elasticsearch => {
+    :version => "7.x",
+    :cluster => {
+      :name => "foundation"
+    }
+  },
+  :memcached => {
+    :memory_limit => 400,
+    :chunk_growth_factor => 1.05,
+    :min_item_size => 5
+  },
+  :mysql => {
+    :settings => {
+      :mysqld => {
+        :innodb_buffer_pool_size => "512M",
+        :key_buffer_size => "64M",
+        :max_connections => "200",
+        :sort_buffer_size => "8M",
+        :tmp_table_size => "48M"
+      }
+    }
+  },
   :networking => {
     :interfaces => {
       :internal => {
@@ -40,10 +62,15 @@ run_list(
   "role[irc]",
   "role[blogs]",
   "role[switch2osm]",
+  "role[elasticsearch]",
   "recipe[foundation::birthday]",
+  "recipe[foundation::board]",
+  "recipe[foundation::dwg]",
   "recipe[foundation::mastodon]",
+  "recipe[foundation::mwg]",
   "recipe[foundation::owg]",
   "recipe[foundation::welcome]",
+  "recipe[foundation::wiki]",
   "recipe[stateofthemap::container]",
   "recipe[hot]",
   "recipe[ideditor]",
