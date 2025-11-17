de34291111126583eed7cc63d98b23e5f65cb89e
Tom Hughes <tom@compton.nu>
2025-11-17 19:01:38 +0000

    Switch foundation wiki to use Vector 2022

--- cookbooks/foundation/recipes/wiki.rb
+++ cookbooks/foundation/recipes/wiki.rb
@@ -36,7 +36,7 @@ mediawiki_site "osmfoundation.org" do
   database_user "osmf-wikiuser"
   database_password passwords["wiki"]["database"]
   admin_password passwords["wiki"]["admin"]
-  skin "vector"
+  skin "vector-2022"
   logo "/w/skins/OSMFoundation/img/logo.png"
   email_contact "webmaster@openstreetmap.org"
   email_sender "wiki@noreply.openstreetmap.org"
