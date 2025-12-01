8f89dc8e935c5c05ba242a5f64eba7c62854a705
Paul Norman <penorman@mac.com>
2025-11-30 17:48:04 -0800

    vectortile: make styles with absolute URLs available

--- test/integration/vectortile/inspec/nginx_spec.rb
+++ test/integration/vectortile/inspec/nginx_spec.rb
@@ -24,3 +24,23 @@ end
 describe http("https://localhost", :ssl_verify => false) do
   its("status") { should cmp 200 }
 end
+
+describe http("https://localhost/styles/shortbread/colorful.json", :ssl_verify => false) do
+  its("status") { should cmp 200 }
+  its("headers") { should_not include "Access-Control-Allow-Origin" }
+  its("headers.vary") { should cmp "Origin" }
+end
+
+describe http("https://localhost/styles/shortbread/colorful.json", :headers => { "Origin" => "https://www.openstreetmap.org" }, :ssl_verify => false) do
+  its("status") { should cmp 200 }
+  its("headers.Access-Control-Allow-Origin") { should cmp "https://www.openstreetmap.org" }
+  its("headers.vary") { should cmp "Origin" }
+end
+
+describe json(:content => http("https://localhost/styles/shortbread/colorful.json", :ssl_verify => false).body) do
+  its(%w[sources versatiles-shortbread tiles]) do
+    should eq(["https://vector.openstreetmap.org/shortbread_v1/{z}/{x}/{y}.mvt"])
+  end
+  its(["sprite", 0, "url"]) { should match %r{https://vector.openstreetmap.org/} }
+  its(["glyphs"]) { should match %r{https://vector.openstreetmap.org/} }
+end
