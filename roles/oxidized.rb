2a9ca5c78610a033559ad3c437f78f741564f8b3
Tom Hughes <tom@compton.nu>
2026-03-02 09:41:02 +0000

    Restore some auxilliary group memberships

--- roles/oxidized.rb
+++ roles/oxidized.rb
@@ -1,6 +1,16 @@
 name "oxidized"
 description "Role applied to all oxidized servers"
 
+default_attributes(
+  :accounts => {
+    :groups => {
+      :oxidized => {
+        :members => [:grant, :tomh]
+      }
+    }
+  }
+)
+
 run_list(
   "recipe[oxidized]"
 )
