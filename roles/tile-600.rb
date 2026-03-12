a1a40e316e00e617fd66f9d1fca02e4a841f8325
Tom Hughes <tom@compton.nu>
2026-03-12 19:27:49 +0000

    Add tile-600 role for tile servers running carto 6.0.0

---
+++ roles/tile-600.rb
@@ -0,0 +1,23 @@
+name "tile-600"
+description "Role applied to all tile servers running carto 6.0.0"
+
+default_attributes(
+  :tile => {
+    :database => {
+      :output_mode => "flex",
+      :tag_transform_script => "/srv/tile.openstreetmap.org/styles/default/openstreetmap-carto-flex.lua"
+    },
+    :styles => {
+      :default => {
+        :revision => "v6.0.0",
+        :fonts_script => "/srv/tile.openstreetmap.org/styles/default/scripts/get-fonts.py",
+        :common_values_script => "/srv/tile.openstreetmap.org/styles/default/common-values.sql",
+        :common_values_tables => %w[carto_pois]
+      }
+    }
+  }
+)
+
+run_list(
+  "role[tile]"
+)
