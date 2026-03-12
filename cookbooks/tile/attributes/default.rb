f1a6787fc9146f1027525b367a88ee69580d20ef
Tom Hughes <tom@compton.nu>
2026-03-12 18:57:41 +0000

    Allow the output mode for osm2pgsql to be set for tile servers

--- cookbooks/tile/attributes/default.rb
+++ cookbooks/tile/attributes/default.rb
@@ -2,6 +2,7 @@ default[:tile][:database][:cluster] = "17/main"
 default[:tile][:database][:postgis] = "3"
 default[:tile][:database][:node_file] = "/store/database/nodes"
 default[:tile][:database][:multi_geometry] = true
+default[:tile][:database][:output_mode] = nil
 default[:tile][:database][:tag_transform_script] = nil
 
 default[:tile][:mapnik] = "3.1"
