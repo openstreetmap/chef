fb7e5a550e1c5e200332481e1620d65cbd8364a7
Tom Hughes <tom@compton.nu>
2026-05-06 18:28:41 +0100

    Switch OTRS to use postgres 18

--- cookbooks/otrs/attributes/default.rb
+++ cookbooks/otrs/attributes/default.rb
@@ -1,9 +1,9 @@
 default[:otrs][:user] = "otrs"
 default[:otrs][:group] = nil
-default[:otrs][:database_cluster] = "16/main"
+default[:otrs][:database_cluster] = "18/main"
 default[:otrs][:database_name] = "otrs"
 default[:otrs][:database_user] = "otrs"
 default[:otrs][:database_password] = "otrs"
 default[:otrs][:site] = "otrs"
 
-default[:postgresql][:versions] |= %w[16 18]
+default[:postgresql][:versions] |= %w[18]
