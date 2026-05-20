35434acc2449fdfe9a1652cb04e776555b498650
Grant Slater <github@firefishy.com>
2026-05-20 09:34:02 +0100

    community: Bump discourse to latest ESR release

--- cookbooks/community/recipes/default.rb
+++ cookbooks/community/recipes/default.rb
@@ -76,7 +76,7 @@ git "/srv/community.openstreetmap.org/docker" do
   action :sync
   repository "https://github.com/discourse/discourse_docker.git"
   # DANGER launch wrapper automatically updates git repo if rebuild method used: https://github.com/discourse/discourse_docker/blob/107ffb40fe8b1ea40e00814468db974a4f3f8e8f/launcher#L799
-  revision "cfc1ce28054d64d26808545fa5e69660a234c530"
+  revision "9fecdb84284ae36eba9a71ca954d74e660a117b8"
   user "root"
   group "root"
   notifies :run, "notify_group[discourse_container_new_data]"
