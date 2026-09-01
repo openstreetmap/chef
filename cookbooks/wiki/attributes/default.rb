9d88e9fbda858ed7e18e9d7fd91660c324413c42
Grant Slater <github@firefishy.com>
2026-09-01 22:45:32 +0100

    wiki: Upgrade to 1.46

--- cookbooks/wiki/attributes/default.rb
+++ cookbooks/wiki/attributes/default.rb
@@ -6,4 +6,4 @@ default[:wiki][:site_aliases] = [
 default[:wiki][:site_notice] = nil
 default[:wiki][:site_readonly] = nil
 default[:wiki][:test_mode] = false
-default[:wiki][:mediawiki_version] = "1.43"
+default[:wiki][:mediawiki_version] = "1.46"
