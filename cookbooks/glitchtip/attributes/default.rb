07a57063203b25acfd7ee373f2f2496d6684a0cf
Tom Hughes <tom@compton.nu>
2026-09-05 12:59:55 +0100

    Enable postgresql connections from glitchtip container

--- cookbooks/glitchtip/attributes/default.rb
+++ cookbooks/glitchtip/attributes/default.rb
@@ -4,3 +4,7 @@ default[:glitchtip][:database_user] = "glitchtip"
 default[:glitchtip][:database_password] = "database"
 
 default[:postgresql][:versions] |= %w[18]
+default[:postgresql][:default][:listen_addresses] = "10.88.0.1"
+default[:postgresql][:default][:late_authentication_rules] = [
+  { :database => "glitchtip", :user => "glitchtip", :address => "10.88.0.0/16" }
+]
